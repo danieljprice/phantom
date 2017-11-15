e51630893a98555988606ca12085a86fdd3c72cb
David Liptai <david.liptai@monash.edu>
2017-11-15 16:48:50 +1100

    (GR) Added the same accretion conditions for Kerr metric as Schwarzschild, for the time being. But it probably should be different.

--- src/main/externalforces_gr.F90
+++ src/main/externalforces_gr.F90
@@ -219,7 +219,14 @@ subroutine accrete_particles(iexternalforce,xi,yi,zi,hi,mi,ti,accreted,i)
     if (r2 < (accradius1_hard)**2) accreted = .true.
 
  case(imet_kerr)
-    if(ifirst==0) print*,'Accrete particles: no accretion implemented for Kerr metric'
+   !  if(ifirst==0) print*,'Accrete particles: no accretion implemented for Kerr metric'
+   r2 = xi*xi + yi*yi + zi*zi
+   if (r2 < accradius1**2 .and. maxphase==maxp .and. present(i)) then
+     call set_particle_type(i,iboundary)
+     npartoftype(igas) = npartoftype(igas) - 1
+     npartoftype(iboundary) = npartoftype(iboundary) + 1
+   endif
+   if (r2 < (accradius1_hard)**2) accreted = .true.
 
  end select
 
