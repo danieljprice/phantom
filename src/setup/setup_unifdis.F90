e125e3e2dd78a3110b1f14289a5f58c49cfb9eb7
Daniel Price <daniel.price@monash.edu>
2018-01-10 13:57:11 +1100

    (setup_unifdis) added .setup file for general uniform setup; also fixed for non-isothermal case

--- src/setup/setup_unifdis.F90
+++ src/setup/setup_unifdis.F90
@@ -23,11 +23,14 @@
 !+
 !--------------------------------------------------------------------------
 module setup
+ use setup_params, only:rhozero
+ use dim,          only:use_dust
+ use options,      only:use_dustfrac
  implicit none
  public :: setpart
 
  integer, private :: npartx,ilattice
- real,    private :: polykset
+ real,    private :: cs0,dust_to_gas
  private
 
 contains
@@ -38,15 +41,12 @@ contains
 !+
 !----------------------------------------------------------------
 subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,time,fileprefix)
- use dim,          only:use_dust
- use options,      only:use_dustfrac
- use setup_params, only:rhozero,npart_total
+ use dim,          only:maxvxyzu
+ use setup_params, only:npart_total
  use io,           only:master
  use unifdis,      only:set_unifdis
  use boundary,     only:xmin,ymin,zmin,xmax,ymax,zmax,dxbound,dybound,dzbound
- use mpiutils,     only:bcast_mpi
  use part,         only:dustfrac
- use prompting,    only:prompt
  use physcon,      only:pi
  use dust,         only:set_dustfrac
  integer,           intent(in)    :: id
@@ -54,65 +54,51 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  integer,           intent(out)   :: npartoftype(:)
  real,              intent(out)   :: xyzh(:,:)
  real,              intent(out)   :: massoftype(:)
- real,              intent(out)   :: polyk,gamma,hfact
+ real,              intent(out)   :: polyk,gamma
+ real,              intent(inout) :: hfact
  real,              intent(inout) :: time
  character(len=20), intent(in)    :: fileprefix
  real,              intent(out)   :: vxyzu(:,:)
- real :: totmass,deltax,dust_to_gas
- integer :: i,maxp,maxvxyzu
-!
-!--general parameters
-!
+ character(len=40) :: filename
+ real    :: totmass,deltax
+ integer :: i,ierr
+ logical :: iexist
+ !
+ !--general parameters
+ !
  time = 0.
- hfact = 1.2
- gamma = 1.
+ if (maxvxyzu < 4) then
+    gamma = 1.
+ else
+    gamma = 5./3.
+ endif
+ !
+ !--get disc setup parameters from file or interactive setup
+ !
+ filename=trim(fileprefix)//'.setup'
+ inquire(file=filename,exist=iexist)
+ if (iexist) then
+    !--read from setup file
+    call read_setupfile(filename,ierr)
+    if (id==master) call write_setupfile(filename)
+    if (ierr /= 0) then
+       stop
+    endif
+ elseif (id==master) then
+    call setup_interactive(id,polyk)
+    call write_setupfile(filename)
+    stop 'rerun phantomsetup after editing .setup file'
+ else
+    stop
+ endif
 !
 !--setup particles
 !
- maxp = size(xyzh(1,:))
- maxvxyzu = size(vxyzu(:,1))
- if (id==master) then
-    npartx = 50
-    print*,' uniform setup... (max = ',nint((maxp)**(1/3.)),')'
-    call prompt('enter number of particles in x direction ',npartx,1)
- endif
- call bcast_mpi(npartx)
  deltax = dxbound/npartx
-
- if (id==master) then
-    rhozero = 1.
-    call prompt(' enter density (gives particle mass)',rhozero,0.)
- endif
- call bcast_mpi(rhozero)
-
- if (maxvxyzu < 4) then
-    if (id==master) then
-       polykset = 1.
-       call prompt(' enter sound speed in code units (sets polyk)',polykset,0.)
-    endif
-    call bcast_mpi(polykset)
-    polyk = polykset**2
-    print*,' polyk = ',polyk
- else
-    polyk = 0.
-    polykset = 0.
- endif
-
- if (use_dust) use_dustfrac = .true.
- if (use_dustfrac) then
-    dust_to_gas = 1.e-2
-    if (id==master) call prompt(' enter dust-to-gas ratio ',dust_to_gas,0.)
-    call bcast_mpi(dust_to_gas)
- endif
-
- if (id==master) then
-    ilattice = 1
-    call prompt(' select lattice type (1=cubic, 2=closepacked)',ilattice,1)
- endif
- call bcast_mpi(ilattice)
 
  npart = 0
  npart_total = 0
+ if (use_dust) use_dustfrac = .true.
 
  select case(ilattice)
  case(1)
@@ -130,10 +116,12 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
 
  totmass = rhozero*dxbound*dybound*dzbound
  massoftype = totmass/npart_total
- print*,' particle mass = ',massoftype(1)
+ if (id==master) print*,' particle mass = ',massoftype(1)
 
+ if (id==master) print*,' initial sound speed = ',cs0,' pressure = ',cs0**2/gamma
  do i=1,npart
     vxyzu(1:3,i) = 0.
+    if (maxvxyzu >= 4) vxyzu(4,i) = cs0**2/(gamma*(gamma-1.))
  enddo
 
  if (use_dustfrac) then
@@ -144,5 +132,117 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
 
 end subroutine setpart
 
-end module setup
+!------------------------------------------------------------------------
+!
+! interactive setup
+!
+!------------------------------------------------------------------------
+subroutine setup_interactive(id,polyk)
+ use io,        only:master
+ use mpiutils,  only:bcast_mpi
+ use dim,       only:maxp,maxvxyzu
+ use prompting, only:prompt
+ integer, intent(in)  :: id
+ real,    intent(out) :: polyk
+ !
+ ! number of particles
+ !
+ npartx = 64
+ if (id==master) then
+    print*,' uniform setup... (max = ',nint((maxp)**(1/3.)),')'
+    call prompt('enter number of particles in x direction ',npartx,1)
+ endif
+ call bcast_mpi(npartx)
+ !
+ ! mean density
+ !
+ rhozero = 1.
+ if (id==master) call prompt(' enter density (gives particle mass)',rhozero,0.)
+ call bcast_mpi(rhozero)
+ !
+ ! sound speed in code units
+ !
+ if (id==master) then
+    cs0 = 1.
+    call prompt(' enter sound speed in code units (sets polyk)',cs0,0.)
+ endif
+ call bcast_mpi(cs0)
+ if (maxvxyzu < 4) then
+    polyk = cs0**2
+    print*,' polyk = ',polyk
+ else
+    polyk = 0.
+ endif
+ !
+ ! dust to gas ratio
+ !
+ if (use_dust) use_dustfrac = .true.
+ if (use_dustfrac) then
+    dust_to_gas = 1.e-2
+    if (id==master) call prompt(' enter dust-to-gas ratio ',dust_to_gas,0.)
+    call bcast_mpi(dust_to_gas)
+ endif
+ !
+ ! type of lattice
+ !
+ if (id==master) then
+    ilattice = 1
+    call prompt(' select lattice type (1=cubic, 2=closepacked)',ilattice,1)
+ endif
+ call bcast_mpi(ilattice)
 
+end subroutine setup_interactive
+
+!------------------------------------------------------------------------
+!
+! write setup file
+!
+!------------------------------------------------------------------------
+subroutine write_setupfile(filename)
+ use infile_utils, only:write_inopt
+ character(len=*), intent(in) :: filename
+ integer :: iunit
+
+ print "(/,a)",' writing setup options file '//trim(filename)
+ open(newunit=iunit,file=filename,status='replace',form='formatted')
+ write(iunit,"(a)") '# input file for uniform setup routine'
+ call write_inopt(npartx,'nx','number of particles in x direction',iunit)
+ call write_inopt(rhozero,'rhozero','initial density in code units',iunit)
+ call write_inopt(cs0,'cs0','initial sound speed in code units',iunit)
+ if (use_dust) call write_inopt(dust_to_gas,'dust_to_gas','initial dust-to-gas ratio',iunit)
+ call write_inopt(ilattice,'ilattice','lattice type (1=cubic, 2=closepacked)',iunit)
+ close(iunit)
+
+end subroutine write_setupfile
+
+!------------------------------------------------------------------------
+!
+! read setup file
+!
+!------------------------------------------------------------------------
+subroutine read_setupfile(filename,ierr)
+ use infile_utils, only:open_db_from_file,inopts,read_inopt,close_db
+ character(len=*), intent(in)  :: filename
+ integer,          intent(out) :: ierr
+ integer, parameter :: iunit = 21
+ integer :: nerr
+ type(inopts), allocatable :: db(:)
+
+ print "(a)",' reading setup options from '//trim(filename)
+ nerr = 0
+ ierr = 0
+ call open_db_from_file(db,filename,iunit,ierr)
+ call read_inopt(npartx,'nx',db,min=8,errcount=nerr)
+ call read_inopt(rhozero,'rhozero',db,min=0.,errcount=nerr)
+ call read_inopt(cs0,'cs0',db,min=0.,errcount=nerr)
+ if (use_dust) call read_inopt(dust_to_gas,'dust_to_gas',db,min=0.,errcount=nerr)
+ call read_inopt(ilattice,'ilattice',db,min=1,max=2,errcount=nerr)
+ call close_db(db)
+ if (nerr > 0) then
+    print "(1x,i2,a)",nerr,' error(s) during read of setup file: re-writing...'
+    ierr = nerr
+ endif
+
+end subroutine read_setupfile
+
+end module setup
