0b0d49404d6ed3b329628c77a189a507f70a83fc
James Wurster <j.wurster@exeter.ac.uk>
2018-01-11 12:35:24 +0000

    (kdtree) readded format change for clarity; no numerical or physical changes

--- src/main/kdtree.F90
+++ src/main/kdtree.F90
@@ -631,7 +631,7 @@ subroutine construct_node(nodeentry, nnode, mymum, level, xmini, xmaxi, npnode, 
        pmassi = massoftype(iamtype(iphase_soa(inoderange(1,nnode)+i-1)))
     endif
     quads(1) = quads(1) + pmassi*(3.*dx*dx - dr2)  ! Q_xx
-    quads(2) = quads(2) + pmassi*(3.*dx*dy)        ! Q_xy = Q_yz
+    quads(2) = quads(2) + pmassi*(3.*dx*dy)        ! Q_xy = Q_yx
     quads(3) = quads(3) + pmassi*(3.*dx*dz)        ! Q_xz = Q_zx
     quads(4) = quads(4) + pmassi*(3.*dy*dy - dr2)  ! Q_yy
     quads(5) = quads(5) + pmassi*(3.*dy*dz)        ! Q_yz = Q_zy
@@ -1051,32 +1051,33 @@ pure subroutine compute_fnode(dx,dy,dz,dr,totmass,quads,fnode)
  dfydzq = dr5*(qyz -  5.*ry*riQiz - 5.0*rz*riQiy + 17.5*ry*rz*rijQij)
  dfzdzq = dr5*(qzz - 10.*rz*riQiz - 2.5*rijQij   + 17.5*rz*rz*rijQij)
  d2fxxxq = dr6*(-15.*qxx*rx + 105.*rx*rx*riQix - 15.*riQix - 157.5*rx*rx*rx*rijQij + 52.5*rx*rijQij)
- d2fxxyq = dr6*(35.*rx*rx*riQiy - 10.*qxy*rx - 5.*qxx*ry - 5.*riQiy &
-              + 70.*rx*ry*riQix - 157.5*rx*rx*ry*rijQij + 17.5*ry*rijQij)
- d2fxxzq = dr6*(35.*rx*rx*riQiz - 10.*qxz*rx - 5.*qxx*rz - 5.*riQiz &
-              + 70.*rx*rz*riQix - 157.5*rx*rx*rz*rijQij + 17.5*rz*rijQij)
- d2fxyyq = dr6*(35.*ry*ry*riQix - 10.*qxy*ry - 5.*qyy*rx - 5.*riQix &
-              + 70.*rx*ry*riQiy - 157.5*rx*ry*ry*rijQij + 17.5*rx*rijQij)
- d2fxyzq = dr6*(35.*rx*ry*riQiz - 5.*qyz*rx - 5.*qxz*ry - 5.*qxy*rz &
-              + 35.*ry*rz*riQix + 35.*rx*rz*riQiy - 157.5*rx*ry*rz*rijQij)
- d2fxzzq = dr6*(35.*rz*rz*riQix - 10.*qxz*rz - 5.*qzz*rx - 5.*riQix &
-              + 70.*rx*rz*riQiz - 157.5*rx*rz*rz*rijQij + 17.5*rx*rijQij)
+ d2fxxyq = dr6*(35.*rx*rx*riQiy -  5.*qxx*ry - 5.*riQiy + 17.5*ry*rijQij - 157.5*rx*rx*ry*rijQij &
+              + 70.*rx*ry*riQix - 10.*qxy*rx)
+ d2fxxzq = dr6*(35.*rx*rx*riQiz -  5.*qxx*rz - 5.*riQiz + 17.5*rz*rijQij - 157.5*rx*rx*rz*rijQij &
+              + 70.*rx*rz*riQix - 10.*qxz*rx)
+ d2fxyyq = dr6*(70.*rx*ry*riQiy - 10.*qxy*ry - 5.*riQix + 17.5*rx*rijQij - 157.5*rx*ry*ry*rijQij &
+              + 35.*ry*ry*riQix -  5.*qyy*rx)
+ d2fxyzq = dr6*(35.*rx*ry*riQiz -  5.*qyz*rx  &
+              + 35.*ry*rz*riQix -  5.*qxz*ry  &
+              + 35.*rx*rz*riQiy -  5.*qxy*rz                             - 157.5*rx*ry*rz*rijQij)
+ d2fxzzq = dr6*(70.*rx*rz*riQiz - 10.*qxz*rz - 5.*riQix + 17.5*rx*rijQij - 157.5*rx*rz*rz*rijQij &
+              + 35.*rz*rz*riQix -  5.*qzz*rx)
  d2fyyyq = dr6*(-15.*qyy*ry + 105.*ry*ry*riQiy - 15.*riQiy - 157.5*ry*ry*ry*rijQij + 52.5*ry*rijQij)
- d2fyyzq = dr6*(35.*ry*ry*riQiz - 10.*qyz*ry - 5.*qyy*rz - 5.*riQiz &
-              + 70.*ry*rz*riQiy - 157.5*ry*ry*rz*rijQij + 17.5*rz*rijQij)
- d2fyzzq = dr6*(35.*rz*rz*riQiy - 10.*qyz*rz - 5.*qzz*ry - 5.*riQiy &
-              + 70.*ry*rz*riQiz - 157.5*ry*rz*rz*rijQij + 17.5*ry*rijQij)
+ d2fyyzq = dr6*(35.*ry*ry*riQiz -  5.*qyy*rz - 5.*riQiz + 17.5*rz*rijQij - 157.5*ry*ry*rz*rijQij &
+              + 70.*ry*rz*riQiy - 10.*qyz*ry)
+ d2fyzzq = dr6*(70.*ry*rz*riQiz - 10.*qyz*rz - 5.*riQiy + 17.5*ry*rijQij - 157.5*ry*rz*rz*rijQij &
+              + 35.*rz*rz*riQiy -  5.*qzz*ry)
  d2fzzzq = dr6*(-15.*qzz*rz + 105.*rz*rz*riQiz - 15.*riQiz - 157.5*rz*rz*rz*rijQij + 52.5*rz*rijQij)
 
- fnode(1)  = fnode(1)  - dx*dr3m + fqx ! fx
- fnode(2)  = fnode(2)  - dy*dr3m + fqy ! fy
- fnode(3)  = fnode(3)  - dz*dr3m + fqz ! fz
- fnode(4)  = fnode(4)  + dr3m*(3.*rx*rx - 1.) + dfxdxq ! dfx/dx
- fnode(5)  = fnode(5)  + dr3m*(3.*rx*ry)      + dfxdyq ! dfx/dy = dfy/dx
- fnode(6)  = fnode(6)  + dr3m*(3.*rx*rz)      + dfxdzq ! dfx/dz = dfz/dx
- fnode(7)  = fnode(7)  + dr3m*(3.*ry*ry - 1.) + dfydyq ! dfy/dy
- fnode(8)  = fnode(8)  + dr3m*(3.*ry*rz)      + dfydzq ! dfy/dz = dfz/dy
- fnode(9)  = fnode(9)  + dr3m*(3.*rz*rz - 1.) + dfzdzq ! dfz/dz
+ fnode( 1) = fnode( 1) - dx*dr3m + fqx ! fx
+ fnode( 2) = fnode( 2) - dy*dr3m + fqy ! fy
+ fnode( 3) = fnode( 3) - dz*dr3m + fqz ! fz
+ fnode( 4) = fnode( 4) + dr3m*(3.*rx*rx - 1.) + dfxdxq ! dfx/dx
+ fnode( 5) = fnode( 5) + dr3m*(3.*rx*ry)      + dfxdyq ! dfx/dy = dfy/dx
+ fnode( 6) = fnode( 6) + dr3m*(3.*rx*rz)      + dfxdzq ! dfx/dz = dfz/dx
+ fnode( 7) = fnode( 7) + dr3m*(3.*ry*ry - 1.) + dfydyq ! dfy/dy
+ fnode( 8) = fnode( 8) + dr3m*(3.*ry*rz)      + dfydzq ! dfy/dz = dfz/dy
+ fnode( 9) = fnode( 9) + dr3m*(3.*rz*rz - 1.) + dfzdzq ! dfz/dz
  fnode(10) = fnode(10) - dr4m3*(5.*rx*rx*rx - 3.*rx) + d2fxxxq ! d2fxdxdx
  fnode(11) = fnode(11) - dr4m3*(5.*rx*rx*ry - ry)    + d2fxxyq ! d2fxdxdy
  fnode(12) = fnode(12) - dr4m3*(5.*rx*rx*rz - rz)    + d2fxxzq ! d2fxdxdz
