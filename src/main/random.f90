50e7fdcf12e27288fbb0b2e3f80a4b32d4cb8ba2
Daniel Price <daniel.price@monash.edu>
2018-02-21 00:06:28 +1100

    [format-bot] end if -> endif; end do -> enddo

--- src/main/random.f90
+++ src/main/random.f90
@@ -99,18 +99,18 @@ function get_random ( s1, s2 )
   s1 = 40014 * ( s1 - k * 53668 ) - k * 12211
   if ( s1 < 0 ) then
     s1 = s1 + 2147483563
-  end if
+  endif
 
   k = s2 / 52774
   s2 = 40692 * ( s2 - k * 52774 ) - k * 3791
   if ( s2 < 0 ) then
     s2 = s2 + 2147483399
-  end if
+  endif
 
   z = s1 - s2
   if ( z < 1 ) then
     z = z + 2147483562
-  end if
+  endif
 
   get_random = real ( z ) / 2147483563.0D+00
 
