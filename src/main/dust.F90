f39793f40d3b165dfe0b7970dc59fe859329adc3
Daniel Price <daniel.price@monash.edu>
2018-03-05 08:25:45 +1100

    (dust) revert f0a1825 due to test failures

--- src/main/dust.F90
+++ src/main/dust.F90
@@ -200,7 +200,8 @@ subroutine get_ts(idrag,sgrain,densgrain,rhogas,rhodust,spsoundgas,dv2, &
  f          = 0.
  ts1        = 0.
  ts         = 0.
- rhosum     = rhogas ! this is an approx. to allow calculations to proceed
+ rhosum     = rhogas + rhodust
+ ! rhosum     = rhogas ! this is an approx. to allow calculations to proceed
  ! efficiently in case of numerical dust trapping
  ! should be rhosum =  rhogas + rhodust
  ! however, without full dust grain population rhodust is an
