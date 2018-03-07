18282636ce4f4c5ad7071f100c4373e9b79d9397
Terrence Tricco <ttricco@cita.utoronto.ca>
2018-03-07 13:08:38 -0500

    (kh test) Updated defaults for Kelvin-Helmholtz test setup

--- src/setup/setup_kh.f90
+++ src/setup/setup_kh.f90
@@ -40,12 +40,14 @@ contains
 subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,time,fileprefix)
  use setup_params, only:npart_total
  use io,           only:master
+ use options,      only:nfulldump
  use unifdis,      only:set_unifdis
  use boundary,     only:set_boundary,xmin,ymin,zmin,xmax,ymax,zmax,dxbound,dzbound
  use mpiutils,     only:bcast_mpi
  use part,         only:igas
  use prompting,    only:prompt
  use physcon,      only:pi
+ use timestep,     only:dtmax,tmax
  integer,           intent(in)    :: id
  integer,           intent(inout) :: npart
  integer,           intent(out)   :: npartoftype(:)
@@ -55,13 +57,22 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  real,              intent(inout) :: time
  character(len=20), intent(in)    :: fileprefix
  real,              intent(out)   :: vxyzu(:,:)
- real :: totmass,deltax,przero,v1,v2
+ character(len=26)                :: filename
+ logical :: iexist
  integer :: i,maxp,maxvxyzu,npartx
+ real :: totmass,deltax,przero,v1,v2
 !
 !--general parameters
 !
  time = 0.
  gamma = 5./3
+ filename= trim(fileprefix)//'.in'
+ inquire(file=filename,exist=iexist)
+ if (.not. iexist) then
+    tmax      = 2.00
+    dtmax     = 0.1
+    nfulldump = 1
+ endif
 !
 !--set particles
 !
