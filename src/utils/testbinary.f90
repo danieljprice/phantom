10f878a664d6eb4a5fdbc7141505bf851a7ebaa1
Daniel Price <daniel.price@monash.edu>
2018-04-06 14:09:24 +1000

    (testbin) utility for plotting binary orbits added, as used in HD142527 paper; deleted obsolete memcheck and tracer utilities

---
+++ src/utils/testbinary.f90
@@ -0,0 +1,31 @@
+program testbin
+ use testbinary, only:test_binary
+ use prompting,  only:prompt
+ use datafiles,  only:find_phantom_datafile
+ implicit none
+ integer :: j,ierr,ierr1,itex
+ real :: a,e,inc,o,w,f,m1,m2
+ character(len=120) :: filename
+
+ m1 = 1.8
+ m2 = 0.4
+ call prompt('enter primary mass',m1)
+ call prompt('enter secondary mass',m2)
+ itex = 0
+
+ filename = find_phantom_datafile('orbits.txt','orbits')
+ open(unit=2,file=filename,status='old',iostat=ierr)
+ if (ierr==0) then
+    open(newunit=itex,file='orbits.tex',status='replace',iostat=ierr1)
+    print "(a)",' writing to orbits.tex'
+ endif
+ j = 0
+ do while(ierr==0)
+    j = j + 1
+    read(2,*,iostat=ierr) a,e,inc,o,w,f
+    if (ierr==0) call test_binary(m1,m2,a,e,inc,o,w,f,j,itex)
+ enddo
+ close(2)
+ if (itex > 0) close(itex)
+
+end program testbin
