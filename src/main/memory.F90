76543ba3ae3c8670c9d9a58bcc521f6e44aa4bcd
Unknown <conrad.chan@monash.edu>
2018-04-13 10:31:26 +1000

    (dynamicmem) migrate common variables

--- src/main/memory.F90
+++ src/main/memory.F90
@@ -8,12 +8,52 @@ contains
 
 subroutine allocate_memory
  use dim,   only:maxp
+
  use part,  only:xyzh
+ use part,  only:xyzh_soa
+
+ use dim,   only:maxvxyzu
+ use part,  only:vxyzu
+
+ use dim,   only:nalpha, maxalpha
+ use part,  only:alphaind
+
+ use dim,   only:ndivcurlv
+ use part,  only:divcurlv
+
+ use dim,   only:ndivcurlB
+ use part,  only:divcurlB
+
+ use dim,   only:maxBevol,maxmhd
+ use part,  only:Bevol
+
+ use part,  only:Bxyz
 
  integer :: allocstat
 
  allocate(xyzh(4, maxp), stat = allocstat)
  if (allocstat /= 0) call fatal('memory','xyzh allocation error')
+
+ allocate(xyzh_soa(maxp, 4), stat = allocstat)
+ if (allocstat /= 0) call fatal('memory','xyzh_soa allocation error')
+
+ allocate(vxyzu(maxvxyzu,maxp), stat = allocstat)
+ if (allocstat /= 0) call fatal('memory','vxyzu allocation error')
+
+ allocate(alphaind(nalpha,maxalpha), stat = allocstat)
+ if (allocstat /= 0) call fatal('memory','alphaind allocation error')
+
+ allocate(divcurlv(ndivcurlv,maxp), stat = allocstat)
+ if (allocstat /= 0) call fatal('memory','divcurlv allocation error')
+
+ allocate(divcurlB(ndivcurlB,maxp), stat = allocstat)
+ if (allocstat /= 0) call fatal('memory','divcurlB allocation error')
+
+ allocate(Bevol(maxBevol,maxmhd), stat = allocstat)
+ if (allocstat /= 0) call fatal('memory','Bevol allocation error')
+
+ allocate(Bxyz(3,maxmhd), stat = allocstat)
+ if (allocstat /= 0) call fatal('memory','Bxyz allocation error')
 
 end subroutine allocate_memory
 
