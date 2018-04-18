0d80daa9be40bfab09d85dbfed20a2da231d3e40
David Liptai <david.liptai@monash.edu>
2018-04-18 15:32:12 +1000

    (GR) setup test particles gives you choice of circular orbit, precession, or custom

--- src/setup/setup_testparticles.F90
+++ src/setup/setup_testparticles.F90
@@ -41,29 +41,24 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  real,              intent(inout) :: time
  character(len=20), intent(in)    :: fileprefix
  real,              intent(out)   :: vxyzu(:,:)
- integer :: i
- real    :: x0,y0,z0,vx0,vy0,vz0,dr,h0,xyz0(3),rhat(3),r2,vcirc,rtan(3)
+ integer :: i,dumpsperorbit,orbtype
+ real    :: x0,y0,z0,vx0,vy0,vz0,dr,h0,xyz0(3),rhat(3),r2,vcirc,rtan(3),norbits,period,r0
 
  call set_units(mass=solarm,G=1.d0,c=1.d0)
 
  !
  ! general parameters
  !
-
  time  = 0.
- tmax  = 500.
- dtmax = tmax/1000
  gamma = 1.
  polyk = 0.
  npart = 10
  ieos  = 11
  nfulldump = 1
-
- alpha    = 0.    ! art. viscosity parameter
- alphamax = 0.
- alphau   = 0.    ! art. conductivity parameter
- beta     = 0.    ! beta viscosity
-
+ alpha     = 0.
+ alphamax  = 0.
+ alphau    = 0.
+ beta      = 0.
  massoftype     = 1.e-10
  npartoftype(:) = 0
  npartoftype(1) = npart
@@ -74,22 +69,57 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  iexternalforce = iext_star
 #endif
 
+ xyzh  = 0.
+ vxyzu = 0.
 
- x0 = 10.
- y0 = 0.
- z0 = 0.
+ x0    = 0.
+ y0    = 0.
+ z0    = 0.
+ vx0   = 0.
+ vy0   = 0.
+ vz0   = 0.
 
- vx0 = 0.
- vy0 = sqrt(1./x0)
- vz0 = 0.
+ orbtype       = 1
+ norbits       = 1.
+ dumpsperorbit = 100
+ period        = 0.
 
- call prompt('initial x position',x0)
- call prompt('initial y position',y0)
- call prompt('initial z position',z0)
+ call prompt('select orbit type (1=cirlce, 2=precession, 0=custom)',orbtype,1,3)
+ select case(orbtype)
 
- call prompt('initial vx velocity',vx0)
- call prompt('initial vy velocity',vy0)
- call prompt('initial vz velocity',vz0)
+ case(1) ! circular
+    x0  = 10.
+    call prompt('initial radius r0',x0)
+    vy0 = sqrt(1./x0)
+    period = 2.*pi*sqrt(x0**3/1.)
+
+ case(2) ! precession
+    x0     = 90.
+    vy0    = 0.0521157
+    period = 2.*pi*sqrt((0.5*x0)**3/1.) ! approximate
+
+ case(0) ! custom : if you just press enter a lot it gives you a circular orbit
+    x0  = 10.
+    vy0 = sqrt(1./x0)
+    call prompt('initial x position',x0)
+    call prompt('initial y position',y0)
+    call prompt('initial z position',z0)
+    call prompt('initial vx velocity',vx0)
+    call prompt('initial vy velocity',vy0)
+    call prompt('initial vz velocity',vz0)
+    tmax  = 500.
+    dtmax = tmax/1000.
+    call prompt('enter tmax',tmax)
+    call prompt('enter dtmax',dtmax)
+
+ end select
+
+ if (orbtype/=0) then
+    call prompt('number of orbits',norbits)
+    call prompt('dumps per orbit',dumpsperorbit)
+    tmax   = norbits*period
+    dtmax  = period/dumpsperorbit
+ endif
 
  print*,''
  print*,' Setup for single test particle: '
@@ -98,29 +128,25 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  print*,' Initial (vx,vy,vz) = ',vx0,vy0,vz0
  print*,''
 
- dr = 0.25
- h0 = 10.*dr
+ xyzh(1:3,1)   = (/x0,y0,z0/)
+ vxyzu(1:3,1)  = (/vx0,vy0,vz0/)
 
- xyzh = 0.
- vxyzu = 0.
- xyzh(4,:) = h0
- vxyzu(4,:) = 0.
-
- xyz0 = (/x0,y0,z0/)
- rhat = xyz0/sqrt(dot_product(xyz0,xyz0))
-
- xyzh(1:3,1) = xyz0
- vxyzu(1:3,1) = (/vx0,vy0,vz0/)
-
- !--- Put all other particles in a radial line outwards from the origin, with their circular velocity
+ !
+ ! Put all other particles in a radial line outwards from the origin, with their circular velocity,
+ ! set smoothing lengths, and set thermal energies
+ !
+ xyz0          = xyzh(1:3,1)
+ r0            = sqrt(dot_product(xyz0,xyz0))
+ rhat          = xyz0/r0
+ dr            = 0.25
+ h0            = 10.*dr
+ xyzh(4,:)     = h0
+ vxyzu(4,:)    = 0.
  do i=2,npart
-    xyzh(1:3,i)    = xyz0 + (i-1)*dr*rhat
-
-    !--- Unit vector tangential to motion
-    call cross_product3D((/0.,0.,1./),xyzh(1:3,i),rtan)
-    rtan = rtan/sqrt(dot_product(rtan,rtan))
-
-    r2           = xyzh(1,i)**2 + xyzh(2,i)**2 + xyzh(3,i)**2
+    xyzh(1:3,i) = xyz0 + (i-1)*dr*rhat
+    call cross_product3D((/0.,0.,1./),xyzh(1:3,i),rtan)          ! Unit vector tangential to motion
+    rtan  = rtan/sqrt(dot_product(rtan,rtan))
+    r2    = xyzh(1,i)**2 + xyzh(2,i)**2 + xyzh(3,i)**2
     vcirc = sqrt(1./sqrt(r2))
     vxyzu(1:3,i) = rtan*vcirc
  enddo
