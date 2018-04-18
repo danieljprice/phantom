4ecc8a92aa3cb2f1a0bb6a7ba5c379f0f813ce94
David Liptai <david.liptai@monash.edu>
2018-04-18 17:45:56 +1000

    (GR) read runtime parameters from setup file

--- src/setup/setup_grdisc.F90
+++ src/setup/setup_grdisc.F90
@@ -27,6 +27,9 @@ module setup
  implicit none
  public :: setpart
 
+ real,    private :: mhole,mdisc,r_in,r_out,spin,honr,theta
+ integer, private :: np
+
  private
 
 contains
@@ -63,7 +66,10 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  real,              intent(out)   :: massoftype(:)
  real,              intent(inout) :: time
  character(len=20), intent(in)    :: fileprefix
- real    :: r_in,r_out,honr,theta,mhole,mdisc,spin
+ character(len=120) :: filename
+ integer :: ierr
+ logical :: iexist
+
 
  time            = 0.
  alphau          = 0.0
@@ -82,39 +88,55 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  tmax  = 1000.
  dtmax = 10.
 
- mhole = 1.e6*solarm
- call set_units(G=1.,c=1.,mass=mhole)
+!
+! Set default problem parameters
+!
+ mhole   = 1.e6    ! (solarm)
+ mdisc   = 10.     ! (solarm)
+ r_in    = 40.     ! (GM/c^2)
+ r_out   = 160.    ! (GM/c^2)
+ spin    = 0.
+ honr    = 0.02
+ theta   = 0.      ! inclination angle (degrees)
+ np      = 1e5
 
- mdisc           = 10.*solarm/umass
+ !
+ !-- Read runtime parameters from setup file
+ !
+ if (id==master) print "(/,65('-'),1(/,a),/,65('-'),/)",' Tidal disruption in GR'
+ filename = trim(fileprefix)//'.setup'
+ inquire(file=filename,exist=iexist)
+ if (iexist) call read_setupfile(filename,ierr)
+ if (.not. iexist .or. ierr /= 0) then
+  if (id==master) then
+     call write_setupfile(filename)
+     print*,' Edit '//trim(filename)//' and rerun phantomsetup'
+  endif
+  stop
+ endif
+
+ npart = np
+
+!
+! Convert to code units
+!
+ mhole = mhole*solarm
+ call set_units(G=1.,c=1.,mass=mhole) ! Set central mass to M=1 in code units
+ mdisc           = mdisc*solarm/umass
  accradius1      = 4.
  accradius1_hard = accradius1
 
 !
-! Set problem parameters (defaults)
+! Convert to radians
 !
- npart   = 1e5
- r_in    = 40.
- r_out   = 160.
- spin    = 0.
- theta   = 0.      ! inclination angle (degrees)
- honr    = 0.02
-
- call prompt('Enter number of particles ',npart)
- call prompt('Enter H on R ',honr)
- call prompt('Enter spin of black hole ',spin,-1.,1.)
- call prompt('Enter inner radius of disc ',r_in,2.)
- call prompt('Enter outer radius of disc ',r_out,r_in)
- call prompt('Enter inclination angle (degrees) ',theta,0.,90.)
+ theta = theta/180. * pi
 
  npartoftype(1)  = npart
-
 #ifdef GR
  a = spin
 #else
  blackhole_spin = spin
 #endif
-
- theta = theta/180. * pi ! convert to radians
 
  call set_disc(id,master,&
                npart         = npart,                &
@@ -143,5 +165,58 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
 
  return
 end subroutine setpart
+
+
+!
+!---Read/write setup file--------------------------------------------------
+!
+subroutine write_setupfile(filename)
+ use infile_utils, only:write_inopt
+ character(len=*), intent(in) :: filename
+ integer, parameter :: iunit = 20
+
+ print "(a)",' writing setup options file '//trim(filename)
+ open(unit=iunit,file=filename,status='replace',form='formatted')
+ write(iunit,"(a)") '# input file for binary setup routines'
+ call write_inopt(mhole, 'mhole', 'mass of black hole (solar mass)'        , iunit)
+ call write_inopt(mdisc, 'mdisc', 'mass of disc       (solar mass)'        , iunit)
+ call write_inopt(r_in , 'r_in' , 'inner edge of disc (GM/c^2, code units)', iunit)
+ call write_inopt(r_out, 'r_out', 'outer edge of disc (GM/c^2, code units)', iunit)
+ call write_inopt(spin , 'spin' , 'spin parameter of black hole |a|<1'     , iunit)
+ call write_inopt(honr , 'honr' , 'scale height H/R for disc'              , iunit)
+ call write_inopt(theta, 'theta', 'inclination of disc (degrees)'          , iunit)
+ call write_inopt(np   , 'np   ', 'number of particles in disc'            , iunit)
+ close(iunit)
+
+end subroutine write_setupfile
+
+subroutine read_setupfile(filename,ierr)
+ use infile_utils, only:open_db_from_file,inopts,read_inopt,close_db
+ use io,           only:error
+ character(len=*), intent(in)  :: filename
+ integer,          intent(out) :: ierr
+ integer, parameter :: iunit = 21
+ integer :: nerr
+ type(inopts), allocatable :: db(:)
+
+ print "(a)",'reading setup options from '//trim(filename)
+ nerr = 0
+ ierr = 0
+ call open_db_from_file(db,filename,iunit,ierr)
+ call read_inopt(mhole,'mhole', db,min=0.,errcount=nerr)
+ call read_inopt(mdisc,'mdisc', db,min=0.,errcount=nerr)
+ call read_inopt(r_in ,'r_in' , db,min=0.,errcount=nerr)
+ call read_inopt(r_out,'r_out', db,min=0.,errcount=nerr)
+ call read_inopt(spin ,'spin' , db,min=0.,errcount=nerr)
+ call read_inopt(honr ,'honr' , db,min=0.,errcount=nerr)
+ call read_inopt(theta,'theta', db,min=0.,errcount=nerr)
+ call read_inopt(np   ,'np   ', db,min=0 ,errcount=nerr)
+ call close_db(db)
+ if (nerr > 0) then
+    print "(1x,i2,a)",nerr,' error(s) during read of setup file: re-writing...'
+    ierr = nerr
+ endif
+
+end subroutine read_setupfile
 
 end module setup
