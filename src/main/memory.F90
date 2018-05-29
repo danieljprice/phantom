64c7d6cd71a341850efc23b62ec2808be3ef9f4f
Unknown <conrad.chan@monash.edu>
2018-05-29 15:18:28 +0800

    (dynamicmem) add datatype for kdnode

--- src/main/memory.F90
+++ src/main/memory.F90
@@ -1,5 +1,6 @@
 module memory
-use io,  only:fatal,error,iprint
+use io,           only:fatal,error,iprint
+use dtypekdtree,  only:kdnode,kdnode_bytes
 
  implicit none
 
@@ -20,7 +21,8 @@ use io,  only:fatal,error,iprint
       allocate_array_integer4_3d, &
       allocate_array_integer1_1d, &
       allocate_array_integer1_2d, &
-      allocate_array_integer1_3d
+      allocate_array_integer1_3d, &
+      allocate_array_kdnode_1d
  end interface
 
 contains
@@ -157,6 +159,17 @@ contains
     call print_allocation_stats(name, (/n1, n2, n3/), 'integer(1)')
  end subroutine allocate_array_integer1_3d
 
+ subroutine allocate_array_kdnode_1d(name, x, n1)
+    character(len=*),               intent(in)     :: name
+    type(kdnode), allocatable,      intent(inout)  :: x(:)
+    integer,                        intent(in)     :: n1
+    integer                                        :: allocstat
+
+    allocate(x(n1), stat = allocstat)
+    call check_allocate(name, allocstat)
+    call print_allocation_stats(name, (/n1/), 'kdnode')
+ end subroutine allocate_array_kdnode_1d
+
  subroutine check_allocate(name, allocstat)
     character(len=*),   intent(in) :: name
     integer,            intent(in) :: allocstat
@@ -170,7 +183,7 @@ contains
     character(len=*),   intent(in) :: type
     character(len=10)              :: number
     character(len=14)              :: dimstring
-    character(len=10)               :: sizestring
+    character(len=10)              :: sizestring
     integer                        :: i
     real                           :: nbytes
     integer                        :: databytes
@@ -178,12 +191,14 @@ contains
     databytes = 0
     if (type == 'real(8)') then
        databytes = 8
-    else if (type == 'real(4)') then
+    elseif (type == 'real(4)') then
        databytes = 4
-    else if (type == 'integer(4)') then
+    elseif (type == 'integer(4)') then
        databytes = 4
-    else if (type == 'integer(1)') then
+    elseif (type == 'integer(1)') then
        databytes = 1
+    elseif (type == 'kdnode') then
+      databytes = kdnode_bytes
     else
        call fatal('memory', 'invalid data type chosen for memory allocation')
     endif
