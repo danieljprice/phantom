33e0b26654614928641c634234522618a0b51253
Daniel Price <daniel.price@monash.edu>
2018-06-20 14:40:33 +0200

    (showheader) now also works for small dumps

--- src/utils/showheader.f90
+++ src/utils/showheader.f90
@@ -21,7 +21,8 @@
 !+
 !--------------------------------------------------------------------------
 program showheader
- use dump_utils, only:open_dumpfile_r,read_header,dump_h,lenid,print_header,free_header
+ use dump_utils, only:open_dumpfile_r,read_header,dump_h,lenid,&
+                      print_header,free_header,ierr_realsize
  implicit none
  integer, parameter   :: iu = 23
  integer              :: nargs,ierr,i
@@ -43,14 +44,23 @@ program showheader
     !
     call open_dumpfile_r(iu,dumpfile,fileid,ierr)
     if (ierr /= 0) then
-       print "(a)",' ERROR opening '//trim(dumpfile)
+       if (ierr == ierr_realsize) then
+          ! try to open it as a small dump
+          close(iu)
+          call open_dumpfile_r(iu,dumpfile,fileid,ierr,singleprec=.true.)
+          if (ierr == 0) call read_header(iu,hdr,.true.,ierr,singleprec=.true.)
+       else
+          print "(a)",' ERROR opening '//trim(dumpfile)
+       endif
     else
-       if (nargs > 1) print "(/,':: ',a,' ::',/)",trim(dumpfile)
-       print "(a)",trim(fileid)
        !
        ! read and print the file header
        !
        call read_header(iu,hdr,.true.,ierr)
+    endif
+    if (ierr == 0) then
+       if (nargs > 1) print "(/,':: ',a,' ::',/)",trim(dumpfile)
+       print "(a)",trim(fileid)
        call print_header(hdr)
        call free_header(hdr,ierr)
        close(iu)
