8ce42cace6e39761155f81315ad02f60768ce06a
Hauke Worpel <hworpel@aip.de>
2018-06-26 15:03:31 +0200

    Self-consistent injection stream velocity, internal energy

--- src/main/inject_rochelobe.f90
+++ src/main/inject_rochelobe.f90
@@ -34,9 +34,9 @@ module inject
 
  real, private :: Mdot = 0.
  real, private :: Mdotcode = 0.
- real, private :: spd_inject = 0.
  real, private :: chi = 0.001
  real, private :: dNdt = 100.
+ real, private :: gastemp = 3000.
 
 contains
 
@@ -51,15 +51,17 @@ subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass, &
  use part,      only:nptmass,massoftype,igas,hfact
  use partinject,only:add_or_update_particle
  use setbinary, only:L1_point
- use physcon,   only:pi,twopi,solarm,years,gg
+ use physcon,   only:pi,twopi,solarm,years,gg,kboltz,mass_proton_cgs
  use random,    only:ran2, rayleigh_deviate
  use units,     only:udist, umass, utime
+ use eos,       only:gmw
  real,    intent(in)    :: time, dtlast
  real,    intent(inout) :: xyzh(:,:), vxyzu(:,:), xyzmh_ptmass(:,:), vxyz_ptmass(:,:)
  integer, intent(inout) :: npart
  integer, intent(inout) :: npartoftype(:)
- real :: m1,m2,q,radL1,h,u,theta_s,A,mu,theta_rand,r_rand,dNdt_code,Porb,r12
- real :: xyzL1(3),xyzi(3),vxyz(3),dr(3),x1(3),x2(3),x0(3),dxyz(3),vxyzL1(3),v1(3),v2(3)
+ real :: m1,m2,q,radL1,h,u,theta_s,A,mu,theta_rand,r_rand,dNdt_code,Porb,r12,r2L1
+ real :: eps, spd_inject
+ real :: xyzL1(3),xyzi(3),vxyz(3),dr(3),x1(3),x2(3),x0(3),dxyz(3),vxyzL1(3),v1(3),v2(3),xyzinj(3)
  integer :: i_part,part_type,s1,wall_i,particles_to_place
 
 !
@@ -72,22 +74,32 @@ subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass, &
  v1 = vxyz_ptmass(1:3,1)
  v2 = vxyz_ptmass(1:3,1)
  dr = x2 - x1
- r12 = sqrt(dot_product(dr,dr))
+ r12 = dist(x2,x1)
  m1 = xyzmh_ptmass(4,1)
  m2 = xyzmh_ptmass(4,2)
  q  = m2/m1
  mu = 1./(1 + q)
+ Porb      = twopi * sqrt( (r12*udist)**3 / (gg*(m1+m2)*umass) )
+ eps = Porb/(twopi*r12) * (gastemp*kboltz/gmw)**0.5*utime/udist
  radL1      = L1_point(m1/m2)                     ! find L1 point given binary mass ratio
  A  = mu / abs(radL1 - 1. + mu)**3 + (1. - mu)/abs(radL1 + mu)**3! See Lubow & Shu 1975
  theta_s = -acos( -4./(3.*A)+(1-8./(9.*A))**0.5)/2.              ! See Lubow & Shu 1975
  xyzL1(1:3) = xyzmh_ptmass(1:3,1) + radL1*dr(:)   ! set as vector position
- vxyzL1 = v1*sqrt( dot_product(xyzL1-x0, xyzL1-x0)/dot_product(x1-x0, x1-x0) ) ! orbital motion of L1 point
+ r2L1 = dist(xyzL1, x2)
+ xyzinj(1:3) = xyzL1 + (/cos(theta_s),sin(theta_s),0.0/)*r2L1*eps*50.0     !do something about this "magic number"; can wait
+ vxyzL1 = v1*dist(xyzL1,x0)/dist(x0, x1) ! orbital motion of L1 point
+ spd_inject = abs((3.*A)/(4*eps)*dist(xyzinj,xyzL1)*sin(2*theta_s))
  !unclear if this is OK with eccentric orbits, but if you have Roche Lobe overflow, orbits should be
  !circularised anyway
 
+ part_type = igas
+ vxyz = (/ cos(theta_s), sin(theta_s), 0.0 /)*spd_inject
+ h = hfact*chi/udist
+ u = 3.*(kboltz*gastemp/(mu*mass_proton_cgs))/2. * (utime/udist)**2
+
  ! mass of gas particles is set by mass accretion rate and particle injection rate
  Mdotcode  = Mdot*(solarm/years)/(umass/utime)
- Porb      = twopi * sqrt( (r12*udist)**3 / (gg*(m1+m2)*umass) )
+
  dNdt_code = dNdt*utime / Porb
  massoftype(igas) = Mdotcode/dNdt_code
 
@@ -108,11 +120,6 @@ subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass, &
     dxyz=(/0.0, cos(theta_rand), sin(theta_rand)/)*r_rand   ! Stream is placed randomly in a cylinder
     ! with a Gaussian density distribution
 
-    ! prepare to add a particle
-    part_type = igas
-    vxyz = (/ cos(theta_s), sin(theta_s), 0.0 /)*spd_inject
-    h = hfact*chi/udist
-    u = 0.0   !get back to this
     i_part = npart + 1
     call rotate_into_plane(dxyz,vxyz,x2-xyzL1)
     vxyz = vxyz + vxyzL1
@@ -124,6 +131,18 @@ subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass, &
  enddo
 
 end subroutine inject_particles
+
+!
+! Function to get the distance between two points
+!
+real function dist(x1,x2)
+ real, intent(in)  :: x1(3), x2(3)
+ real :: dr(3)
+
+ dr = x1 - x2
+ dist = sqrt(dot_product(dr, dr))
+ return
+end function dist
 
 subroutine get_v_spherical(r1,v1,vr,vphi,vtheta)
  real, intent(in)  :: r1(3),v1(3)
@@ -188,8 +207,8 @@ subroutine write_options_inject(iunit)
 
  call write_inopt(Mdot,'Mdot','mass injection rate at L1, in Msun/yr',iunit)
  call write_inopt(dNdt,'dNdt','particle injection rate in particles/binary orbit',iunit)
- call write_inopt(spd_inject,'spd_inject','speed of injection at L1 in cm/s',iunit)
  call write_inopt(chi,'chi','width of injection stream in cm',iunit)
+ call write_inopt(gastemp, 'gastemp', 'Temperature at injection point in K',iunit)
 
 end subroutine write_options_inject
 
@@ -223,13 +242,14 @@ subroutine read_options_inject(name,valstring,imatch,igotall,ierr)
     read(valstring,*,iostat=ierr) dNdt
     ngot = ngot + 1
     if (dNdt < 0.) call fatal(label,'dNdt < 0 in input options')
- case('spd_inject')
-    read(valstring,*,iostat=ierr) spd_inject
-    ngot = ngot + 1
  case('chi')
     read(valstring,*,iostat=ierr) chi
     ngot = ngot + 1
     if (dNdt <= 0.) call fatal(label,'chi <= 0 in input options')
+ case('gastemp')
+    read(valstring,*,iostat=ierr) gastemp
+    ngot = ngot + 1
+    if (gastemp<= 0.) call fatal(label,'gastemp nonpositive in input options')
  case default
     imatch = .false.
  end select
