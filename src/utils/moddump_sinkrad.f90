757549dc934954aa0dcb8de6ebcd9928dc35e5b1
Enrico Ragusa <enrico.ragusa@unimi.it>
2018-07-03 15:31:59 +0200

    Added moddump to modify sink radius.

---
+++ src/utils/moddump_sinkrad.f90
@@ -0,0 +1,32 @@
+module moddump
+ 
+ use part, only:xyzmh_ptmass
+ 
+ implicit none
+
+contains
+
+subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
+ implicit none
+ integer, intent(inout) :: npart
+ integer, dimension(:), intent(inout) :: npartoftype
+ real, dimension(:), intent(inout) :: massoftype
+ real, dimension(:,:), intent(inout) :: xyzh,vxyzu
+ !integer :: i
+ real :: radius
+ 
+ print*,' Phantommoddump: Change sink radii '
+
+ radius=0.1
+ !
+ !--------Changing sink radius
+ !
+ print*,'Changing sink radii to',radius,' ...' 
+ !xyzmh_ptmass(5,1)=radius
+ xyzmh_ptmass(5,2)=radius
+
+ return
+end subroutine modify_dump
+
+end module moddump
+
