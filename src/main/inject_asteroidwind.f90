8c4d3ccd8b7302648a533d242c062e8229565cde
David Liptai <david.liptai@monash.edu>
2018-07-05 14:23:54 +0100

    (asteroid wind) added setup for particle injection from asteroid orbiting white dwarf

---
+++ src/main/inject_asteroidwind.f90
@@ -0,0 +1,134 @@
+module inject
+ implicit none
+ character(len=*), parameter, public :: inject_type = 'asteroidwind'
+
+ public :: inject_particles,write_options_inject,read_options_inject
+
+ private
+
+ real :: mdot          = 5.e8       ! mass injection rate in grams/second
+ real :: npartperorbit = 100.       ! particle injection rate in particles per orbit
+ real :: gastemp       = 3000.      ! gas temperature
+
+contains
+
+subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npart,npartoftype)
+ use io,        only:fatal
+ use part,      only:nptmass,massoftype,igas,hfact,ihsoft
+ use partinject,only:add_or_update_particle
+ use physcon,   only:pi,twopi,gg,kboltz,mass_proton_cgs
+ use random,    only:ran2
+ use units,     only:udist, umass, utime
+ real,    intent(in)    :: time, dtlast
+ real,    intent(inout) :: xyzh(:,:), vxyzu(:,:), xyzmh_ptmass(:,:), vxyz_ptmass(:,:)
+ integer, intent(inout) :: npart
+ integer, intent(inout) :: npartoftype(:)
+ real,    dimension(3)  :: xyz,vxyz,r1,r2,v2,vhat
+ integer :: i,ipart,npinject,seed
+ real    :: dmdt,dndt,rasteroid,h,u,speed
+ real    :: m1,m2,mu,period,r,q
+ real    :: phi,theta
+
+ if (nptmass < 2) call fatal('inject_rochelobe','not enough point masses for asteroid wind injection')
+ if (nptmass > 2) call fatal('inject_rochelobe','too many point masses for asteroid wind injection')
+
+ r1        = xyzmh_ptmass(1:3,1)
+ r2        = xyzmh_ptmass(1:3,2)
+ rasteroid = xyzmh_ptmass(ihsoft,2)
+ m1        = xyzmh_ptmass(4,1)
+ m2        = xyzmh_ptmass(4,2)
+ v2        = vxyz_ptmass(1:3,2)
+
+ speed     = sqrt(dot_product(v2,v2))
+ vhat      = v2/speed
+
+ r         = sqrt(dot_product(r1-r2,r1-r2))
+ q         = m2/m1
+ mu        = 1./(1 + q)
+ period    = twopi*sqrt((r*udist)**3/(gg*(m1+m2)*umass))       ! period of orbit in code units
+
+ dmdt      = mdot/(umass/utime)                                ! convert grams/sec to code units
+ dndt      = npartperorbit*utime/period                        ! convert particles per orbit into code units
+
+!
+!-- Mass of gas particles is set by mass accretion rate and particle injection rate
+!
+ massoftype(igas) = dmdt/dndt
+
+!
+!-- How many particles do we need to inject?
+!   (Seems to need at least eight gas particles to not crash) <-- This statement may or may not be true...
+!
+ if(npartoftype(igas)<8) then
+    npinject = 8-npartoftype(igas)
+ else
+    npinject = max(0, int(0.5 + (time*dmdt/massoftype(igas)) - npartoftype(igas) ))
+ endif
+
+!
+!-- Randomly inject particles around the asteroids outer 'radius'
+!
+ do i=1,npinject
+    phi       = ran2(seed)*twopi
+    theta     = ran2(seed)*pi
+    xyz       = r2 + (/rasteroid*cos(phi)*sin(theta),rasteroid*sin(phi)*sin(theta),rasteroid*cos(theta)/)
+    vxyz      = 0.999*speed*vhat
+    u         = 3.*(kboltz*gastemp/(mu*mass_proton_cgs))/2. * (utime/udist)**2
+    h         = hfact*(rasteroid/2.)
+    ipart     = npart + 1
+    call add_or_update_particle(igas,xyz,vxyz,h,u,ipart,npart,npartoftype,xyzh,vxyzu)
+ enddo
+
+end subroutine inject_particles
+
+!-----------------------------------------------------------------------
+!+
+!  Writes input options to the input file.
+!+
+!-----------------------------------------------------------------------
+subroutine write_options_inject(iunit)
+ use infile_utils, only:write_inopt
+ integer, intent(in) :: iunit
+
+ call write_inopt(mdot         ,'mdot'         ,'mass injection rate in grams/second'              ,iunit)
+ call write_inopt(npartperorbit,'npartperorbit','particle injection rate in particles/binary orbit',iunit)
+ call write_inopt(gastemp      ,'gastemp'      ,'temperature at injection point in K'              ,iunit)
+
+end subroutine write_options_inject
+
+!-----------------------------------------------------------------------
+!+
+!  Reads input options from the input file.
+!+
+!-----------------------------------------------------------------------
+subroutine read_options_inject(name,valstring,imatch,igotall,ierr)
+ use io, only:fatal
+ character(len=*), intent(in)  :: name,valstring
+ logical,          intent(out) :: imatch,igotall
+ integer,          intent(out) :: ierr
+ integer, save :: ngot = 0
+ character(len=30), parameter :: label = 'read_options_inject'
+
+ imatch  = .true.
+ select case(trim(name))
+ case('mdot')
+    read(valstring,*,iostat=ierr) mdot
+    ngot = ngot + 1
+    if (mdot  <  0.) call fatal(label,'mdot < 0 in input options')
+case('npartperorbit')
+    read(valstring,*,iostat=ierr) npartperorbit
+    ngot = ngot + 1
+    if (npartperorbit < 0.) call fatal(label,'npartperorbit < 0 in input options')
+ case('gastemp')
+    read(valstring,*,iostat=ierr) gastemp
+    ngot = ngot + 1
+    if (gastemp<= 0.) call fatal(label,'gastemp 0 or negative in input options')
+ case default
+    imatch = .false.
+ end select
+
+ igotall = (ngot >= 1)
+
+end subroutine read_options_inject
+
+end module inject
---
+++ src/setup/setup_asteroidwind.f90
@@ -0,0 +1,164 @@
+module setup
+ implicit none
+ public :: setpart
+
+ real :: m1,m2,ecc,semia,hacc1,rasteroid,norbits
+ integer :: dumpsperorbit
+
+ private
+
+contains
+
+subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,time,fileprefix)
+ use part,      only:nptmass,xyzmh_ptmass,vxyz_ptmass,ihacc,ihsoft,idust,set_particle_type,ihsoft
+ use setbinary, only:set_binary,get_a_from_period
+ use spherical, only:set_sphere
+ use units,     only:set_units,umass,udist
+ use physcon,   only:solarm,au,pi,solarr,ceresm,km
+ use io,        only:master,fatal
+ use timestep,  only:tmax,dtmax
+ use inject,    only:inject_particles
+ integer,           intent(in)    :: id
+ integer,           intent(inout) :: npart
+ integer,           intent(out)   :: npartoftype(:)
+ real,              intent(out)   :: xyzh(:,:)
+ real,              intent(out)   :: massoftype(:)
+ real,              intent(out)   :: polyk,gamma,hfact
+ real,              intent(inout) :: time
+ character(len=20), intent(in)    :: fileprefix
+ real,              intent(out)   :: vxyzu(:,:)
+ character(len=120) :: filename
+ integer :: ierr
+ logical :: iexist
+ real    :: period,hacc2,massr
+
+ call set_units(mass=solarm,dist=solarr,G=1.d0)
+
+!
+!--Default runtime parameters
+!
+ m1            = 0.7  ! (solar masses)
+ m2            = 0.1  ! (ceres masses)
+ ecc           = 0.4  ! (eccentricity)
+ semia         = 0.7  ! (solar radii)
+ hacc1         = 0.01 ! (solar radii)
+ rasteroid     = 100. ! (km)
+ norbits       = 1.
+ dumpsperorbit = 100
+
+!
+!--Read runtime parameters from setup file
+!
+ if (id==master) print "(/,65('-'),1(/,a),/,65('-'),/)",' Asteroid wind'
+ filename = trim(fileprefix)//'.setup'
+ inquire(file=filename,exist=iexist)
+ if (iexist) call read_setupfile(filename,ierr)
+ if (.not. iexist .or. ierr /= 0) then
+    if (id==master) then
+       call write_setupfile(filename)
+       print*,' Edit '//trim(filename)//' and rerun phantomsetup'
+    endif
+    stop
+ endif
+
+ !
+ !--Convert to code units
+ !
+ m1    = m1*solarm/umass
+ m2    = m2*ceresm/umass
+
+ semia = semia*solarr/udist
+ hacc1 = hacc1*solarr/udist
+ rasteroid = rasteroid*km/udist
+
+!
+!--general parameters
+!
+ time = 0.
+ polyk = 0.
+ gamma = 1.
+
+!
+!--space available for injected gas particles
+!
+ npart = 0
+ npartoftype(:) = 0
+ xyzh(:,:)  = 0.
+ vxyzu(:,:) = 0.
+ nptmass = 0
+
+ massr  = m2/m1
+ period = sqrt(4.*pi**2*semia**3/(m1+m2))
+ hacc2  = 0.                                 ! Asteroid should not accrete
+ tmax   = norbits*period
+ dtmax  = period/dumpsperorbit
+
+!
+!--Set a binary orbit given the desired orbital parameters
+!
+ call set_binary(m1,massr,semia,ecc,hacc1,hacc2,xyzmh_ptmass,vxyz_ptmass,nptmass)
+
+ xyzmh_ptmass(ihsoft,2) = rasteroid ! Asteroid radius softening
+
+ call inject_particles(time,0.,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npart,npartoftype)
+
+ if (nptmass == 0) call fatal('setup','no sink particles setup')
+ if (npart == 0)   call fatal('setup','no hydro particles setup')
+ if (ierr /= 0)    call fatal('setup','ERROR during setup')
+
+end subroutine setpart
+
+
+!
+!---Read/write setup file--------------------------------------------------
+!
+subroutine write_setupfile(filename)
+ use infile_utils, only:write_inopt
+ character(len=*), intent(in) :: filename
+ integer, parameter :: iunit = 20
+
+ print "(a)",' writing setup options file '//trim(filename)
+ open(unit=iunit,file=filename,status='replace',form='formatted')
+ write(iunit,"(a)") '# input file for binary setup routines'
+ call write_inopt(m1,           'm1',           'mass of white dwarf (solar mass)',                 iunit)
+ call write_inopt(m2,           'm2',           'mass of asteroid (ceres mass)',                    iunit)
+ call write_inopt(ecc,          'ecc',          'eccentricity',                                     iunit)
+ call write_inopt(semia,        'semia',        'semi-major axis (solar radii)',                    iunit)
+ call write_inopt(hacc1,        'hacc1',        'white dwarf (sink) accretion radius (solar radii)',iunit)
+ call write_inopt(rasteroid,    'rasteroid',    'radius of asteroid (km)',                          iunit)
+ call write_inopt(norbits,      'norbits',      'number of orbits',                                 iunit)
+ call write_inopt(dumpsperorbit,'dumpsperorbit','number of dumps per orbit',                        iunit)
+ close(iunit)
+
+end subroutine write_setupfile
+
+subroutine read_setupfile(filename,ierr)
+ use infile_utils, only:open_db_from_file,inopts,read_inopt,close_db
+ use io,           only:error
+ character(len=*), intent(in)  :: filename
+ integer,          intent(out) :: ierr
+ integer, parameter :: iunit = 21
+ integer :: nerr
+ type(inopts), allocatable :: db(:)
+
+ print "(a)",'reading setup options from '//trim(filename)
+ nerr = 0
+ ierr = 0
+ call open_db_from_file(db,filename,iunit,ierr)
+ call read_inopt(m1,           'm1',           db,min=0.,errcount=nerr)
+ call read_inopt(m2,           'm2',           db,min=0.,errcount=nerr)
+ call read_inopt(ecc,          'ecc',          db,min=0.,errcount=nerr)
+ call read_inopt(semia,        'semia',        db,min=0.,errcount=nerr)
+ call read_inopt(hacc1,        'hacc1',        db,min=0.,errcount=nerr)
+ call read_inopt(rasteroid,    'rasteroid',    db,min=0.,errcount=nerr)
+ call read_inopt(norbits,      'norbits',      db,min=0.,errcount=nerr)
+ call read_inopt(dumpsperorbit,'dumpsperorbit',db,min=0 ,errcount=nerr)
+ call close_db(db)
+ if (nerr > 0) then
+    print "(1x,i2,a)",nerr,' error(s) during read of setup file: re-writing...'
+    ierr = nerr
+ endif
+
+end subroutine read_setupfile
+
+end module setup
