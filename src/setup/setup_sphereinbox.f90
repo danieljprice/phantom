b80c96ec65c1b484a3a00623f82197cf05573b33
James Wurster <j.wurster@exeter.ac.uk>
2018-07-09 15:51:43 +0100

    (setup_sphereinbox) no need to reprint .in file due to previous commit

--- src/setup/setup_sphereinbox.f90
+++ src/setup/setup_sphereinbox.f90
@@ -311,10 +311,7 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
                     hfact,npart,np,xyzh,r_sphere,vol_sphere,npart_total)
  print "(a,es10.3)",' Particle separation in sphere = ',psep
  npartsphere = npart
- if (np_in/=npartsphere .and. id==master) then
-    np = npartsphere
-    call write_setupfile(filename)
- endif
+ if (np_in/=npartsphere) np = npartsphere
  !
  ! setup surrounding low density medium
  !
@@ -334,7 +331,7 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
     call set_particle_type(i,igas)
  enddo
  !
- ! reset to centre of mas
+ ! reset to centre of mass
  !
  call reset_centreofmass(npart,xyzh,vxyzu)
  !
