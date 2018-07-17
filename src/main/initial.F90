18297b3f7ad6cb3da7712fd9d39461ba696c628a
Daniel Price <daniel.price@monash.edu>
2018-07-18 00:06:19 +1000

    [header-bot] updated file headers

--- src/main/initial.F90
+++ src/main/initial.F90
@@ -20,12 +20,11 @@
 !
 !  DEPENDENCIES: balance, boundary, centreofmass, checkoptions, checksetup,
 !    chem, cooling, cpuinfo, densityforce, deriv, dim, domain, dust,
-!    energies, eos, evwrite, externalforces, fastmath, forcing, gitinfo,
-!    growth, h2cooling, initial_params, io, io_summary, linklist, mf_write,
-!    mpi, mpiutils, nicil, nicil_sup, omputils, options, part, photoevap,
+!    energies, eos, evwrite, externalforces, fastmath, forcing, growth,
+!    h2cooling, initial_params, io, io_summary, linklist, mf_write, mpi,
+!    mpiutils, nicil, nicil_sup, omputils, options, part, photoevap,
 !    ptmass, readwrite_dumps, readwrite_infile, setup, sort_particles,
-!    step_lf_global, timestep, timestep_ind, timestep_sts, timing, units,
-!    writeheader
+!    timestep, timestep_ind, timestep_sts, timing, units, writeheader
 !+
 !--------------------------------------------------------------------------
 module initial
