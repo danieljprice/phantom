b4b32c90ea65a4697c80f0668c71df831d944a0b
David Liptai <david.liptai@monash.edu>
2018-08-11 19:10:13 +1000

    (GR) utils_gr -- add fatal in get_u0 when trying to take the square root of a negative

--- src/main/utils_gr.f90
+++ src/main/utils_gr.f90
@@ -30,13 +30,16 @@ end function dot_product_gr
 !-------------------------------------------------------------------------------
 
 subroutine get_u0(gcov,v,U0)
+ use io, only:fatal
  real, intent(in)  :: gcov(0:3,0:3), v(1:3)
  real, intent(out) :: U0
- real :: v4(0:3)
+ real :: v4(0:3),vv
 
  v4(0) = 1.
  v4(1:3) = v(1:3)
- U0 = 1./sqrt(-dot_product_gr(v4,v4,gcov))
+ vv = dot_product_gr(v4,v4,gcov)
+ if (vv>0.) call fatal('get_u0','1/sqrt(-v_mu v^mu) ---> non-negative: v_mu v^mu',val=vv)
+ U0 = 1./sqrt(-vv)
 
 end subroutine get_u0
 
