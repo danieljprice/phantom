e66fff20e098b6a4ab19c5bf87105b1347625411
Daniel Mentiplay <daniel.mentiplay@monash.edu>
2018-09-04 14:00:33 +1000

    (largegrains) combinedustdumps: set grainsize and graindens, reset units after call to initialise, set npartoftype and massoftype correctly

--- src/utils/combinedustdumps.f90
+++ src/utils/combinedustdumps.f90
@@ -9,9 +9,9 @@
 !
 !  DESCRIPTION: This program is a utility for stacking multiple 2-fluid dust
 !  dumps onto a single set of gas particles. The gas positions will be taken
-!  from the dumpfile in the first argument on the command line, and the output
-!  dumpfile is the last argument. We assume that all dumps have the same number
-!  of dust particles.
+!  from the dumpfile named in the first argument on the command line, and the
+!  output dumpfile name is the last argument. We assume that all dumps have the
+!  same number of dust particles.
 !
 !  REFERENCES: None
 !
@@ -21,7 +21,7 @@
 !
 !  USAGE: combinedustdumps inputdumpfile1 ... inputdumpfileN outputdumpfile
 !
-!  DEPENDENCIES: deriv, dim, initial, io, part, readwrite_dumps
+!  DEPENDENCIES: deriv, dim, initial, io, part, readwrite_dumps, units
 !+
 !--------------------------------------------------------------------------
 program combinedustdumps
@@ -30,23 +30,26 @@ program combinedustdumps
  use initial,         only:initialise
  use io,              only:set_io_unit_numbers,iprint,idisk1
  use part,            only:xyzh,vxyzu,npart,hfact,iphase,npartoftype,massoftype,&
-                           igas,idust,&
-                           ndusttypes,ndustsmall,ndustlarge,fxyzu,fext,divcurlv,&
-                           divcurlB,Bevol,dBevol,dustfrac,ddustevol,temperature,&
-                           dustprop,ddustprop,set_particle_type
+                           igas,idust,ndusttypes,ndustsmall,ndustlarge,fxyzu,fext,&
+                           divcurlv,divcurlB,Bevol,dBevol,dustfrac,ddustevol,&
+                           temperature,dustprop,ddustprop,set_particle_type,&
+                           grainsize,graindens
  use readwrite_dumps, only:read_dump,write_fulldump
+ use units,           only:set_units,select_unit,umass,udist,utime
  implicit none
  character(len=120), allocatable :: indumpfiles(:)
  character(len=120) :: outdumpfile
- real, allocatable  :: xyzh_tmp(:,:,:),vxyzu_tmp(:,:,:),massoftype_tmp(:),npartoftype_tmp(:)
+ real, allocatable  :: xyzh_tmp(:,:,:),vxyzu_tmp(:,:,:),massofdust_tmp(:)
+ real, allocatable  :: npartofdust_tmp(:),grainsize_tmp(:),graindens_tmp(:)
  integer :: i,j,counter,ipart,itype,ierr,nargs,idust_tmp,ninpdumps
  real    :: time,dtdum
+ real(kind=8) :: utime_tmp,udist_tmp,umass_tmp
 
  call set_io_unit_numbers
  iprint = 6
 
  !
- !--get name of run from the command line
+ !--get dumpfile names from the command line
  !
  nargs = command_argument_count()
  ninpdumps = nargs -1
@@ -67,35 +70,39 @@ program combinedustdumps
 
  !
  !--allocate arrays for dust particle positions and dustfrac
+ !  we assume all dumps are from the same phantom version
  !
  call read_dump(trim(indumpfiles(1)),time,hfact,idisk1,iprint,0,1,ierr)
-
- !--assume all dumps are from the same phantom version
  if (npartoftype(2) > 0) then
     !--old dumps
     idust_tmp = 2
  else
     !--new dumps
-    idust_tmp = 7
+    idust_tmp = idust
  endif
-
  allocate (xyzh_tmp(ninpdumps,4,npartoftype(idust_tmp)),stat=ierr)
  if (ierr /= 0) stop 'error allocating memory to store positions'
  allocate (vxyzu_tmp(ninpdumps,4,npartoftype(idust_tmp)),stat=ierr)
  if (ierr /= 0) stop 'error allocating memory to store velocities'
- allocate (npartoftype_tmp(ninpdumps),stat=ierr)
- if (ierr /= 0) stop 'error allocating memory to store particle mass'
- allocate (massoftype_tmp(ninpdumps),stat=ierr)
- if (ierr /= 0) stop 'error allocating memory to store particle mass'
+ allocate (npartofdust_tmp(ninpdumps),stat=ierr)
+ if (ierr /= 0) stop 'error allocating memory to store number of dust particles'
+ allocate (massofdust_tmp(ninpdumps),stat=ierr)
+ if (ierr /= 0) stop 'error allocating memory to store dust particle mass'
+ allocate (grainsize_tmp(ninpdumps),stat=ierr)
+ if (ierr /= 0) stop 'error allocating memory to store grain size'
+ allocate (graindens_tmp(ninpdumps),stat=ierr)
+ if (ierr /= 0) stop 'error allocating memory to store grain dens'
 
  !
- !--get dust particle positions and velocities
+ !--read dumps and get dust particle information
  !
  do i=1,ninpdumps
     call read_dump(trim(indumpfiles(i)),time,hfact,idisk1,iprint,0,1,ierr)
     if (ierr /= 0) stop 'error reading dumpfile'
-    npartoftype_tmp(i) = npartoftype(idust_tmp)
-    massoftype_tmp(i) = massoftype(idust_tmp)
+    npartofdust_tmp(i) = npartoftype(idust_tmp)
+    massofdust_tmp(i) = massoftype(idust_tmp)
+    grainsize_tmp(i) = grainsize(1)
+    graindens_tmp(i) = graindens(1)
     counter = 0
     do j=1,maxp
        if (iphase(j)==idust_tmp) then
@@ -108,7 +115,7 @@ program combinedustdumps
  enddo
 
  !
- !--re-read first dump and set particle properties
+ !--re-read first dump and correct dust particle properties
  !
  call read_dump(trim(indumpfiles(1)),time,hfact,idisk1,iprint,0,1,ierr)
  do i=1,maxp
@@ -116,40 +123,50 @@ program combinedustdumps
        call set_particle_type(i,idust)
     endif
  enddo
- npartoftype(idust) = npartoftype_tmp(1)
- massoftype(idust) = massoftype_tmp(1)
+ npartoftype(idust) = npartofdust_tmp(1)
+ massoftype(idust) = massofdust_tmp(1)
+ npartoftype(2) = 0
+ massoftype(2) = 0
+ grainsize(1) = grainsize_tmp(1)
+ graindens(1) = graindens_tmp(1)
 
  !
  !--add dust from other dumps
  !
  do i=2,ninpdumps
     itype = idust + i - 1
-    npartoftype(itype) = npartoftype_tmp(i)
-    massoftype(itype) = massoftype_tmp(i)
-    do j=1,npartoftype_tmp(i)
+    npartoftype(itype) = npartofdust_tmp(i)
+    massoftype(itype) = massofdust_tmp(i)
+    grainsize(i) = grainsize_tmp(i)
+    graindens(i) = graindens_tmp(i)
+    do j=1,npartofdust_tmp(i)
        ipart = npart + j
        call set_particle_type(ipart,itype)
        xyzh(:,ipart) = xyzh_tmp(i,:,j)
        vxyzu(:,ipart) = vxyzu_tmp(i,:,j)
     enddo
-    npart = npart + npartoftype_tmp(i)
+    npart = npart + npartofdust_tmp(i)
  enddo
-
- ! units?
 
  !
  !--dust properties
  !
- ! grainsize?
- ! graindens?
  ndusttypes = ninpdumps
  ndustlarge = ndusttypes
  ndustsmall = 0
 
  !
+ !--store units
+ !
+ utime_tmp = utime
+ umass_tmp = umass
+ udist_tmp = udist
+
+ !
  !--calculate dustfrac
  !
  call initialise()
+ call set_units(udist_tmp,umass_tmp,utime_tmp)
  call derivs(1,npart,npart,xyzh,vxyzu,fxyzu,fext,divcurlv,divcurlB,&
              Bevol,dBevol,dustprop,ddustprop,dustfrac,ddustevol,temperature,time,0.,dtdum)
 
@@ -158,10 +175,12 @@ program combinedustdumps
  !
  call write_fulldump(time,outdumpfile)
 
- if (allocated(xyzh_tmp)) deallocate(xyzh_tmp)
- if (allocated(vxyzu_tmp)) deallocate(vxyzu_tmp)
- if (allocated(npartoftype_tmp)) deallocate(npartoftype_tmp)
- if (allocated(massoftype_tmp)) deallocate(massoftype_tmp)
+ if (allocated(xyzh_tmp))        deallocate(xyzh_tmp)
+ if (allocated(vxyzu_tmp))       deallocate(vxyzu_tmp)
+ if (allocated(npartofdust_tmp)) deallocate(npartofdust_tmp)
+ if (allocated(massofdust_tmp))  deallocate(massofdust_tmp)
+ if (allocated(grainsize_tmp))   deallocate(grainsize_tmp)
+ if (allocated(graindens_tmp))   deallocate(graindens_tmp)
 
 end program combinedustdumps
 
