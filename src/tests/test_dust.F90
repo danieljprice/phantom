a3ccc83eb301684daf65114c97c7ad93aa6729f8
Daniel Mentiplay <daniel.mentiplay@monash.edu>
2018-09-14 12:04:07 +1000

    (largegrains) initialize variable [skip ci]

--- src/tests/test_dust.F90
+++ src/tests/test_dust.F90
@@ -662,6 +662,7 @@ subroutine test_epsteinstokes(ntests,npass)
     write(filename,"(a,1pe8.2)") 'rho=',rhogas*unit_density
     ncheck = 0
     nfailed = 0
+    errmax = 0.
     tol = 6.3e-2
     do i=1,npts
        grainsizei = smin*10**((i-1)*ds)/udist
