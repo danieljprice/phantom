aadc5fe7e92afa6eaf9a8213484f23a04b39f843
Daniel Price <daniel.price@monash.edu>
2018-10-08 08:33:05 +1100

    (setup_gwdisc) added inclination angle to .setup file; added setup_interactive routine to use standard organisational format; general cleanup

--- src/setup/setup_gwdisc.f90
+++ src/setup/setup_gwdisc.f90
@@ -1,6 +1,6 @@
 !--------------------------------------------------------------------------!
 ! The Phantom Smoothed Particle Hydrodynamics code, by Daniel Price et al. !
-! Copyright (c) 2007-2018 The Authors (see AUTHORS)                        !
+! Copyright (c) 2007-2017 The Authors (see AUTHORS)                        !
 ! See LICENCE file for usage and distribution conditions                   !
 ! http://users.monash.edu.au/~dprice/phantom                               !
 !--------------------------------------------------------------------------!
@@ -36,12 +36,13 @@
 !+
 !--------------------------------------------------------------------------
 module setup
+ use extern_binary, only:accradius1,accradius2,massr,a0 !,binary_posvel
  implicit none
  public :: setpart
 
  integer :: np
  real :: R_in, R_out, HoverRinput, discm, alphaSS
- real :: p_indexinput, q_indexinput
+ real :: p_indexinput, q_indexinput, inc
 
  private
 
@@ -56,15 +57,11 @@ contains
 !----------------------------------------------------------------
 subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,time,fileprefix)
  use setdisc,       only:set_disc
- use units,         only:umass,set_units
- use physcon,       only:solarm
+ use units,         only:set_units
+ use physcon,       only:pi,solarm
  use io,            only:master
  use options,       only:iexternalforce, alpha
- use prompting,     only:prompt
- use extern_binary, only:accradius1,accradius2,binary_posvel
- use extern_binary, only:massr,a0
  use externalforces, only:iext_binary
-
  integer,            intent(in)            :: id
  integer,            intent(out)           :: npart
  integer,            intent(out)           :: npartoftype(:)
@@ -74,7 +71,8 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  real,               intent(out)           :: massoftype(:)
  real,               intent(inout)         :: time
  character (len=20), intent (in), optional :: fileprefix
- !integer :: i
+ real     :: xinc
+ integer  :: ierr
  !real :: xbinary(10),vbinary(6)
 
  logical :: iexist
@@ -83,10 +81,8 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  !
  !--set code units
  !
-
  call set_units(mass=1.*solarm,c=1.)
 
-! filename = 'gwdisc.setup'
  filename=trim(fileprefix)//'.setup'
 
  np = size(xyzh(1,:))
@@ -96,7 +92,7 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  hfact = 1.2
  time  = 0.
  accradius1 = 2.0 ! R_schwarzschild
- a0=11.5
+ a0= 11.5
  R_in  = 6. ! R_isco =3*R_schw
  R_out  = 0.9*a0
  HoverRinput = 0.01
@@ -104,44 +100,20 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  alphaSS = 0.1
  p_indexinput = 1.5
  q_indexinput = 0.75
-
+ inc = 0.
 
  print "(a,/)",'Phantomsetup: routine to set a shrinking binary. '
  inquire(file=filename,exist=iexist)
  if (iexist) then
-    call read_gwinputfile(filename)
+    call read_setupfile(filename,ierr)
+    if (ierr /= 0) then
+       if (id==master) call write_setupfile(filename)
+       stop
+    endif
  elseif (id==master) then
     print "(a,/)",trim(filename)//' not found: using interactive setup'
-    !
-    !--set default options
-    !
-!    print "(a,f6.3,a,1pe8.2,a)",' Schwarzschild radius is ',2.0*udist/au,' AU for ',umass/solarm,' M_sun black hole'
-
-    call prompt('Enter total number of gas particles ',np,0,size(xyzh(1,:)))
-    call prompt('Enter mass ratio of binary',massr,0.,1.)
-
-    accradius2 = massr*accradius1
-
-    call prompt('Enter initial binary separation',a0,0.)
-    call prompt('Enter accretion radius of the PRIMARY black hole ',accradius1,accradius1,a0)
-    call prompt('Enter accretion radius of the SECONDARY black hole ',accradius2,accradius2,a0)
-
-    call prompt('Enter inner disc edge R_in ',R_in,accradius1)
-    call prompt('Enter outer disc edge R_out ',R_out,R_in)
-    call prompt('Enter H/R at R=R_in ',HoverRinput,0.)
-    call prompt('Enter p index of surface density profile Sigma = Sigma0*R^-p',p_indexinput,0.)
-    call prompt('Enter q index of temperature profile cs = cs0*R^-q',q_indexinput,0.)
-    print "(a,es10.4,a)",'Enter disc mass in units of ',umass/solarm,' solar masses (Mjup = 8.6 x 10^-4 Msun) '
-    call prompt(' ',discm)
-    call prompt('Enter desired value of alpha_SS',alphaSS,0.)
-
-    massoftype(1) = discm/real(npart)
-
-    !
-    !--write default input file
-    !
-    call write_gwinputfile(filename)
-
+    call setup_interactive()
+    call write_setupfile(filename)
     print "(a)",'>>> rerun phantomsetup using the options set in '//trim(filename)//' <<<'
     stop
  else
@@ -151,6 +123,7 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  npart = np
  npartoftype(1) = npart
  massoftype(1) = discm/real(npart)
+ xinc = inc*(pi/180.0) ! Must be in radians
 
  alpha=alphaSS
 
@@ -166,9 +139,8 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
                gamma     = gamma,  &
                particle_mass = massoftype(1), &
                hfact=hfact,xyzh=xyzh,vxyzu=vxyzu,polyk=polyk,alpha=alpha, &
+               inclination = xinc, &
                prefix = fileprefix )
-
-
  !
  !--set default options for the input file
  !
@@ -190,12 +162,39 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  return
 end subroutine setpart
 
+subroutine setup_interactive()
+ use prompting, only:prompt
+ use physcon,   only:solarm
+ use units,     only:umass
+    !
+    !--set default options
+    !
+!    print "(a,f6.3,a,1pe8.2,a)",' Schwarzschild radius is ',2.0*udist/au,' AU for ',umass/solarm,' M_sun black hole'
 
-subroutine write_gwinputfile(filename)
- use infile_utils, only:write_inopt
- use extern_binary, only:accradius1,accradius2,binary_posvel
- use extern_binary, only:massr,a0
- implicit none
+    call prompt('Enter total number of gas particles ',np,0)
+    call prompt('Enter mass ratio of binary',massr,0.,1.)
+
+    accradius2 = massr*accradius1
+
+    call prompt('Enter initial binary separation',a0,0.)
+    call prompt('Enter accretion radius of the PRIMARY black hole ',accradius1,accradius1,a0)
+    call prompt('Enter accretion radius of the SECONDARY black hole ',accradius2,accradius2,a0)
+
+    call prompt('Enter inner disc edge R_in ',R_in,accradius1)
+    call prompt('Enter outer disc edge R_out ',R_out,R_in)
+    call prompt('Enter H/R at R=R_in ',HoverRinput,0.)
+    call prompt('Enter p index of surface density profile Sigma = Sigma0*R^-p',p_indexinput,0.)
+    call prompt('Enter q index of temperature profile cs = cs0*R^-q',q_indexinput,0.)
+    print "(a,es10.4,a)",'Enter disc mass in units of ',umass/solarm,' solar masses (Mjup = 8.6 x 10^-4 Msun) '
+    call prompt(' ',discm,0.)
+    call prompt('Enter desired value of alpha_SS',alphaSS,0.)
+
+    call prompt('Enter disc inclination (deg) ',inc,-180.,180.)
+
+end subroutine setup_interactive
+
+subroutine write_setupfile(filename)
+ use infile_utils,  only:write_inopt
  character(len=*), intent(in) :: filename
  integer, parameter :: iunit = 20
 
@@ -223,43 +222,48 @@ subroutine write_gwinputfile(filename)
  call write_inopt(p_indexinput,'p_indexinput','surface density profile',iunit)
  call write_inopt(q_indexinput,'q_indexinput','temperature profile',iunit)
  call write_inopt(alphaSS,'alphaSS','desired alpha_SS',iunit)
-
+ call write_inopt(inc,'inc','inclination (tilt) in degrees',iunit)
 
  close(iunit)
 
-end subroutine write_gwinputfile
+end subroutine write_setupfile
 
-subroutine read_gwinputfile(filename)
+subroutine read_setupfile(filename,ierr)
  use infile_utils, only:open_db_from_file,inopts,read_inopt,close_db
  use extern_binary, only:accradius1,accradius2,binary_posvel
  use extern_binary, only:massr,a0
- implicit none
- character(len=*), intent(in) :: filename
+ character(len=*), intent(in)  :: filename
+ integer,          intent(out) :: ierr
  integer, parameter :: iunit = 21
- integer :: ierr
+ integer :: nerr
  type(inopts), dimension(:), allocatable :: db
 
  print "(a)",'reading setup options from '//trim(filename)
 
  call open_db_from_file(db,filename,iunit,ierr)
 
- call read_inopt(np,'np',db,ierr)
- call read_inopt(massr,'massr',db,ierr)
- call read_inopt(a0,'a0',db,ierr)
- call read_inopt(accradius1,'accradius1',db,ierr)
- call read_inopt(accradius2,'accradius2',db,ierr)
- call read_inopt(R_in,'R_in',db,ierr)
- call read_inopt(R_out,'R_out',db,ierr)
- call read_inopt(HoverRinput,'HoverRinput',db,ierr)
- call read_inopt(discm,'discm',db,ierr)
- call read_inopt(p_indexinput,'p_indexinput',db,ierr)
- call read_inopt(q_indexinput,'q_indexinput',db,ierr)
- call read_inopt(alphaSS,'alphaSS',db,ierr)
+ nerr = 0
+ call read_inopt(np,'np',db,min=1,errcount=nerr)
+ call read_inopt(massr,'massr',db,min=0.,errcount=nerr)
+ call read_inopt(a0,'a0',db,min=0.,errcount=nerr)
+ call read_inopt(accradius1,'accradius1',db,min=0.,errcount=nerr)
+ call read_inopt(accradius2,'accradius2',db,min=0.,errcount=nerr)
+ call read_inopt(R_in,'R_in',db,min=0.,errcount=nerr)
+ call read_inopt(R_out,'R_out',db,min=R_in,errcount=nerr)
+ call read_inopt(HoverRinput,'HoverRinput',db,min=0.,errcount=nerr)
+ call read_inopt(discm,'discm',db,min=0.,errcount=nerr)
+ call read_inopt(p_indexinput,'p_indexinput',db,errcount=nerr)
+ call read_inopt(q_indexinput,'q_indexinput',db,errcount=nerr)
+ call read_inopt(alphaSS,'alphaSS',db,min=0.,errcount=nerr)
+ call read_inopt(inc,'inc',db,errcount=nerr)
+ ierr = nerr
+ if (nerr > 0) then
+    print "(1x,i2,a)",nerr,' error(s) during read of setup file: re-writing...'
+ endif
 
  call close_db(db)
  close(iunit)
 
-end subroutine read_gwinputfile
-
+end subroutine read_setupfile
 
 end module setup
