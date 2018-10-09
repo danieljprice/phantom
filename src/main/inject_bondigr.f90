8d7ea7b1351bd7c3cff2665cc2f4f35eb0338e8a
David Liptai <david.liptai@monash.edu>
2018-10-09 14:25:43 +1100

    (GR) massive cleanup of bondigr-inject

--- src/main/inject_bondigr.f90
+++ src/main/inject_bondigr.f90
@@ -1,49 +1,19 @@
-!--------------------------------------------------------------------------!
-! The Phantom Smoothed Particle Hydrodynamics code, by Daniel Price et al. !
-! Copyright (c) 2007-2017 The Authors (see AUTHORS)                        !
-! See LICENCE file for usage and distribution conditions                   !
-! http://users.monash.edu.au/~dprice/phantom                               !
-!--------------------------------------------------------------------------!
-!+
-!  MODULE: inject
-!
-!  DESCRIPTION:
-!  Handles wind injection for the generalised Bondi accretion solution in the Schwarzschild metric
-!
-!  REFERENCES: Hawley, Smarr and Wilson (1984) ApJ 277, 296
-!
-!  OWNER: David Liptai
-!
-!  $Id$
-!
-!  RUNTIME PARAMETERS:
-!    gammawind       -- adiabatic index gamma
-!    iwindres        -- resolution of the wind
-!    drwindsph       -- distance between spheres / neighbours
-!    isphereshandled -- number of handled inner spheres of the wind (integer)
-!    rin             -- radius of injection of the wind
-!
-!  DEPENDENCIES: eos, icosahedron, infile_utils, io, part, partinject,
-!    physcon, timestep, metric
-!+
-!--------------------------------------------------------------------------
 module inject
- use bondiexact, only:get_bondi_solution,isol
- use metric,     only:mass1
+ use bondiexact, only:get_bondi_solution,isol,iswind
+
  implicit none
- character(len=*), parameter, public :: inject_type = 'wind'
 
- public :: inject_particles,write_options_inject,read_options_inject,wind_init
+ character(len=*), parameter, public :: inject_type = 'bondi'
+ public :: inject_particles,     &
+           write_options_inject, &
+           read_options_inject,  &
+           inject_init
 
-!
-!--runtime settings for this module
-!
-
-!--- Read from input file--------------------------
+!--- Runtime settings read from input file--------------------------
  integer, private :: iwindres        = 4
- real,    private :: drwindsph       = 4.
- integer, private :: isphereshandled = 4
- real,    public  :: rin             = 5.    ! Injection radius (in units of central mass M)
+ real,    private :: dtsphere        = 2.
+ integer, private :: nhandled        = 3
+ real,    public  :: rin             = 18.1     ! Injection radius (in units of central mass M)
  real,    public  :: gammawind       = 5./3.
 
 ! Calculated from the previous parameters
@@ -51,14 +21,10 @@ module inject
 
  private
 
- logical, parameter :: wind_verbose = .false.
- logical, parameter :: inflow       = .false.
-
- real    :: geodesic_R(0:19,3,3),geodesic_v(0:11,3),uthermontemp
- real    :: rhoin,vin,uthermin,mdot
- real    :: speed
- real    :: masssphere,dtsphere,neighdist
+ logical :: wind_verbose = .false.
  integer :: npsphere
+ real    :: vin
+ real    :: geodesic_R(0:19,3,3),geodesic_v(0:11,3)
 
 contains
 
@@ -66,9 +32,11 @@ contains
 subroutine get_solution(rho,v,u,r)
  real, intent(out) :: rho,v,u
  real, intent(in)  :: r
+ real, parameter   :: mass1 = 1.
+ !-- Note: GR code is written such that central mass is always 1 (i.e. distance, time, and BH spin are in units of M always)
+
  call get_bondi_solution(rho,v,u,r,mass1,gammawind)
- ! Direction of wind
- if (inflow) v = -v
+
 end subroutine get_solution
 
 !-----------------------------------------------------------------------
@@ -76,44 +44,68 @@ end subroutine get_solution
 !  Initialize reusable variables
 !+
 !-----------------------------------------------------------------------
-subroutine wind_init(setup)
- use physcon,     only: Rg, pi
- use icosahedron, only: compute_matrices, compute_corners
- use timestep,    only: dtmax
- use eos,         only: gmw
- logical, intent(in) :: setup
+subroutine inject_init(setup,sol)
+ use physcon,     only:Rg, pi
+ use icosahedron, only:compute_matrices, compute_corners
+ use timestep,    only:dtmax
+ use part,        only:massoftype,igas
+ use kernel,      only:radkern
+ logical, intent(in)  :: setup
+ integer, intent(out), optional :: sol
  real, parameter :: phi = (sqrt(5.)+1.)/2. ! Golden ratio
- real :: irrational_numbre_close_to_one
+ real, parameter :: pi3 = pi/3.            ! irrational number close to one
+ real :: speed,rhoin,uthermin,mdot,masssphere,neighdist,drsphere
 
- rin   = rin*mass1
  call get_solution(rhoin,vin,uthermin,rin)
- speed = abs(vin)
 
- uthermontemp = Rg/(gmw*(gammawind-1.))
+ speed        = abs(vin)
  npsphere     = 20 * (2*iwindres*(iwindres-1)) + 12
  neighdist    = 2./((2.*iwindres-1.)*sqrt(sqrt(5.)*phi))
+ mdot         = 4.*pi*rin**2*rhoin*speed
 
- mdot       = 4.*pi*rin**2*rhoin*speed
- masspart   = drwindsph * neighdist * rin * mdot / (npsphere * speed)
- masssphere = masspart * npsphere
- dtsphere   = masssphere / mdot
+ ! drsphere     = neighdist
+ drsphere     = speed*dtsphere
+
+ ! dtsphere     = drsphere/speed
+
+ masspart     = mdot*dtsphere/npsphere
+ masssphere   = npsphere*masspart
 
  call compute_matrices(geodesic_R)
  call compute_corners(geodesic_v)
 
- ! adjusting dtmax to avoid uterm < 0 errors
+ print*,'========= GR Bondi Wind Injection ======================'
+ print*,'Options adjustable in .in file:                         '
+ print*,' -- wind                    : ',iswind
+ print*,' -- isol (type of flow)     : ',isol
+ print*,' -- iwindres                : ',iwindres
+ print*,' -- nhandled                : ',nhandled
+ print*,' -- rin                     : ',rin
+ print*,' -- dtsphere                : ',dtsphere
+ print*,' -- gammawind               : ',gammawind
+ print*,''
+ print*,'Other info:'
+ print*,' -- Particles per sphere    : ',npsphere
+ print*,' -- Nieghbour distance      : ',neighdist
+ print*,' -- Mass of particles       : ',masspart
+ print*,' -- Mass of spheres         : ',masssphere
+ print*,' -- Mdot                    : ',mdot
+ print*,' -- Approx. drsphere at rin : ',drsphere
+ print*,' -- vr               at rin : ',vin
+ print*,' -- utherm           at rin : ',uthermin
+ print*,' -- rho              at rin : ',rhoin
+ print*,'========================================================'
+ print*,''
+
+ !-- Adjusting dtmax to avoid uterm < 0 errors (???)
  if (setup) then
-    irrational_numbre_close_to_one = pi/3.
-    dtmax = 0.5 * irrational_numbre_close_to_one * dtsphere
+    dtmax = 0.5 * pi3 * dtsphere
  endif
 
- print*,'========= GR Bondi Wind Injection =========='
- print*,'Particles per sphere :',npsphere
- print*,'Nieghbour distance   :',neighdist
- print*,'Mass of particles    :',masspart
- print*,'Mass of spheres      :',masssphere
- print*,'time between spheres ;',dtsphere
- print*,'============================================'
+ !-- Set particle mass
+ massoftype(igas) = masspart
+
+ if (present(sol)) sol = isol
 
 end subroutine
 
@@ -130,63 +122,80 @@ subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npar
  integer, intent(inout) :: npartoftype(:)
  logical, save :: first_run = .true.
  logical :: outerbound
- integer :: isphereouter,isphereinner,ihandledinner,i
- real :: tlocal,r,v,u,rho,mass_lost
+ integer :: imin,imax,ihandledinner,isphere,j,ipartbegin
+ real :: tlocal,r,v,u,rho
  real :: r2,outer_boundary
 
  if (first_run) then
-    call wind_init(.false.)
+    call inject_init(setup=.false.)
     first_run = .false.
  endif
 
- isphereouter  = floor((time-dtlast)/dtsphere) + 1
- isphereinner  = floor(time/dtsphere)
- ihandledinner = isphereinner + isphereshandled
+ !--- Spheres injected since last timestep
+ imin  = floor((time-dtlast)/dtsphere) + 1
+
+ !--- Spheres injected since code start
+ imax  = floor(time/dtsphere)
+
+ ihandledinner = imax + nhandled
+
  if (wind_verbose) then
     write(iprint,*) '   time between spheres',dtsphere
-    write(iprint,*) '   Inner sphere  :'     ,isphereinner ,'Outer sphere:',isphereouter
-    write(iprint,*) '   Loop goes from:'     ,ihandledinner,'to:          ',isphereouter
+    write(iprint,*) '   Inner sphere  :'     ,imax         ,'Outer sphere:',imin
+    write(iprint,*) '   Loop goes from:'     ,ihandledinner,'to:          ',imin
  endif
- do i=ihandledinner,isphereouter,-1
-    tlocal = time - (i-isphereshandled) * dtsphere
-    call compute_sphere_properties(tlocal,r,v,u,rho)
-    if (wind_verbose) then
-       write(iprint,*) '* Sphere:'
-       if (i > isphereinner) then
-          write(iprint,*) 'HANDLED'
-       else
-          write(iprint,*) 'INJECTED'
-       endif
-       write(iprint,*) '   Number i      : ',i
-       write(iprint,*) '   Local Time    : ',tlocal
-       write(iprint,*) '   Radius        : ',r
-       write(iprint,*) '   Velocity      : ',v
-       write(iprint,*) '   Temperature   : ',u / uthermontemp,' K'
-       write(iprint,*) '   Density (rho) : ',rho
-    endif
-    if (i > isphereinner) then
-       call inject_geodesic_sphere(i,(ihandledinner-i)*npsphere+1,r,v,u,rho,npart,npartoftype,xyzh,vxyzu) ! handled sphere
-    else
-       call inject_geodesic_sphere(i,npart+1,r,v,u,rho,npart,npartoftype,xyzh,vxyzu) ! injected sphere
-    endif
- enddo
- mass_lost = masssphere * (isphereinner-isphereouter+1)
 
+ do isphere=ihandledinner,imin,-1
+    tlocal = time - isphere*dtsphere
+    call compute_sphere_properties(tlocal,r,v,u,rho)
+
+    if (isphere > imax) then
+    !--- update values of the handled (boundary) sphere
+       ipartbegin = (ihandledinner-isphere)*npsphere+1
+    else
+    !--- injected a new sphere
+       ipartbegin = npart + 1
+    endif
+
+    call inject_geodesic_sphere(isphere,ipartbegin,r,v,u,rho,npart,npartoftype,xyzh,vxyzu)
+
+    if (wind_verbose) then
+      if (isphere > imax) then
+          write(iprint,*) '* Sphere: HANDLED'
+      else
+          write(iprint,*) '* Sphere: INJECTED'
+      endif
+      write(iprint,*) '   Number i      : ',isphere
+      write(iprint,*) '   Local Time    : ',tlocal
+      write(iprint,*) '   Radius        : ',r
+      write(iprint,*) '   Velocity      : ',v
+      write(iprint,*) '   Utherm        : ',u
+      write(iprint,*) '   Density (rho) : ',rho
+      write(iprint,*) '   ipartbegin    : ',ipartbegin
+    endif
+
+ enddo
+
+ !--- LEGACY code ---
  !--- "Accrete" particles after reaching some outer boundary
  outerbound = .false.
  if (outerbound) then
-    outer_boundary = 20.*mass1
+    outer_boundary = 20.
     !$omp parallel do default(none) &
     !$omp shared(npart,xyzh,outer_boundary) &
-    !$omp private(i,r2)
-    do i=1,npart
-       r2 = xyzh(1,i)**2 + xyzh(2,i)**2 + xyzh(3,i)**2
-       if (r2 >outer_boundary**2) xyzh(4,i) = -abs(xyzh(4,i))
+    !$omp private(j,r2)
+    do j=1,npart
+       r2 = xyzh(1,j)**2 + xyzh(2,j)**2 + xyzh(3,j)**2
+       if (r2 >outer_boundary**2) xyzh(4,j) = -abs(xyzh(4,j))
     enddo
     !$omp end parallel do
  endif
 
 end subroutine inject_particles
+
+!
+!===========================================================================================
+!
 
 !-----------------------------------------------------------------------
 !+
@@ -318,14 +327,14 @@ subroutine write_options_inject(iunit)
  use infile_utils, only: write_inopt
  integer, intent(in) :: iunit
 
- call write_inopt(gammawind,'gammawind','polytropic index of the wind',iunit)
- call write_inopt(iwindres,'iwindres',&
-      'resolution of the wind (1-6,10,15)-- DO NOT CHANGE DURING SIMULATION --',iunit)
- call write_inopt(drwindsph,'drwindsph','distance between spheres / neighbours -- DO NOT CHANGE DURING SIMULATION --',iunit)
- call write_inopt(isphereshandled,'isphereshandled','handle inner spheres of the wind (integer)',iunit)
- call write_inopt(rin,'rin', &
-      'radius of injection of the wind (in units of the central mass) -- DO NOT CHANGE DURING SIMULATION --',iunit)
- call write_inopt(isol,'isol','solution type (1 = geodesic flow  |  2 = sonic point flow)',iunit)
+ call write_inopt(iswind   ,'iswind'   ,'is it a wind? (logical)'                                   ,iunit)
+ call write_inopt(isol     ,'isol'     ,'solution type (1 = geodesic flow  |  2 = sonic point flow)',iunit)
+ call write_inopt(iwindres ,'iwindres' ,'resolution of the wind (1-6,10,15)'                        ,iunit)
+ call write_inopt(nhandled ,'nhandled' ,'number of handled/boundary spheres (integer)'              ,iunit)
+ call write_inopt(rin      ,'rin'      ,'injection radius'                                          ,iunit)
+ call write_inopt(dtsphere ,'dtsphere' ,'time between spheres'                                      ,iunit)
+ call write_inopt(gammawind,'gammawind','polytropic index of the wind'                              ,iunit)
+
 end subroutine
 
 !-----------------------------------------------------------------------
@@ -346,26 +355,29 @@ subroutine read_options_inject(name,valstring,imatch,igotall,ierr)
  imatch  = .true.
  igotall = .false.
  select case(trim(name))
+ case('iswind')
+    read(valstring,*,iostat=ierr) iswind
+    ngot = ngot + 1
  case('gammawind')
     read(valstring,*,iostat=ierr) gammawind
     ngot = ngot + 1
-    if (gammawind < 0.)       call fatal(label,'invalid setting for gammawind (<0)')
+    if (gammawind < 0.)       call fatal(label,'gamma must be greater than 0')
  case('iwindres')
     read(valstring,*,iostat=ierr) iwindres
     ngot = ngot + 1
     if (iwindres < 1)  call fatal(label,'iwindres must be bigger than zero')
- case('drwindsph')
-    read(valstring,*,iostat=ierr) drwindsph
+ case('dtsphere')
+    read(valstring,*,iostat=ierr) dtsphere
     ngot = ngot + 1
-    if (drwindsph <= 0.)    call fatal(label,'drwindsph must be >=0')
- case('isphereshandled')
-    read(valstring,*,iostat=ierr) isphereshandled
+    if (dtsphere <= 0.)    call fatal(label,'dtsphere must be >=0')
+ case('nhandled')
+    read(valstring,*,iostat=ierr) nhandled
     ngot = ngot + 1
-    if (isphereshandled <= 0) call fatal(label,'isphereshandled must be > 0')
+    if (nhandled <= 0) call fatal(label,'nhandled must be > 0')
  case('rin')
     read(valstring,*,iostat=ierr) rin
     ngot = ngot + 1
-    if (rin < 2.*mass1) call fatal(label,'invalid setting for rin (<2M)')
+    if (rin < 2.) call fatal(label,'invalid setting for rin (<2M)')
  case('isol')
     read(valstring,*,iostat=ierr) isol
     ngot = ngot + 1
@@ -374,7 +386,7 @@ subroutine read_options_inject(name,valstring,imatch,igotall,ierr)
     imatch = .false.
  end select
 
- noptions = 6
+ noptions = 7
 
  igotall = (ngot >= noptions)
 
--- src/setup/setup_bondiwind.f90
+++ src/setup/setup_bondiwind.f90
@@ -35,16 +35,16 @@ contains
 !+
 !----------------------------------------------------------------
 subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,time,fileprefix)
- use part,           only: xyzmh_ptmass, vxyz_ptmass, nptmass, gr
- use units,          only: set_units
- use options,        only: iexternalforce
- use timestep,       only: tmax,nmax
- use io,             only: fatal
- use prompting,      only: prompt
- use metric,         only: mass1, imetric
- use metric_tools,   only: imet_schwarzschild
- use externalforces, only: accradius1,accradius1_hard
- use inject,         only: wind_init,inject_particles,gammawind,masspart
+ use part,           only:gr,igas
+ use units,          only:set_units
+ use options,        only:iexternalforce,alpha,alphau
+ use timestep,       only:tmax
+ use io,             only:fatal
+ use prompting,      only:prompt
+ use metric,         only:imetric
+ use metric_tools,   only:imet_schwarzschild
+ use externalforces, only:accradius1,accradius1_hard
+ use inject,         only:inject_init,inject_particles,gammawind,masspart
  integer,           intent(in)    :: id
  integer,           intent(inout) :: npart
  integer,           intent(out)   :: npartoftype(:)
@@ -54,6 +54,8 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
  real,              intent(out)   :: polyk,gamma,hfact
  real,              intent(inout) :: time
  character(len=20), intent(in)    :: fileprefix
+ character(len=40) :: infile
+ integer :: isol
 
  if (.not.gr) call fatal('setup_bondiwind','This setup only works with GR on')
  if (imetric/=imet_schwarzschild) call fatal('setup_bondiwind','This setup is meant for use with the Schwarzschild metric')
@@ -61,40 +63,29 @@ subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,
 
  ! General parameters
  time  = 0.
- tmax  = 1000.
+ tmax  = 360.
  polyk = 0.
- gamma = gammawind
+
  iexternalforce  = 1
- mass1           = 1.
- accradius1      = 2.*mass1
+ accradius1      = 2.5
  accradius1_hard = accradius1
 
- call wind_init(.true.)
+ call inject_init(setup=.true.,sol=isol)
+ !-- Geodesic flow
+ if (isol == 1) then
+    alpha         = 0.
+    alphau        = 0.
+    tmax          = 90.
+ endif
 
-!
-!--space available for injected gas particles
-!
- npart = 0
- npartoftype(:) = 0
+ massoftype(igas) = masspart
+ gamma            = gammawind
+ npart            = 0
+ npartoftype(:)   = 0
 
- massoftype = masspart
-
- xyzh(:,:)  = 0.
- vxyzu(:,:) = 0.
- xyzmh_ptmass(:,:) = 0.
- vxyz_ptmass(:,:) = 0.
-
-
- ! --- Put sink with no mass (needed to get around bug where phantom doesn't want to read an empty dumpfile)
- nptmass = 1
- xyzmh_ptmass(1,1) = 0.
- xyzmh_ptmass(2,1) = 0.
- xyzmh_ptmass(3,1) = 0.
- xyzmh_ptmass(4,1) = 0.
- xyzmh_ptmass(5,1) = 3.
-
- ! call inject_particles(time,0.,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npart,npartoftype)
- ! nmax = 0
+ print "(50('*'))"
+ print*,'Edit options in '//trim(infile)//' and re-run phantomsetup'
+ print "(50('*'),/)"
 
 end subroutine setpart
 
