8caa271c74a5bdca2df44a3da6ddf70143ae95b6
Lionel Siess <lionel.siess@astro.ulb.ac.be>
2019-02-11 18:14:49 +0100

    fix missing #endif statement

--- src/main/step_leapfrog.F90
+++ src/main/step_leapfrog.F90
@@ -388,6 +388,7 @@ subroutine step(npart,nactive,t,dtsph,dtextforce,dtnew)
              vxyzu(1:3,i) = vxyzu(1:3,i) + dti*fxyzu(1:3,i)
 #else
              vxyzu(:,i) = vxyzu(:,i) + dti*fxyzu(:,i)
+#endif
              if (use_dustgrowth .and. itype==idust) dustprop(:,i) = dustprop(:,i) + dti*ddustprop(:,i)
              if (itype==igas) then
                 if (mhd)          Bevol(:,i)    = Bevol(:,i)    + dti*dBevol(:,i)
@@ -481,6 +482,7 @@ subroutine step(npart,nactive,t,dtsph,dtextforce,dtnew)
           vxyzu(1:3,i) = vxyzu(1:3,i) - hdtsph*fxyzu(1:3,i)
 #else
           vxyzu(:,i) = vxyzu(:,i) - hdtsph*fxyzu(:,i)
+#endif
           if (itype==idust .and. use_dustgrowth) dustprop(:,i) = dustprop(:,i) - hdtsph*ddustprop(:,i)
           if (itype==igas) then
              if (mhd)          Bevol(:,i)  = Bevol(:,i)  - hdtsph*dBevol(:,i)
