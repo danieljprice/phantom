92f14c285e9b9ef5383a92c34ec9961f2f97534d
David Liptai <dliptai@hotmail.com>
2019-02-20 18:55:47 +1100

    (hdf5) removed unused routines write_array_4d and write_array_6d

--- src/main/utils_outputhdf5.F90
+++ src/main/utils_outputhdf5.F90
@@ -9,7 +9,7 @@ module utils_outputhdf5
  private
 
  interface write_to_hdf5
-  module procedure write_scalar, write_array_1d, write_array_2d, write_array_3d, write_array_4d, write_array_6d, &
+  module procedure write_scalar, write_array_1d, write_array_2d, write_array_3d, &
                    write_scalar_int, write_scalar_intkind8, write_intarray_1d, write_intarray_1dkind8, write_intarray_1dkind1, &
                    write_array_1dkind4, write_array_2dkind4, write_string
  end interface
@@ -229,72 +229,6 @@ subroutine write_array_3d(x, name, id, error)
  error = maxval(abs(errors))
 
 end subroutine write_array_3d
-
-subroutine write_array_4d(x, name, id, error)
- real,           intent(in) :: x(:,:,:,:)
- character(*),   intent(in) :: name
- integer(HID_T), intent(in) :: id
- integer,        intent(out):: error
-
- integer, parameter  :: ndims = 4
- integer(HSIZE_T)    :: xshape(ndims)
- integer(HID_T)      :: dspace_id
- integer(HID_T)      :: dset_id
- integer :: errors(5)
-
- xshape = shape(x)
-
- ! Create dataspace
- call h5screate_simple_f(ndims, xshape, dspace_id, errors(1))
-
- ! Create dataset in file
- call h5dcreate_f(id, name, H5T_NATIVE_DOUBLE, dspace_id, dset_id, errors(2))
-
- ! Write to file
- call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, x, xshape, errors(3))
-
- ! Close dataset
- call h5dclose_f(dset_id, errors(4))
-
- ! Closet dataspace
- call h5sclose_f(dspace_id, errors(5))
-
- error = maxval(abs(errors))
-
-end subroutine write_array_4d
-
-subroutine write_array_6d(x, name, id, error)
- real,           intent(in) :: x(:,:,:,:,:,:)
- character(*),   intent(in) :: name
- integer(HID_T), intent(in) :: id
- integer,        intent(out):: error
-
- integer, parameter  :: ndims = 6
- integer(HSIZE_T)    :: xshape(ndims)
- integer(HID_T)      :: dspace_id
- integer(HID_T)      :: dset_id
- integer :: errors(5)
-
- xshape = shape(x)
-
- ! Create dataspace
- call h5screate_simple_f(ndims, xshape, dspace_id, errors(1))
-
- ! Create dataset in file
- call h5dcreate_f(id, name, H5T_NATIVE_DOUBLE, dspace_id, dset_id, errors(2))
-
- ! Write to file
- call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, x, xshape, errors(3))
-
- ! Close dataset
- call h5dclose_f(dset_id, errors(4))
-
- ! Closet dataspace
- call h5sclose_f(dspace_id, errors(5))
-
- error = maxval(abs(errors))
-
-end subroutine write_array_6d
 
 subroutine write_scalar_int(x, name, id, error)
  integer,        intent(in) :: x
