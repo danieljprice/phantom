46eda9c38efffdaf4b9302ef567c75381f1a69ac
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-02-22 17:19:09 +1100

    (HDF5) only write 1:npart particles for full dump This is an issue because we don't allocate the correct array sizes in phantomsetup yet.

--- src/main/readwrite_dumps_hdf5.F90
+++ src/main/readwrite_dumps_hdf5.F90
@@ -296,23 +296,36 @@ subroutine write_dump(t,dumpfile,fulldump,ntotal)
 
  call create_hdf5file(trim(dumpfile)//'.h5',hdf5_file_id,error)
  if (error/=0) call fatal('write_fulldump_hdf5','could not open file')
- call write_hdf5_header(hdf5_file_id,error,trim(fileident),maxtypes,nblocks,isink,nptmass,ndustlarge,ndustsmall,idust,&
-                        phantom_version_major,phantom_version_minor,phantom_version_micro,                             &
-                        int(nparttot),int(npartoftypetot),iexternalforce,ieos,t,dtmax,gamma,rhozero,                   &
-                        polyk,hfact,tolh,C_cour,C_force,alpha,alphau,alphaB,polyk2,qfacdisc,                           &
-                        massoftype,Bextx,Bexty,Bextz,xmin,xmax,ymin,ymax,zmin,zmax,get_conserv,                        &
-                        etot_in,angtot_in,totmom_in,mdust_in,grainsize,graindens,udist,umass,utime,unit_Bfield         )
+
+ call write_hdf5_header(hdf5_file_id,error,trim(fileident),maxtypes,nblocks,   &
+                        isink,nptmass,ndustlarge,ndustsmall,idust,             &
+                        phantom_version_major,phantom_version_minor,           &
+                        phantom_version_micro,int(nparttot),                   &
+                        int(npartoftypetot),iexternalforce,ieos,t,dtmax,gamma, &
+                        rhozero, polyk,hfact,tolh,C_cour,C_force,alpha,alphau, &
+                        alphaB,polyk2,qfacdisc,massoftype,Bextx,Bexty,Bextz,   &
+                        xmin,xmax,ymin,ymax,zmin,zmax,get_conserv,etot_in,     &
+                        angtot_in,totmom_in,mdust_in,grainsize,graindens,      &
+                        udist,umass,utime,unit_Bfield)
  if (error/=0) call fatal('write_fulldump_hdf5','could not write header')
+
  if (fulldump) then
-    call write_hdf5_arrays(hdf5_file_id,error,xyzh,vxyzu,int(iphase),pressure,alphaind,dtind,poten,xyzmh_ptmass,            &
-                           vxyz_ptmass,Bxyz,Bevol,divcurlB,divBsymm,eta_nimhd,                                               &
-                           dustfrac(1:ndusttypes,:),tstop(1:ndustsmall,:),deltav(:,1:ndustsmall,:),dustprop,st,              &
-                           abundance,temperature,divcurlv,luminosity,beta_pr,                                                &
-                           const_av,ind_timesteps,gravity,nptmass,mhd,maxBevol,ndivcurlB,mhd_nonideal,use_dust,              &
-                           use_dustfrac,use_dustgrowth,h2chemistry,store_temperature,ndivcurlv,lightcurve,prdrag,isothermal  )
+    call write_hdf5_arrays(hdf5_file_id,error,xyzh(:,1:npart),vxyzu(:,1:npart), &
+                           int(iphase(1:npart)),pressure, &
+                           alphaind,dtind,poten,xyzmh_ptmass,vxyz_ptmass,Bxyz, &
+                           Bevol,divcurlB,divBsymm,eta_nimhd,                  &
+                           dustfrac(1:ndusttypes,:),tstop(1:ndustsmall,:),     &
+                           deltav(:,1:ndustsmall,:),dustprop,st,abundance,     &
+                           temperature,divcurlv(:,1:npart),luminosity,beta_pr,const_av,   &
+                           ind_timesteps,gravity,nptmass,mhd,maxBevol,         &
+                           ndivcurlB,mhd_nonideal,use_dust,use_dustfrac,       &
+                           use_dustgrowth,h2chemistry,store_temperature,       &
+                           ndivcurlv,lightcurve,prdrag,isothermal)
  else
-    call write_hdf5_arrays_small(hdf5_file_id,error,xyzh,int(iphase),xyzmh_ptmass,Bxyz,dustfrac,dustprop,st,   &
-                                 abundance,luminosity,nptmass,mhd,use_dust,use_dustgrowth,h2chemistry,lightcurve)
+    call write_hdf5_arrays_small(hdf5_file_id,error,xyzh,int(iphase),       &
+                                 xyzmh_ptmass,Bxyz,dustfrac,dustprop,st,    &
+                                 abundance,luminosity,nptmass,mhd,use_dust, &
+                                 use_dustgrowth,h2chemistry,lightcurve)
  endif
  if (error/=0) call fatal('write_fulldump_hdf5','could not write arrays')
  call close_hdf5file(hdf5_file_id,error)
