eedba9eef0b0d1584d67d46c1fc195c835970373
David Liptai <dliptai@hotmail.com>
2019-02-27 18:01:12 +1100

    (hdf5) phantom2hdf5: can now be run on multiple files

--- src/utils/phantom2hdf5.f90
+++ src/utils/phantom2hdf5.f90
@@ -5,7 +5,7 @@ program phantom2hdf5
  use readwrite_dumps,      only:read_dump,write_fulldump
  use readwrite_dumps_hdf5, only:read_dump_hdf5=>read_dump, write_fulldump_hdf5=>write_fulldump
  implicit none
- integer :: nargs
+ integer :: nargs,iarg
  character(len=120) :: dumpfile
  real :: time
  integer :: ierr
@@ -16,21 +16,24 @@ program phantom2hdf5
 !--get name of run from the command line
 !
  nargs = command_argument_count()
- if (nargs /= 1) then
+ if (nargs < 1) then
     print "(a,/)",trim(tagline)
-    print "(a)",' Usage: phantom2hdf5 dumpfile'
+    print "(a)",' Usage: phantom2hdf5 dumpfile(s)'
     stop
  endif
- call get_command_argument(1,dumpfile)
 
  print "(/,a,/)",' Phantom2hdf5: The best conversion in the west...'
-!
-!--read particle setup from dumpfile
-!
- call read_dump(trim(dumpfile),time,hfact,idisk1,iprint,0,1,ierr)
- if (ierr /= 0) stop 'error reading dumpfile'
 
- call write_fulldump_hdf5(time,trim(dumpfile))
+ over_args: do iarg=1,nargs
+    call get_command_argument(iarg,dumpfile)
+   !
+   !--read particle setup from dumpfile
+   !
+    call read_dump(trim(dumpfile),time,hfact,idisk1,iprint,0,1,ierr)
+    if (ierr /= 0) stop 'error reading dumpfile'
+
+    call write_fulldump_hdf5(time,trim(dumpfile))
+ enddo over_args
 
  print "(/,a,/)",' Phantom2hdf5: Enjoy a better file format.'
 
