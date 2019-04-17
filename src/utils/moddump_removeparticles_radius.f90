7f890e2b66185ff7931c3120e17a60556334899c
Arnaud Vericel <arnaud.vericel@univ-lyon1.fr>
2019-04-17 15:57:32 +0200

    (utils) can now moddump and remove particles outside and/or inside some radius [skip ci]

--- src/utils/moddump_removeparticles_radius.f90
+++ src/utils/moddump_removeparticles_radius.f90
@@ -22,7 +22,8 @@
 !--------------------------------------------------------------------------
 module moddump
 
- use part, only:delete_particles_inside_radius
+ use part,         only:delete_particles_inside_radius,delete_particles_outside_sphere
+ use prompting,    only:prompt
 
  implicit none
 
@@ -34,20 +35,48 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  integer, dimension(:), intent(inout) :: npartoftype
  real, dimension(:), intent(inout) :: massoftype
  real, dimension(:,:), intent(inout) :: xyzh,vxyzu
- real, dimension(3) :: center
- real :: radius
+ real, dimension(3) :: incenter,outcenter
+ real :: inradius,outradius
+ logical :: icutinside,icutoutside
 
- print*,' Phantommoddump: Remove particles inside a particular radius'
+ icutinside   = .false.
+ icutoutside  = .false.
+ incenter(:)  = 0.
+ outcenter(:) = 0.
+ inradius     = 10.
+ outradius    = 200.
+
  !
- !--set the center and the radius
+ !--set the centers and the radius
  !
- center(:)=0.
- radius=10.
+ call prompt('Deleting particles inside a given radius ?',icutinside)
+ call prompt('Deleting particles outside a given radius ?',icutoutside)
+ if (icutinside) then
+    call prompt('Enter the inward radius in au',inradius,0.)
+    call prompt('Enter the x coordinates of the center of that sphere',incenter(1),0.)
+    call prompt('Enter the y coordinates of the center of that sphere',incenter(2),0.)
+    call prompt('Enter the z coordinates of the center of that sphere',incenter(3),0.)
+ endif
+ if (icutoutside) then
+    call prompt('Enter the outward radius in au',outradius,0.)
+    call prompt('Enter the x coordinates of the center of that sphere',outcenter(1),0.)
+    call prompt('Enter the y coordinates of the center of that sphere',outcenter(2),0.)
+    call prompt('Enter the z coordinates of the center of that sphere',outcenter(3),0.)
+ endif
+
+ if (icutinside) print*,' Phantommoddump: Remove particles inside a particular radius'
  !
  !--removing particles
  !
- print*,'Removing particles inside radius ',radius
- call delete_particles_inside_radius(center,radius,npart,npartoftype)
+ print*,'Removing particles inside radius ',inradius
+ call delete_particles_inside_radius(incenter,inradius,npart,npartoftype)
+
+ if (icutoutside) print*,' Phantommoddump: Remove particles outside a particular radius'
+ !
+ !--removing particles
+ !
+ print*,'Removing particles outside radius ',outradius
+ call delete_particles_outside_sphere(outcenter,outradius)
 
 end subroutine modify_dump
 
