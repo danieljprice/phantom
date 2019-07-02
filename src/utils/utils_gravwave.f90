2fb351cf56b59cd73f52d5144554ac1acc94c538
David Liptai <david.liptai@monash.edu>
2019-07-02 16:49:21 +1000

    (GR) utils_gravwave: small optimisations, and put everything in 1 loop

--- src/utils/utils_gravwave.f90
+++ src/utils/utils_gravwave.f90
@@ -13,48 +13,57 @@ pure subroutine calculate_strain(hx,hp,hxx,hpp,xyzh,vxyz,axyz,pmass,npart)
  real, intent(out)   :: hx,hp,hxx,hpp
  real, intent(in)    :: xyzh(:,:), vxyz(:,:), axyz(:,:), pmass
  integer, intent(in) :: npart
- real       :: q(6), ddq(6)
+ real       :: q(6),ddq(6),x,y,z,vx,vy,vz,ax,ay,az,fac,r2
  integer    :: i
  real       :: distan
 
  distan = 0.03*3.0e24
 
- ! initialise quadrupole to zero
- q(:) = 0
+ ! initialise quadrupole and second deriv to zero
+ q(:)   = 0.
+ ddq(:) = 0.
 
- ! calculate the components of the traceless quadrupole--not necessary but maybe useful
  do i=1,npart
     if(xyzh(4,i)>tiny(xyzh)) then  !if not accreted
-       q(1) = q(1)+pmass*(xyzh(1,i)*xyzh(1,i)-0.3*(xyzh(1,i)**2.+xyzh(2,i)**2.+xyzh(3,i)**2.)) !qxx
-       q(2) = q(2)+pmass*(xyzh(1,i)*xyzh(2,i)-0.3*(xyzh(1,i)**2.+xyzh(2,i)**2.+xyzh(3,i)**2.)) !qxy
-       q(3) = q(3)+pmass*(xyzh(1,i)*xyzh(3,i)-0.3*(xyzh(1,i)**2.+xyzh(2,i)**2.+xyzh(3,i)**2.)) !qxz
-       q(4) = q(4)+pmass*(xyzh(2,i)*xyzh(2,i)-0.3*(xyzh(1,i)**2.+xyzh(2,i)**2.+xyzh(3,i)**2.)) !qyy
-       q(5) = q(5)+pmass*(xyzh(2,i)*xyzh(3,i)-0.3*(xyzh(1,i)**2.+xyzh(2,i)**2.+xyzh(3,i)**2.)) !qyz
-       q(6) = q(6)+pmass*(xyzh(3,i)*xyzh(3,i)-0.3*(xyzh(1,i)**2.+xyzh(2,i)**2.+xyzh(3,i)**2.)) !qzz
+      x  = xyzh(1,i)
+      y  = xyzh(2,i)
+      z  = xyzh(3,i)
+      vx = vxyz(1,i)
+      vy = vxyz(2,i)
+      vz = vxyz(3,i)
+      ax = axyz(1,i)
+      ay = axyz(2,i)
+      az = axyz(3,i)
+
+      r2 = dot_product(xyzh(1:3,i),xyzh(1:3,i))
+
+      ! calculate the components of the traceless quadrupole--not necessary but maybe useful
+       q(1) = q(1) + pmass*(x*x-0.3*r2) !qxx
+       q(2) = q(2) + pmass*(x*y-0.3*r2) !qxy
+       q(3) = q(3) + pmass*(x*z-0.3*r2) !qxz
+       q(4) = q(4) + pmass*(y*y-0.3*r2) !qyy
+       q(5) = q(5) + pmass*(y*z-0.3*r2) !qyz
+       q(6) = q(6) + pmass*(z*z-0.3*r2) !qzz
+
+      ! calculate the second time derivative of the traceless quadrupole
+       ddq(1) = ddq(1) + pmass*(2.*vx*vx+x*ax+x*ax) !ddqxx
+       ddq(2) = ddq(2) + pmass*(2.*vx*vy+x*ay+y*ax) !ddqxy
+       ddq(3) = ddq(3) + pmass*(2.*vx*vz+x*az+z*ax) !ddqxz
+       ddq(4) = ddq(4) + pmass*(2.*vy*vy+y*ay+y*ay) !ddqyy
+       ddq(5) = ddq(5) + pmass*(2.*vy*vz+y*az+z*ay) !ddqyz
+       ddq(6) = ddq(6) + pmass*(2.*vz*vz+z*az+z*az) !ddqzz
     end if
  enddo
 
- ! initialise the second time derivative of the quadrupole to zero
- ddq(:)=0
-
- ! calculate the second time derivative of the traceless quadrupole
- do i=1,npart
-    if(xyzh(4,i)>tiny(xyzh)) then !if not accreted
-       ddq(1) = ddq(1)+pmass*(2*vxyz(1,i)*vxyz(1,i)+xyzh(1,i)*axyz(1,i)+xyzh(1,i)*axyz(1,i)) !ddqxx
-       ddq(2) = ddq(2)+pmass*(2*vxyz(1,i)*vxyz(2,i)+xyzh(1,i)*axyz(2,i)+xyzh(2,i)*axyz(1,i)) !ddqxy
-       ddq(3) = ddq(3)+pmass*(2*vxyz(1,i)*vxyz(3,i)+xyzh(1,i)*axyz(3,i)+xyzh(3,i)*axyz(1,i)) !ddqxz
-       ddq(4) = ddq(4)+pmass*(2*vxyz(2,i)*vxyz(2,i)+xyzh(2,i)*axyz(2,i)+xyzh(2,i)*axyz(2,i)) !ddqyy
-       ddq(5) = ddq(5)+pmass*(2*vxyz(2,i)*vxyz(3,i)+xyzh(2,i)*axyz(3,i)+xyzh(3,i)*axyz(2,i)) !ddqyz
-       ddq(6) = ddq(6)+pmass*(2*vxyz(3,i)*vxyz(3,i)+xyzh(3,i)*axyz(3,i)+xyzh(3,i)*axyz(3,i)) !ddqzz
-    end if
- enddo
+ fac = gg*c**(-4)*distan**(-1)*umass*udist**(2)*utime**(-2)
 
  ! gw strain in the direction perpendicular to the orbit, theta=0
- hp = gg*c**(-4.)*distan**(-1.)*umass*udist**(2.)*utime**(-2.)*(ddq(1)-ddq(4))
- hx = 2.*gg*c**(-4.)*distan**(-1.)*umass*udist**(2.)*utime**(-2.)*ddq(2)
+ hp = fac*(ddq(1)-ddq(4))
+ hx = 2.*fac*ddq(2)
+
  ! gw strain in the plane of the orbit, theta=pi/2
- hpp = gg*c**(-4.)*distan**(-1.)*umass*udist**(2.)*utime**(-2.)*(ddq(1)-ddq(6))
- hxx = -2.*gg*c**(-4.)*distan**(-1.)*umass*udist**(2.)*utime**(-2.)*ddq(3)
+ hpp = fac*(ddq(1)-ddq(6))
+ hxx = -2.*fac*ddq(3)
 
 end subroutine calculate_strain
 
