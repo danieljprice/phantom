danieljprice/phantom

commit 0107eb48cba1b954d1a7a292500030a434b5f509

BUG FIX with test suite failure in rwdumps (dustfrac) - not serious

src/main/readwrite_dumps.F90
@@ -1857,7 +1857,7 @@ subroutine unfill_rheader(hdr,phantomdump,ntypesinfile,&
  use eos,            only:polyk,gamma,polyk2,qfacdisc,extract_eos_from_hdr
  use options,        only:ieos,iexternalforce
  use part,           only:massoftype,Bextx,Bexty,Bextz,mhd,periodic,&
-                          maxtypes,grainsize,graindens
+                          maxtypes,grainsize,graindens,ndusttypes
  use initial_params, only:get_conserv,etot_in,angtot_in,totmom_in,mdust_in
  use setup_params,   only:rhozero
  use externalforces, only:read_headeropts_extern,extract_iextern_from_hdr
@@ -1985,7 +1985,7 @@ subroutine unfill_rheader(hdr,phantomdump,ntypesinfile,&
  call extract('etot_in',    etot_in,    hdr,ierrs(2))
  call extract('angtot_in',  angtot_in,  hdr,ierrs(3))
  call extract('totmom_in',  totmom_in,  hdr,ierrs(4))
- call extract('mdust_in',   mdust_in,   hdr,ierrs(5))
+ call extract('mdust_in',   mdust_in(1:ndusttypes), hdr,ierrs(5))
  if (any(ierrs(1:4) /= 0)) then
     write(*,*) 'ERROR reading values to verify conservation laws.  Resetting initial values.'
     get_conserv = 1.0
@@ -1996,8 +1996,8 @@ subroutine unfill_rheader(hdr,phantomdump,ntypesinfile,&
 
  !--pull grain size and density arrays
  if (use_dust) then
-    call extract('grainsize',grainsize,hdr,ierrs(1))
-    call extract('graindens',graindens,hdr,ierrs(2))
+    call extract('grainsize',grainsize(1:ndusttypes),hdr,ierrs(1))
+    call extract('graindens',graindens(1:ndusttypes),hdr,ierrs(2))
     if (any(ierrs(1:2) /= 0)) then
        write(*,*) 'ERROR reading grain size/density from file header'
     endif

src/tests/test_rwdump.F90
@@ -37,7 +37,7 @@ subroutine test_rwdump(ntests,npass)
                            maxp,poten,gravity,use_dust,dustfrac,xyzmh_ptmass,&
                            nptmass,nsinkproperties,xyzh_label,xyzmh_ptmass_label,&
                            dustfrac_label,vxyz_ptmass,vxyz_ptmass_label,&
-                           vxyzu_label,set_particle_type,iphase,ndustsmall
+                           vxyzu_label,set_particle_type,iphase,ndustsmall,ndusttypes
  use dim,             only:maxp,maxdusttypes
  use memory,          only:allocate_memory,deallocate_memory
  use testutils,       only:checkval
@@ -50,6 +50,7 @@ subroutine test_rwdump(ntests,npass)
  use mpiutils,        only:barrier_mpi
  use dump_utils,      only:read_array_from_file
  use timing,          only:getused,printused
+ use options,         only:use_dustfrac
  integer, intent(inout) :: ntests,npass
  integer :: nfailed(64)
  integer :: i,j,ierr,itest,ngas,ndust,ntot,maxp_old,iu
@@ -114,8 +115,10 @@ subroutine test_rwdump(ntests,npass)
           poten(i) = 15._4
        endif
        if (use_dust) then
+          use_dustfrac = .true.
           ndustsmall = maxdusttypes
-          dustfrac(:,i) = 16._4
+          ndusttypes = ndustsmall
+          dustfrac(:,i) = 0.16_4
        endif
     enddo
     nptmass = 10
@@ -266,7 +269,7 @@ subroutine test_rwdump(ntests,npass)
        endif
        if (use_dust) then
           do i = 1,ndustsmall
-             call checkval(npart,dustfrac(i,:),16.,tol,nfailed(16),'dustfrac')
+             call checkval(npart,dustfrac(i,:),0.16,real(epsilon(0._4)),nfailed(16),'dustfrac')
           enddo
        endif
     endif

commit 2d6eb50e9e9d47312cecb28da59ddafa471c5c6a

BUG FIX with grain sizes = 0 when running phantom; caused by 0107eb4

src/main/readwrite_dumps.F90
@@ -1710,7 +1710,20 @@ subroutine unfill_header(hdr,phantomdump,got_tags,nparttot, &
     call extract('npartoftype',npartoftype(1:ntypesinfile),hdr,ierr1)
  endif
  if (id==master) write(*,*) 'npart(total) = ',nparttot
-
+!
+!--number of dust species
+!
+ if (use_dust) then
+    call extract('ndustsmall',ndustsmall,hdr,ierrs(1))
+    call extract('ndustlarge',ndustlarge,hdr,ierrs(2))
+    ndusttypes = ndustsmall + ndustlarge
+    if (any(ierrs(1:2) /= 0)) then
+       write(*,*) 'ERROR reading number of small/large grain types from file header'
+    endif
+ endif
+!
+!--units
+!
  call extract('udist',udist,hdr,ierrs(1))
  call extract('umass',umass,hdr,ierrs(2))
  call extract('utime',utime,hdr,ierrs(3))
@@ -1723,16 +1736,6 @@ subroutine unfill_header(hdr,phantomdump,got_tags,nparttot, &
 !--default real
  call unfill_rheader(hdr,phantomdump,ntypesinfile,&
                      tfile,hfactfile,alphafile,iprint,ierr)
-
- if (use_dust) then
-    call extract('ndustsmall',ndustsmall,hdr,ierrs(1))
-    call extract('ndustlarge',ndustlarge,hdr,ierrs(2))
-    ndusttypes = ndustsmall + ndustlarge
-    if (any(ierrs(1:2) /= 0)) then
-       write(*,*) 'ERROR reading number of small/large grain types from file header'
-    endif
- endif
-
  if (ierr /= 0) return
 
  if (id==master) write(iprint,*) 'time = ',tfile