cf1056239fe0e90ae6ea0eba1a35f2b1d4fa2a34
Daniel Price <daniel.price@monash.edu>
2019-09-13 17:52:45 +1000

    BUG FIX with memory allocation for dvdx; possibly affecting viscosity switch if DISC_VISCOSITY=no

--- src/main/config.F90
+++ src/main/config.F90
@@ -159,6 +159,8 @@ module dim
 #else
  integer, parameter :: ndivcurlv = 1
 #endif
+ ! storage of velocity derivatives
+ integer :: maxdvdx = 0  ! set to maxp when memory allocated
 
  ! periodic boundaries
 #ifdef PERIODIC
@@ -204,16 +206,6 @@ module dim
 #else
  logical, parameter :: mhd_nonideal = .false.
 #endif
-
-!--------------------
-! Velocity gradients
-!--------------------
-!
-! storage of velocity derivatives, necessary if
-! physical viscosity is done with two
-! first derivatives or if dust is used
-!
- integer, parameter :: maxdvdx = maxp_hard ! TO FIX
 
 !--------------------
 ! H2 Chemistry
@@ -356,6 +348,7 @@ subroutine update_max_sizes(n)
 ! Very convoluted, but follows original logic...
  maxphase = maxan
  maxgradh = maxan
+ maxdvdx = maxan
 
 end subroutine update_max_sizes
 
