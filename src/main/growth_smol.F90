3c8a8097486e4a9ba41a56daba141fc035475873
Daniel Price <daniel.price@monash.edu>
2020-04-08 14:01:27 +1000

    [indent-bot] standardised indentation

--- src/main/growth_smol.F90
+++ src/main/growth_smol.F90
@@ -39,37 +39,37 @@ contains
 !+
 !--------------------------------------------------------------------------
 subroutine grain_growth_smol(ndusttypes,dustfraci,rhoi,grainsize,graindens,dt)
-  use smol2other, only:grain_growth   ! smoluchowsky library routine
-  use units,      only:utime,umass,unit_density
-  use physcon,    only:pi
-  integer, intent(in) :: ndusttypes
-  real,    intent(in)    :: grainsize(ndusttypes),graindens(ndusttypes)
-  real,    intent(inout) :: dustfraci(ndusttypes)
-  real,    intent(in)    :: rhoi,dt
-  real :: rhodust(ndusttypes),grainmass(ndusttypes)
-  !
-  ! convert from dust fraction to dust density in g/cm^3
-  !
-  rhodust = rhoi*dustfraci*unit_density
-  !
-  ! convert grain sizes to grain mass in g
-  !
-  grainmass = 4./3.*pi*grainsize**3*graindens*umass
-  !
-  ! call Smoluchowsky solver
-  !
-  print*,' calling grain growth ntypes = ',ndusttypes
-  print*,' grainmass = ',grainmass
-  print*,' rhodust = ',rhodust
+ use smol2other, only:grain_growth   ! smoluchowsky library routine
+ use units,      only:utime,umass,unit_density
+ use physcon,    only:pi
+ integer, intent(in) :: ndusttypes
+ real,    intent(in)    :: grainsize(ndusttypes),graindens(ndusttypes)
+ real,    intent(inout) :: dustfraci(ndusttypes)
+ real,    intent(in)    :: rhoi,dt
+ real :: rhodust(ndusttypes),grainmass(ndusttypes)
+ !
+ ! convert from dust fraction to dust density in g/cm^3
+ !
+ rhodust = rhoi*dustfraci*unit_density
+ !
+ ! convert grain sizes to grain mass in g
+ !
+ grainmass = 4./3.*pi*grainsize**3*graindens*umass
+ !
+ ! call Smoluchowsky solver
+ !
+ print*,' calling grain growth ntypes = ',ndusttypes
+ print*,' grainmass = ',grainmass
+ print*,' rhodust = ',rhodust
 
-  call grain_growth(ndusttypes,rhodust,grainmass,dt,utime)
+ call grain_growth(ndusttypes,rhodust,grainmass,dt,utime)
 
-  print*,' AFTER GROWTH: rhodust = ',rhodust
-  !
-  ! convert from dust density to dust fraction
-  !
-  dustfraci = rhodust/rhoi
+ print*,' AFTER GROWTH: rhodust = ',rhodust
+ !
+ ! convert from dust density to dust fraction
+ !
+ dustfraci = rhodust/rhoi
 
- end subroutine grain_growth_smol
+end subroutine grain_growth_smol
 
 end module growth_smol
