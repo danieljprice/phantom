d21af8d5a9b23dce9a2266556b5a5d3d5eee4ee5
Bec Nealon <rebecca.nealon@leicester.ac.uk>
2020-07-29 15:05:10 +0100

    (splitting) merging and splitting moddumps re-formatted for future use

--- src/utils/splitpart.f90
+++ src/utils/splitpart.f90
@@ -22,7 +22,6 @@
 !--------------------------------------------------------------------------
 module splitpart
  implicit none
- integer, parameter :: ires = 1 ! use 12 particles per sphere
 
 contains
 
@@ -31,82 +30,44 @@ contains
 !  split particles into nchild particle
 !+
 !--------------------------------------------------------------------------
-subroutine split_particles(nchild,npart,npartoftype,xyzh,vxyzu,massoftype,ierr)
- use injectutils, only:get_parts_per_sphere
+subroutine split_particles(nchild,iparent,xyzh_parent,vxyzu_parent, &
+           lattice_type,ires,xyzh_child,vxyzu_child)
  use icosahedron, only:pixel2vector,compute_corners,compute_matrices
- use part,        only:copy_particle
- use io,          only:error
  integer, intent(in)    :: nchild
- integer, intent(inout) :: npart,npartoftype(:)
- real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
- real,    intent(inout) :: massoftype(:)
- integer, intent(out)   :: ierr
- real, allocatable, dimension(:,:) :: shifts
- integer :: npart_per_sphere,iold,inew,j,iseed
- real    :: dhfac,dx(3),sep,vel,vmax,dv(3)
- real    :: geodesic_R(0:19,3,3), geodesic_v(0:11,3)
+ integer, intent(in)    :: iparent,lattice_type,ires
+ real,    intent(in)    :: xyzh_parent(:),vxyzu_parent(:)
+ real,    intent(out)   :: xyzh_child(4,nchild),vxyzu_child(3,nchild)
+ integer :: j,iseed
+ real    :: dhfac,dx(3),sep,geodesic_R(0:19,3,3), geodesic_v(0:11,3)
 
- ierr = 0
- if (nchild <= 0) then
-    npart_per_sphere = get_parts_per_sphere(ires)
+ if (lattice_type == 0) then
     call compute_matrices(geodesic_R)
     call compute_corners(geodesic_v)
  else
     ! initialise random number generator
-    npart_per_sphere = nchild - 1
     iseed = -6542
  endif
 
- !--check there is enough memory
- if (size(xyzh(1,:)) < npart*(npart_per_sphere+1)) then
-    call error('split_particles','not enough memory, increase MAXP and recompile')
-    ierr = 1
-    return
- endif
+ ! first child sits where the parent was
+ ! for now, assume all children have same velocity as parent
+ xyzh_child(1:3,1) = xyzh_parent(1:3)
+ vxyzu_child(:,1)  = vxyzu_parent(1:3)
+ sep = xyzh_parent(4)
 
- !--divide mass of all particles by 13
- massoftype(:) = massoftype(:) / (npart_per_sphere + 1)
-
- !--update npartoftype
- npartoftype(:) = npartoftype*(npart_per_sphere+1)
-
- !--amend smoothing length of original particles
- !--also find the maximum velocity
- vmax = -1.0
- dhfac = 1./(npart_per_sphere + 1)**(1./3.)
- do iold=1,npart
-    xyzh(4,iold) = xyzh(4,iold)*dhfac
-    dv = vxyzu(1:3,iold)
-    vel = sqrt(dot_product(dv,dv))
-    if (vel > vmax) vmax = vel
+ do j=2,nchild
+   ! find positions of original particles
+   if (lattice_type == 0) then
+     call pixel2vector(j-2,ires,geodesic_R,geodesic_v,dx)
+   else
+     call sample_kernel(iseed,dx)
+   endif
+      xyzh_child(1:3,j) = xyzh_parent(1:3) + sep*dx(:)
+      vxyzu_child(1:3,j) = vxyzu_parent(1:3)
  enddo
 
- !--find positions of the new particles
- inew = npart ! put new particles after original parts
- do iold=1,npart
-    sep = xyzh(4,iold)
-    ! add 12 new child particles
-    do j=0,npart_per_sphere-1
-       inew = inew + 1
-       ! copy all properties from original particle
-       call copy_particle(iold,inew)
-       ! find positions of original particles
-       if (nchild <= 0) then
-          call pixel2vector(j,ires,geodesic_R,geodesic_v,dx)
-       else
-          call sample_kernel(iseed,dx)
-       endif
-       xyzh(1:3,inew) = xyzh(1:3,iold) + sep*dx(:)
-    enddo
-    ! amend smoothing length of original particle
-    npart = npart + npart_per_sphere
- enddo
-
-!-- now shift all the particles a little
-allocate(shifts(3,npart))
-call shift_particles(npart,xyzh,vmax,0.0001,100.0,shifts)
-xyzh(1:3,1:npart) = xyzh(1:3,1:npart) + shifts(:,:)
-deallocate(shifts)
+ !--amend smoothing length
+ dhfac = 1./(nchild)**(1./3.)
+ xyzh_child(4,:) = xyzh_parent(4)*dhfac
 
 end subroutine split_particles
 
@@ -124,19 +85,25 @@ subroutine sample_kernel(iseed,dx)
 
 end subroutine sample_kernel
 
-subroutine shift_particles(npart,xyzh,vmax,deltat,beta,shifts)
-  real, intent(in)    :: xyzh(:,:)
-  real, intent(in)    :: deltat,vmax,beta
+subroutine shift_particles(npart,xyzh,vxyzu,deltat,beta,shifts)
+  use kernel, only:radkern2
+  real, intent(in)    :: xyzh(:,:),vxyzu(:,:)
+  real, intent(in)    :: deltat,beta
   integer, intent(in) :: npart
   real, intent(out)   :: shifts(3,npart)
   integer             :: i,j,neighbours
-  real                :: rnaught,rij2,dr3
+  real                :: rnaught,rij2,dr3,vel2,vmax
   real                :: q2,rij(3),rsum(3)
+
+  vmax = tiny(vmax)
+  vel2 = 0.
 
   do i = 1,npart
     rnaught = 0.
     neighbours = 0
     rsum = 0.
+    vel2 = dot_product(vxyzu(1:3,i),vxyzu(1:3,i))
+    if (vel2 > vmax) vmax = vel2
 
     over_npart: do j = 1,npart
       if (i == j) cycle over_npart
@@ -144,7 +111,7 @@ subroutine shift_particles(npart,xyzh,vmax,deltat,beta,shifts)
       rij2 = dot_product(rij,rij)
       q2 = rij2/(xyzh(4,i)*xyzh(4,i))
 
-      if (q2 < 4.0) then !assuming defaults for testing, must be updated
+      if (q2 < radkern2) then
         neighbours = neighbours + 1
         rnaught = rnaught + sqrt(rij2)
       endif
@@ -154,8 +121,10 @@ subroutine shift_particles(npart,xyzh,vmax,deltat,beta,shifts)
     enddo over_npart
 
     rnaught = rnaught/neighbours
-    shifts(:,i) = beta*rnaught*rnaught*vmax*deltat*rsum
+    shifts(:,i) = beta*rnaught*rnaught*deltat*rsum
   enddo
+
+  shifts = shifts*sqrt(vel2)
 
 end subroutine shift_particles
 
@@ -165,31 +134,29 @@ end subroutine shift_particles
 !+
 !-----------------------------------------------------------------------
 subroutine merge_particles(nchild,children_list,mchild,npart, &
-           xyzh,vxyzu,xyzh_parent,vxyzu_parent,mparent)
- use kernel,      only:get_kernel,cnormk,radkern
+           xyzh,vxyzu,xyzh_parent,vxyzu_parent)
+ use kernel, only:get_kernel,cnormk,radkern
  integer, intent(in)    :: nchild,children_list(nchild)
  integer, intent(inout) :: npart
  real,    intent(in)    :: xyzh(:,:),vxyzu(:,:),mchild
- real,    intent(out)   :: xyzh_parent(4),vxyzu_parent(3),mparent
+ real,    intent(out)   :: xyzh_parent(4),vxyzu_parent(3)
  integer :: i,j,ichild
  real    :: h1,h31,rij_vec(3)
  real    :: qij,rij,wchild,grkernchild,rho_parent
-
- !--set mass
- mparent = mchild*nchild
 
  !-- positions and velocities from centre of mass
  xyzh_parent = 0.
  vxyzu_parent = 0.
  do i=1,nchild
    ichild = children_list(i)
-   xyzh_parent(1:3) = xyzh_parent(1:3) + mchild*xyzh(1:3,ichild)
-   vxyzu_parent(:)  = vxyzu_parent(:) + mchild*vxyzu(1:3,ichild)
+   xyzh_parent(1:3) = xyzh_parent(1:3) + xyzh(1:3,ichild)
+   vxyzu_parent(:)  = vxyzu_parent(:) + vxyzu(1:3,ichild)
  enddo
- xyzh_parent = xyzh_parent/mparent
- vxyzu_parent = vxyzu_parent/mparent
+ xyzh_parent = xyzh_parent/nchild
+ vxyzu_parent = vxyzu_parent/nchild
 
 !-- calculate density at the parent position from the children
+!-- procedure described in Vacondio et al. 2013, around Eq 21
 rho_parent = 0.
   over_npart:  do j=1,npart
     if (xyzh(4,j) < 0.) cycle over_npart
@@ -208,7 +175,7 @@ rho_parent = 0.
   enddo over_npart
 
 !-- smoothing length from density
-xyzh_parent(4) = 1.2*(mparent/rho_parent)**(1./3.)
+xyzh_parent(4) = 1.2*(nchild*mchild/rho_parent)**(1./3.)
 
 end subroutine merge_particles
 
