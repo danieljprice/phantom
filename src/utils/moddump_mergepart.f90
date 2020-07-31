8f4658d7b57ec70ca9562febd50272680d263575
Bec Nealon <rebecca.nealon@leicester.ac.uk>
2020-07-31 20:40:22 +0100

    (splitting/merging) changed to use copy_particle and to remove large arrays

--- src/utils/moddump_mergepart.f90
+++ src/utils/moddump_mergepart.f90
@@ -23,37 +23,44 @@
 !--------------------------------------------------------------------------
 module moddump
  implicit none
- integer, parameter :: nchild = 13
+ integer, parameter :: nchild = 10
 
 contains
 
 subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  use splitpart, only:merge_particles
  use part,      only:igas,kill_particle,delete_dead_or_accreted_particles
+ use part,      only:isdead_or_accreted,copy_particle
  use io,        only:fatal,error
  integer, intent(inout) :: npart
  integer, intent(inout) :: npartoftype(:)
  real,    intent(inout) :: massoftype(:)
  real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
- integer :: ierr,nactive,nparent,remainder, i,j,k
+ integer :: ierr,nparent,remainder, i,j,k
  integer :: on_list(npart), children_list(nchild)
- integer :: ichild,iparent,child_found
- logical :: iactive(npart)
- real, allocatable, dimension(:,:) :: xyzh_parent,vxyzu_parent,xyzh_in,vxyzu_in
+ integer :: ichild,iparent,child_found,nactive
  real    :: rik(3),rik2,rikmax
 
 ierr = 0
 
  !-- how many active particles (this routine currently ignores inactive particles)
  nactive = 0
- iactive = .false.
- do i=1,npart
-   if (xyzh(4,i) > 0.) then
+ do i = 1,npart
+   if (.not.isdead_or_accreted(xyzh(4,i))) then
      nactive = nactive + 1
-     iactive(i) = .true.
+   else
+     call kill_particle(i,npartoftype)
    endif
  enddo
- if (nactive < npart) print*,'Discarding inactive particles'
+
+ print*,'nactive',nactive,'npart',npart
+
+ if (nactive < npart) then
+   call delete_dead_or_accreted_particles(npart,npartoftype)
+   print*,'Discarding inactive particles'
+ endif
+
+ print*,'nactive',nactive,'npart',npart
 
  !-- check number of parent particles
  nparent = floor(real(nactive)/real(nchild))
@@ -64,31 +71,32 @@ ierr = 0
     ierr = 1
     return
   elseif (remainder > 0) then
-    ! ignore a couple
-    do i = 1,remainder
-      if (xyzh(4,npart-i) > 0.) iactive(npart-i) = .false.
-    enddo
+    ! we just forget about these later on
     print*,'Ignoring',remainder,'particles to get an even split'
  endif
 
- !-- arrays for new parent particles
- allocate(xyzh_parent(4,nparent),vxyzu_parent(3,nparent))
- allocate(xyzh_in(4,nchild),vxyzu_in(3,nchild))
+ !--check there is enough memory
+ if (size(xyzh(1,:)) < nparent + npart) then
+    call error('merge_particles','not enough memory, increase MAXP and recompile')
+    ierr = 1
+    return
+ endif
 
  iparent = 0
  ichild = 0
  on_list = -1
+ children_list = 0
+ i = 0
 
-over_parent: do i=1,npart
-  !already on the list or inactive
-  if (on_list(i) > 0 .or. .not.iactive(i)) cycle over_parent
-  children_list = 0
-  ichild = 0
+over_parent: do while (iparent < nparent) ! until all the children are found
+  i = i + 1
+  !already on the list
+  if (on_list(i) > 0) cycle over_parent
 
   ! first child
   iparent = iparent + 1
-  ichild = ichild + 1
-  on_list(i) = 1
+  ichild = 1
+  on_list(i) = i
   children_list(ichild) = i
 
   ! find nearby children
@@ -98,7 +106,7 @@ over_parent: do i=1,npart
     child_found = -1
     rikmax = huge(rikmax)
     over_neighbours: do k=1,npart
-      if (on_list(k) > 0 .or. .not.iactive(k)) cycle over_neighbours
+      if (on_list(k) > 0) cycle over_neighbours
       rik = xyzh(1:3,k) - xyzh(1:3,i)
       rik2 = dot_product(rik,rik)
       if (rik2 < rikmax) then
@@ -107,39 +115,41 @@ over_parent: do i=1,npart
       endif
     enddo over_neighbours
 
-    if (child_found < 0) then
+    if (child_found > 0) then
+      ! if child found, save the child to the list
+      ichild = ichild + 1
+      children_list(ichild) = child_found
+      on_list(child_found) = k
+      !print*,'parent',iparent,'paired with',child_found
+    else
       ! no children found for the parent particle
       call error('mergepart','no child found for parent particle')
-      print*,'parent particle is',i
+      print*,'parent particle is',iparent
       ierr = 1
     endif
 
-    ! if child found, save the child to the list
-    ichild = ichild + 1
-    children_list(ichild) = child_found
-    on_list(child_found) = 1
   enddo over_child
+  !read*
 
 ! send in children, parent returns
-! have to save these in a temporary array because need all
-! the children to calculate density at new parent
+! parents temporarily stored after all the children
 call merge_particles(nchild,children_list,massoftype(igas), npart, &
-     xyzh,vxyzu,xyzh_parent(1:4,iparent),vxyzu_parent(1:3,iparent))
+     xyzh,vxyzu,npart+iparent)
 
 enddo over_parent
 
-!-- store results
-  xyzh(1:4,1:nparent) = xyzh_parent(:,:)
-  vxyzu(1:3,1:nparent) = vxyzu_parent(:,:)
+!-- move the new parents
+do i = 1,nparent
+  call copy_particle(npart+i,i)
+enddo
 
 !-- kill all the useless children
-  do i=nparent+1,npart
-    call kill_particle(i,npartoftype)
-  enddo
+do i=nparent+1,npart
+  call kill_particle(i)
+enddo
 
 !--tidy up
-  call delete_dead_or_accreted_particles(npart,npartoftype)
-  deallocate(xyzh_parent,vxyzu_parent,xyzh_in,vxyzu_in)
+call delete_dead_or_accreted_particles(npart,npartoftype)
 
 !--update npartoftype
   npartoftype(igas) = nparent
--- src/utils/splitpart.f90
+++ src/utils/splitpart.f90
@@ -25,14 +25,14 @@ contains
 !  split particles into nchild particle
 !+
 !--------------------------------------------------------------------------
-subroutine split_particles(nchild,iparent,xyzh_parent,vxyzu_parent, &
-           lattice_type,ires,xyzh_child,vxyzu_child)
+subroutine split_particles(nchild,iparent,xyzh,vxyzu, &
+           lattice_type,ires,ichildren)
  use icosahedron, only:pixel2vector,compute_corners,compute_matrices
- integer, intent(in)    :: nchild
- integer, intent(in)    :: iparent,lattice_type,ires
- real,    intent(in)    :: xyzh_parent(:),vxyzu_parent(:)
- real,    intent(out)   :: xyzh_child(4,nchild),vxyzu_child(3,nchild)
- integer :: j,iseed
+ use part,        only:copy_particle
+ integer, intent(in)    :: nchild,iparent,lattice_type,ires
+ integer, intent(in)    :: ichildren !the index *after* which children are stored
+ real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
+ integer :: j,iseed,ichild
  real    :: dhfac,dx(3),sep,geodesic_R(0:19,3,3), geodesic_v(0:11,3)
 
  if (lattice_type == 0) then
@@ -43,26 +43,27 @@ subroutine split_particles(nchild,iparent,xyzh_parent,vxyzu_parent, &
     iseed = -6542
  endif
 
- ! first child sits where the parent was
- ! for now, assume all children have same velocity as parent
- xyzh_child(1:3,1) = xyzh_parent(1:3)
- vxyzu_child(:,1)  = vxyzu_parent(1:3)
- sep = xyzh_parent(4)
+ sep = xyzh(4,iparent)
+ ichild = 0
 
- do j=2,nchild
-   ! find positions of original particles
+ do j=0,nchild-2
+   ichild = ichild + 1
+   ! copy properties
+   call copy_particle(iparent,ichildren+ichild)
+
+   ! adjust the position
    if (lattice_type == 0) then
-     call pixel2vector(j-2,ires,geodesic_R,geodesic_v,dx)
+     call pixel2vector(j,ires,geodesic_R,geodesic_v,dx)
    else
      call sample_kernel(iseed,dx)
    endif
-      xyzh_child(1:3,j) = xyzh_parent(1:3) + sep*dx(:)
-      vxyzu_child(1:3,j) = vxyzu_parent(1:3)
+      xyzh(1:3,ichildren+ichild) = xyzh(1:3,iparent) + sep*dx(:)
  enddo
 
- !--amend smoothing length
+ !--amend smoothing length of children + parent
  dhfac = 1./(nchild)**(1./3.)
- xyzh_child(4,:) = xyzh_parent(4)*dhfac
+ xyzh(4,ichildren:ichildren+nchild-1) = xyzh(4,iparent)*dhfac
+ xyzh(4,iparent) = xyzh(4,iparent)*dhfac
 
 end subroutine split_particles
 
@@ -80,6 +81,13 @@ subroutine sample_kernel(iseed,dx)
 
 end subroutine sample_kernel
 
+!-----------------------------------------------------------------------
+!+
+! routine that shuffles the particle positions slightly
+! (see Vacondio et al. 2013 Equation 11)
+! (currently mostly used in testing, not sure if it's important yet)
+!+
+!-----------------------------------------------------------------------
 subroutine shift_particles(npart,xyzh,vxyzu,deltat,beta,shifts)
   use kernel, only:radkern2
   real, intent(in)    :: xyzh(:,:),vxyzu(:,:)
@@ -128,27 +136,32 @@ end subroutine shift_particles
 ! merges nchild particles in one parent particle
 !+
 !-----------------------------------------------------------------------
-subroutine merge_particles(nchild,children_list,mchild,npart, &
-           xyzh,vxyzu,xyzh_parent,vxyzu_parent)
+subroutine merge_particles(nchild,ichildren,mchild,npart, &
+           xyzh,vxyzu,iparent)
  use kernel, only:get_kernel,cnormk,radkern
- integer, intent(in)    :: nchild,children_list(nchild)
+ use part,   only:copy_particle
+ integer, intent(in)    :: nchild,ichildren(nchild),iparent
  integer, intent(inout) :: npart
- real,    intent(in)    :: xyzh(:,:),vxyzu(:,:),mchild
- real,    intent(out)   :: xyzh_parent(4),vxyzu_parent(3)
+ real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
+ real,    intent(out)   :: mchild
  integer :: i,j,ichild
  real    :: h1,h31,rij_vec(3)
  real    :: qij,rij,wchild,grkernchild,rho_parent
 
+ !-- copy properties from first child
+ call copy_particle(ichildren(1),iparent)
+
  !-- positions and velocities from centre of mass
- xyzh_parent = 0.
- vxyzu_parent = 0.
+ xyzh(1:3,iparent) = 0.
+ vxyzu(1:3,iparent) = 0.
+
  do i=1,nchild
-   ichild = children_list(i)
-   xyzh_parent(1:3) = xyzh_parent(1:3) + xyzh(1:3,ichild)
-   vxyzu_parent(:)  = vxyzu_parent(:) + vxyzu(1:3,ichild)
+   ichild = ichildren(i)
+   xyzh(1:3,iparent) = xyzh(1:3,iparent) + xyzh(1:3,ichild)
+   vxyzu(1:3,iparent) = vxyzu(1:3,iparent) + vxyzu(1:3,ichild)
  enddo
- xyzh_parent = xyzh_parent/nchild
- vxyzu_parent = vxyzu_parent/nchild
+ xyzh(1:3,iparent) = xyzh(1:3,iparent)/nchild
+ vxyzu(1:3,iparent) = vxyzu(1:3,iparent)/nchild
 
 !-- calculate density at the parent position from the children
 !-- procedure described in Vacondio et al. 2013, around Eq 21
@@ -158,7 +171,7 @@ rho_parent = 0.
     h1 = 1./xyzh(4,j)
     h31 = h1**3
 
-    rij_vec = xyzh_parent(1:3) - xyzh(1:3,j)
+    rij_vec = xyzh(1:3,iparent) - xyzh(1:3,j)
 
     rij = sqrt(dot_product(rij_vec,rij_vec))
     qij = rij*h1
@@ -170,7 +183,7 @@ rho_parent = 0.
   enddo over_npart
 
 !-- smoothing length from density
-xyzh_parent(4) = 1.2*(nchild*mchild/rho_parent)**(1./3.)
+xyzh(4,iparent) = 1.2*(nchild*mchild/rho_parent)**(1./3.)
 
 end subroutine merge_particles
 
