057581bc6f49888dda73a0656f16bb47cfcb7bf2
Mike Lau <mike.lau@monash.edu>
2020-11-25 23:36:07 +1100

    (fixedentropysoftening) restructure the entropy function and use gas+rad entropy for MESA EoS instead of MESA tables

--- src/main/eos.F90
+++ src/main/eos.F90
@@ -1270,29 +1270,31 @@ end subroutine calc_temp_and_ene
 !  up to an additive integration constant, from density and pressure.
 !+
 !-----------------------------------------------------------------------
-function entropy(rho,pres,ierr)
+function entropy(rho,pres,ientropy,ierr)
+   use io,                only:fatal
    use physcon,           only:radconst,kb_on_mh
    use eos_idealplusrad,  only:get_idealgasplusrad_tempfrompres
    use eos_mesa,          only:get_eos_eT_from_rhop_mesa
    use mesa_microphysics, only:getvalue_mesa
    real, intent(in)               :: rho,pres
-   integer, intent(out), optional :: ierr
+   integer, intent(out), optional :: ierr,ientropy
    real                           :: inv_mu,entropy,logentropy,temp,eint
   
    if (present(ierr)) ierr=0
    inv_mu = 1/gmw
   
-   select case(ieos)
-   case(2) ! Include only gas entropy for adiabatic EoS
+   select case(ientropy)
+   case(1) ! Include only gas entropy (up to additive constants)
        temp = pres * gmw / (rho * kb_on_mh)
        entropy = kb_on_mh * inv_mu * log(temp**1.5/rho)
   
-   case(12) ! Include both gas and radiation entropy gas plus rad. EoS
+   case(2) ! Include both gas and radiation entropy (up to additive constants)
        temp = pres * gmw / (rho * kb_on_mh) ! Guess for temp
        call get_idealgasplusrad_tempfrompres(pres,rho,gmw,temp) ! First solve for temp from rho and pres
        entropy = kb_on_mh * inv_mu * log(temp**1.5/rho) + 4.*radconst*temp**3 / (3.*rho)
    
-   case(10) ! MESA EoS
+   case(3) ! Get entropy from MESA tables if using MESA EoS
+       if (ieos /= 10) call fatal('eos','Using MESA tables to calculate S from rho and pres, but not using MESA EoS')
        call get_eos_eT_from_rhop_mesa(rho,pres,eint,temp)
   
        ! Get entropy from rho and eint from MESA tables
@@ -1302,6 +1304,9 @@ function entropy(rho,pres,ierr)
           call getvalue_mesa(rho,eint,9,logentropy)
        endif
        entropy = 10.d0**logentropy
+
+   case default
+       call fatal('eos','Unknown ientropy (can only be 1, 2, or 3)')
    end select
     
   end function entropy
