e5d92f3c8803ce608b779e5dcebd55aec83e14cb
Simone Ceppi <simo.ceppi@gmail.com>
2021-03-01 08:53:28 +0100

    Fix the code overlap of the set_binary and set_multiple subroutines

--- src/setup/set_binary.f90
+++ src/setup/set_binary.f90
@@ -260,18 +260,15 @@ subroutine set_binary(m1,m2,semimajoraxis,eccentricity, &
 end subroutine set_binary
 
   
-  
 !----------------------------------------------------------------
 !+
-!  setup for a multiple
+!  setup for a multiple, using set_binary
 !+
 !----------------------------------------------------------------
 subroutine set_multiple(m1,m2,semimajoraxis,eccentricity, &
                       accretion_radius1,accretion_radius2, &
                       xyzmh_ptmass,vxyz_ptmass,nptmass,ierr,omega_corotate,&
                       posang_ascnode,arg_peri,incl,f,verbose,subst)
-  use binaryutils, only:get_E
-!  use io,   only:warning,fatal
  real,    intent(in)    :: m1,m2
  real,    intent(in)    :: semimajoraxis,eccentricity
  real,    intent(in)    :: accretion_radius1,accretion_radius2
@@ -283,23 +280,23 @@ subroutine set_multiple(m1,m2,semimajoraxis,eccentricity, &
  real,    intent(out), optional :: omega_corotate
  logical, intent(in),  optional :: verbose
  integer :: i1,i2,i,subst_index
- real    :: mtot,dx(3),dv(3),Rochelobe1,Rochelobe2,period
- real    :: x1(3),x2(3),v1(3),v2(3),omega0,cosi,sini,xangle,reducedmass,angmbin
+ real    :: mtot,period
  real    :: x_subst(3),v_subst(3)
- real    :: a,E,E_dot,P(3),Q(3),omega,big_omega,inc,ecc,tperi,term1,term2,theta
- logical :: do_verbose
+ real    :: omega,inc
+ !logical :: do_verbose
 
  real, dimension(24,10) :: data  
- character(len=20)     :: hier_prefix
- logical               :: iexist
- integer               :: io, lines
- real                  :: period_ratio,criterion,q_comp,a_comp,e_comp,m_comp,rel_posang_ascnode,rel_arg_peri,rel_incl
- real                  :: q2,mprimary,msecondary
- real                  :: alpha_y, beta_y, gamma_y, alpha_z, beta_z, gamma_z, sign_alpha, sign_gamma
+ character(len=20)      :: hier_prefix
+ logical                :: iexist
+ integer                :: io, lines
+ real                   :: period_ratio,criterion,q_comp,a_comp,e_comp,m_comp
+ real                   :: rel_posang_ascnode=0.,rel_arg_peri=0.,rel_incl=0.
+ real                   :: q2,mprimary,msecondary
+ real                   :: alpha_y, beta_y, gamma_y, alpha_z, beta_z, gamma_z, sign_alpha, sign_gamma
 
  ierr = 0
- do_verbose = .true.
- if (present(verbose)) do_verbose = verbose
+ !do_verbose = .true.
+ !if (present(verbose)) do_verbose = verbose
 
  
   !--- Load/Create HIERARCHY file: xyzmh_ptmass index | hierarchical index | star mass | companion star mass | semi-major axis | eccentricity | period | inclination | argument of pericenter | ascending node longitude
@@ -407,212 +404,48 @@ subroutine set_multiple(m1,m2,semimajoraxis,eccentricity, &
 
     x_subst(:)=xyzmh_ptmass(1:3, subst_index)
     v_subst(:)=vxyz_ptmass(:, subst_index)
-    i1 = subst_index
-    i2 = nptmass + 1
-    nptmass = nptmass + 1
+    !i1 = subst_index
+    !i2 = nptmass + 1
+    !nptmass = nptmass + 1
 
     period = sqrt(4.*pi**2*semimajoraxis**3/mtot)
-   
  else
+    mprimary = m1
+    msecondary = m2
+
     if (present(posang_ascnode)) rel_posang_ascnode = posang_ascnode
     if (present(arg_peri)) rel_arg_peri= arg_peri
     if (present(incl)) rel_incl = incl
    
-    i1 = nptmass + 1
-    i2 = nptmass + 2
-    nptmass = nptmass + 2
-
-    mprimary = m1
-    msecondary = m2
-    
  endif
  !---
+
+ !--- Create the binary
+ call set_binary(mprimary,msecondary,semimajoraxis=semimajoraxis,eccentricity=eccentricity, &
+            posang_ascnode=rel_posang_ascnode,arg_peri=rel_arg_peri,incl=rel_incl, &
+            f=f,accretion_radius1=accretion_radius1,accretion_radius2=accretion_radius2, &
+            xyzmh_ptmass=xyzmh_ptmass,vxyz_ptmass=vxyz_ptmass,nptmass=nptmass, ierr=ierr)
+ !---
  
- ! masses
- Rochelobe1 = Rochelobe_estimate(msecondary,mprimary,semimajoraxis)
- Rochelobe2 = Rochelobe_estimate(mprimary,msecondary,semimajoraxis)
- reducedmass = mprimary*msecondary/mtot
- angmbin = reducedmass*sqrt(mtot*semimajoraxis*(1. - eccentricity**2))
- 
- 
- if (do_verbose) then
-    print "(/,2x,a)",'---------- binary parameters ----------- '
-    print "(8(2x,a,g12.3,/),2x,a,g12.3)", &
-        'primary mass     :',mprimary, &
-        'secondary mass   :',msecondary, &
-        'mass ratio m2/m1 :',msecondary/mprimary, &
-        'reduced mass     :',reducedmass, &
-        'semi-major axis  :',semimajoraxis, &
-        'period           :',period, &
-        'eccentricity     :',eccentricity, &
-        'pericentre       :',semimajoraxis*(1. - eccentricity), &
-        'apocentre        :',semimajoraxis*(1. + eccentricity)
- endif
+ if (present(subst)) then
+    
+    !--- lower nptmass, copy one of the new sinks to the subst star
+    nptmass = nptmass-1
+    i1 = subst_index
+    i2 = nptmass
+    
+    ! positions and accretion radii
+    xyzmh_ptmass(1:6,i1) = xyzmh_ptmass(1:6,nptmass+1)
+    
+    ! velocities
+    vxyz_ptmass(:,i1) = vxyz_ptmass(:,nptmass+1)
 
- if (do_verbose .and. present(subst)) then
-    print "(2x,a,g12.3)", &
-         'star to be subst :',subst
- endif
- 
- if (accretion_radius1 > Rochelobe1) then
-    print "(1x,a)",'WARNING: set_binary: accretion radius of primary > Roche lobe'
- endif
- if (accretion_radius2 > Rochelobe2) then
-    print "(1x,a)",'WARNING: set_binary: accretion radius of secondary > Roche lobe'
- endif
-!
-!--check for stupid parameter choices
-!
- if (mprimary <= 0.) then
-    print "(1x,a)",'ERROR: set_binary: primary mass <= 0'
-    ierr = ierr_m1
- endif
- if (msecondary < 0.) then
-    print "(1x,a)",'ERROR: set_binary: secondary mass <= 0'
-    ierr = ierr_m2
- endif
- if (semimajoraxis <= 0.) then
-    print "(1x,a)",'ERROR: set_binary: semi-major axis <= 0'
-    ierr = ierr_semi
- endif
- if (eccentricity > 1. .or. eccentricity < 0.) then
-    print "(1x,a)",'ERROR: set_binary: eccentricity must be between 0 and 1'
-    ierr = ierr_ecc
- endif
- ! exit routine if cannot continue
- if (ierr /= 0) return
+    !---
 
-
- dx = 0.
- dv = 0.
- if ((present(posang_ascnode) .and. present(arg_peri) .and. present(incl)) .or. present(subst)) then
-    ! Campbell elements
-    a = semimajoraxis
-    ecc = eccentricity
-    omega     = rel_arg_peri*pi/180.
-    ! our conventions here are Omega is measured East of North
-    big_omega = rel_posang_ascnode*pi/180. + 0.5*pi
-    inc       = rel_incl*pi/180.
-
-    if (present(f)) then
-       ! get eccentric anomaly from true anomaly
-       ! (https://en.wikipedia.org/wiki/Eccentric_anomaly#From_the_true_anomaly)
-       theta = f*pi/180.
-       E = atan2(sqrt(1. - ecc**2)*sin(theta),(ecc + cos(theta)))
-    else
-       ! set binary at apastron
-       tperi = 0.5*period ! time since periastron: half period = apastron
-
-       ! Solve Kepler equation for eccentric anomaly
-       call get_E(period,eccentricity,tperi,E)
-    endif
-
-    ! Positions in plane (Thiele-Innes elements)
-    P(1) = cos(omega)*cos(big_omega) - sin(omega)*cos(inc)*sin(big_omega)
-    P(2) = cos(omega)*sin(big_omega) + sin(omega)*cos(inc)*cos(big_omega)
-    P(3) = sin(omega)*sin(inc)
-    Q(1) = -sin(omega)*cos(big_omega) - cos(omega)*cos(inc)*sin(big_omega)
-    Q(2) = -sin(omega)*sin(big_omega) + cos(omega)*cos(inc)*cos(big_omega)
-    Q(3) = sin(inc)*cos(omega)
-
-    term1 = cos(E)-eccentricity
-    term2 = sqrt(1.-(eccentricity*eccentricity))*sin(E)
-    E_dot = sqrt((mprimary + msecondary)/(a**3))/(1.-eccentricity*cos(E))
-
-    if (do_verbose) then
-       print "(4(2x,a,g12.4,/),2x,a,g12.4)", &
-             'Eccentric anomaly:',E, &
-             'E_dot            :',E_dot, &
-             'inclination     (i, deg):',rel_incl, &
-             'angle asc. node (O, deg):',rel_posang_ascnode, &
-             'arg. pericentre (w, deg):',rel_arg_peri
-       if (present(f)) print "(2x,a,g12.4)", &
-             'true anomaly    (f, deg):',f
-    endif
-
-    ! Rotating everything
-    ! Set the positions for the primary and the central secondary
-    dx(:) = a*(term1*P(:) + term2*Q(:)) ! + xyzmh_ptmass(1,1)
-
-    ! Set the velocities
-    dv(:) = -a*sin(E)*E_dot*P(:) + a*sqrt(1.-(ecc*ecc))*cos(E)*E_dot*Q(:)
-
- else
-    ! set binary at apastron
-    dx = (/semimajoraxis*(1. + eccentricity),0.,0./)
-    dv = (/0.,sqrt(semimajoraxis*(1.-eccentricity**2)*mtot)/dx(1),0./)
- endif
-
- ! positions of each star so centre of mass is at zero
- x1 = -dx*msecondary/mtot
- x2 =  dx*mprimary/mtot
-
- ! velocities
- v1 = -dv*msecondary/mtot !(/0.,-m2/mtot*vmag,0./)
- v2 =  dv*mprimary/mtot !(/0.,m1/mtot*vmag,0./)
-
- omega0 = dv(2)/semimajoraxis
-
- ! print info about positions and velocities
- if (do_verbose) then
-    print "(7(2x,a,g12.4,/),2x,a,g12.4)", &
-        'angular momentum :',angmbin, &
-        'mean ang. speed  :',omega0, &
-        'Omega_0 (prim)   :',v1(2)/x1(1), &
-        'Omega_0 (second) :',v1(2)/x1(1), &
-        'R_accretion (1)  :',accretion_radius1, &
-        'R_accretion (2)  :',accretion_radius2, &
-        'Roche lobe  (1)  :',Rochelobe1, &
-        'Roche lobe  (2)  :',Rochelobe2
- endif
-
- if (present(omega_corotate)) then
-    if (do_verbose) print "(a)",' SETTING VELOCITIES FOR COROTATING FRAME: '
-    omega_corotate = omega0
-    v1(2) = v1(2) - omega0*x1(1)
-    v2(2) = v2(2) - omega0*x2(1)
-    if (do_verbose) print "(2(2x,a,g12.4,/))", &
-     'Omega_0 (primary)     :',v1(2)/x1(1), &
-     'Omega_0 (secondary)   :',v2(2)/x2(1)
- endif
-
- ! conclude printout
- if (do_verbose) print "(2x,40('-'),/)"
-
-!
-!--positions and accretion radii
-!
- xyzmh_ptmass(:,i1) = 0.
- xyzmh_ptmass(:,i2) = 0.
- 
- xyzmh_ptmass(1:3,i1) = x1
- xyzmh_ptmass(1:3,i2) = x2
- xyzmh_ptmass(4,i1) = mprimary
- xyzmh_ptmass(4,i2) = msecondary
- xyzmh_ptmass(5,i1) = accretion_radius1
- xyzmh_ptmass(5,i2) = accretion_radius2
- xyzmh_ptmass(6,i1) = 0.0
- xyzmh_ptmass(6,i2) = 0.0
-!
-!--velocities
-!
- vxyz_ptmass(:,i1) = v1
- vxyz_ptmass(:,i2) = v2
-!
-! rotate if inclination is non-zero
-!
- if (present(incl) .and. .not.(present(arg_peri) .and. present(posang_ascnode)) .and. .not. present(subst)) then
-    xangle = rel_incl*pi/180.
-    cosi = cos(xangle)
-    sini = sin(xangle)
-    do i=i1,i2
-       call rotate(xyzmh_ptmass(1:3,i),cosi,sini)
-       call rotate(vxyz_ptmass(1:3,i),cosi,sini)
-    enddo
- endif
-
- if (present(subst)) then 
-    if (subst>0) then ! Rotate the substituting binary with orientational parameters referring to the substituted star's orbital plane
+    
+    ! Rotate the substituting binary with orientational parameters
+    ! referring to the substituted star's orbital plane
+    if (subst>0) then 
        omega     = rel_arg_peri*pi/180.
        !big_omega = rel_posang_ascnode*pi/180.! + 0.5*pi
        inc       = rel_incl*pi/180.
@@ -695,7 +528,8 @@ subroutine set_multiple(m1,m2,semimajoraxis,eccentricity, &
     close(1)  
  endif
 
-end subroutine set_multiple
+end subroutine set_multiple  
+  
 
 !------------------------------------
 ! Rotate an (x,y,z) point by theta
