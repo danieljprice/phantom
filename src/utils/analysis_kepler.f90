9d7d5986e775204e1b578119a6f798d1c58ac8dd
Megha Sharma <msha0023@student.monash.edu>
2021-08-08 19:25:56 +1000

    added entropy and int energy to analysis_kepler file

--- src/utils/analysis_kepler.f90
+++ src/utils/analysis_kepler.f90
@@ -1,7 +1,7 @@
 !!This code reads the data from the TDE dumpfile and stores the data in the format liked by kepler.
 !!Flaws in this code- Does not consider bins. Instead the total npart. We need to bin data so that we
 !!have grid similar to input file??
-
+!!Another problem is every value it reads is 0!!!
 !
 !--Writing the analysis module for making a kepler file.
 module analysis
@@ -33,6 +33,9 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
    real(4) :: pressure(npart)
    real(4) :: density(npart)
    real(4) :: temperature(npart)
+   real(4) :: entropy(npart)
+   real(4) :: int_eng(npart)
+   real    :: gamma = 5./3.
 
    if (.not.opened_full_dump) then
       write(*,'("SKIPPING FILE -- (Not a full dump)")')
@@ -69,6 +72,24 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
    endif
    print*, 'temperature found'
 
+   !Read temperature of each particle calculated.
+   call read_array_from_file(123,dumpfile,'entropy',entropy(1:npart),ierr)
+   if (ierr/=0) then
+      write(*,*)
+      write(*,'("WARNING: could not read entropy from file. It will be set to zero")')
+      write(*,*)
+      pressure = 0.
+   endif
+   print*, 'entropy found'
+
+   !Calculate internal energy
+   !one problem if density is 0. fine to leave it like this??
+
+   do i = 1, npart
+     int_eng(i) = pressure(i)/((gamma - 1)*density(i))
+   end do
+   print*, 'energy calculated'
+
    ! Print the analysis being done
     write(*,'("Performing analysis type ",A)') analysistype
     write(*,'("Input file name is ",A)') dumpfile
@@ -81,21 +102,23 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
     write(*,*)
     write(*,'("ASSUMING G == 1")')
 
-    !Use EOS to find energy of each particle?
+    !open the output file and save the data in the format kepler likes.
     open(iunit,file=output)
-
-    print*, output,'output'
     write(iunit,'("# ",es20.12,"   # TIME")') time !Don't need time for the kepler file but just testing things.
     write(iunit,"('#',3(1x,'[',i2.2,1x,a11,']',2x))") &
           1,'density',      &
           2,'temperature',  &
-          3,'pressure'
+          3,'pressure',     &
+          4,'entropy',      &
+          5,'int. energy'
 
     do i = 1,npart
        write(iunit,'(3(es18.10,1X))') &
-              density(i),   &
+              density(i),        &
               temperature(i),    &
-              pressure(i)
+              pressure(i),       &
+              entropy(i),        &
+              int_eng(i)
     enddo
 
  end subroutine do_analysis
