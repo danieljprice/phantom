76e1cd0a17c51fec7dba4a726e4435ba91cc0bd9
Megha Sharma <msha0023@student.monash.edu>
2021-08-09 16:24:08 +1000

    added the calculation for angular velocity

--- src/utils/analysis_kepler.f90
+++ src/utils/analysis_kepler.f90
@@ -12,8 +12,9 @@ module analysis
 private
 
 !---- These can be changed in the params file
-integer :: nbins
-real :: mh     = 1.
+!integer :: nbins
+!real :: mh     = 1.
+
 contains
 
 subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
@@ -23,24 +24,29 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
    use units     , only:udist,umass,unit_density,unit_pressure,unit_ergg,unit_velocity !units required to convert to kepler units.
    use prompting,  only:prompt
    use readwrite_dumps, only: opened_full_dump
+   use vectorutils, only:cross_product3D
 
    character(len=*),   intent(in) :: dumpfile
    integer,            intent(in) :: numfile,npart,iunit
    real,               intent(in) :: xyzh(:,:),vxyzu(:,:)
    real,               intent(in) :: pmass,time
    character(len=120)             :: output
-   character(len=20)              :: filename
+   !character(len=20)              :: filename
 
    integer :: i,ierr
-   logical :: iexist
+   !logical :: iexist
    real(4) :: pressure(npart)
    real(4) :: density(npart)
    real(4) :: temperature(npart)
    real(4) :: entropy(npart)
    real(4) :: int_eng(npart)
+   real(4) :: ang_vel(npart)
    real, dimension(npart) :: mass, velocity, outer_rad, tot_mass
    real    :: gamma = 5./3.
+   real    :: Li(3) !defining angular momentum vector
+   real , dimension(npart):: Li_mag !|Li(3)|
 
+   !If dumpfile is not a complete dump we don't read it.
    if (.not.opened_full_dump) then
       write(*,'("SKIPPING FILE -- (Not a full dump)")')
       return
@@ -51,14 +57,24 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
      !set the mass as the particle mass.
      mass(i) = pmass !every particle has same mass.
 
-     !calculate the poasition which is the location of the particle.
+     !calculate the position which is the location of the particle.
      outer_rad(i) = sqrt(dot_product(xyzh(1:3,i),xyzh(1:3,i)))
 
      !calculate the velocity using the magnitude of the velocity vector i.e., velocity = sqrt(v.v) where v is a vector.
      velocity(i) = sqrt(dot_product(vxyzu(1:3,i),vxyzu(1:3,i)))
-
    end do
 
+   do i = 1, npart
+      !Calculating angular momentum and saving it as an array.
+      !Calculate the (r_x, r_y, r_z) X (v_x, v_y, v_z), ignoring mass as it gets cancelled when equating to I omega
+      call cross_product3D(xyzh(1:3,i),vxyzu(1:3,i),Li)
+      Li_mag(i) = sqrt(dot_product(Li, Li))
+   end do
+
+   !from angular momentum, calculate angular velocity
+   do i = 1, npart
+     ang_vel(i) = Li_mag(i)/outer_rad(i)**2
+   end do
 
    !Read density of each particle caculated.
    call read_array_from_file(123,dumpfile,'rhogas',density(1:npart),ierr)
@@ -123,7 +139,7 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
     !open the output file and save the data in the format kepler likes.
     open(iunit,file=output)
     write(iunit,'("# ",es20.12,"   # TIME")') time !Don't need time for the kepler file but just testing things.
-    write(iunit,"('#',3(1x,'[',i2.2,1x,a11,']',2x))") &
+    write(iunit,"('#',9(1x,'[',i2.2,1x,a11,']',2x))") &
           1,'mass[g]',               &  !mass of particle
           2,'position[cm]',          &  !position
           3,'vel[cm/s]',             &  !velocity
@@ -131,10 +147,11 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
           5,'temp[K]',               &  !temperature
           6,'press[g/(cm s^2)]',     &  !pressure
           7,'entropy',               &  !entropy
-          8,'int.eng[erg/g]'            !specific internal energy
+          8,'int.eng[erg/g]' ,       &  !specific internal energy
+          9,'ang vel'                  !angular velocity
 
     do i = 1,npart
-       write(iunit,'(3(es18.10,1X))') &
+       write(iunit,'(9(es18.10,1X))') &                             
               mass(i)*umass,                   &
               outer_rad(i)*udist,              &
               velocity(i)*unit_velocity,       &
@@ -142,7 +159,8 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
               temperature(i),                  &
               pressure(i)*unit_pressure,       &
               entropy(i),                      & !no entropy unit?
-              int_eng(i)*unit_ergg
+              int_eng(i)*unit_ergg,            &
+              ang_vel(i)
     enddo
 
  print*, sum(mass*umass), 'tot mass?'
