43eefdd4e4ff85637ef7f407ed5f52bba9d85769
Megha Sharma <msha0023@student.monash.edu>
2021-08-10 19:00:10 +1000

    edited analysis_kepler file

--- src/utils/analysis_kepler.f90
+++ src/utils/analysis_kepler.f90
@@ -1,9 +1,5 @@
 !!This code reads the data from the TDE dumpfile and stores the data in the format liked by kepler.
-!!Flaws in this code- Does not consider bins. Instead the total npart. We need to bin data so that we
-!!have grid similar to input file??
-!!Another problem is 0 values.
-!
-!--Writing the analysis module for making a kepler file.
+
 module analysis
   implicit none
   character(len=3), parameter, public :: analysistype = 'tde'
@@ -11,43 +7,54 @@ module analysis
 
 private
 
-!---- These can be changed in the params file
-!integer :: nbins
-!real :: mh     = 1.
-
 contains
 
 subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
 
-   use io,         only:warning
-   use dump_utils, only:read_array_from_file
-   use units     , only:udist,umass,unit_density,unit_pressure,unit_ergg,unit_velocity !units required to convert to kepler units.
-   use prompting,  only:prompt
+   use io,              only:warning
+   use dump_utils,      only:read_array_from_file
+   use units,           only:udist,umass,unit_density,unit_pressure,unit_ergg,unit_velocity !units required to convert to kepler units.
+   use prompting,       only:prompt
    use readwrite_dumps, only: opened_full_dump
-   use vectorutils, only:cross_product3D
-   use part,         only: nptmass,xyzmh_ptmass,vxyz_ptmass
-   use centreofmass, only: get_centreofmass
+   use vectorutils,     only:cross_product3D
+   use part,            only: nptmass,xyzmh_ptmass,vxyz_ptmass,rhoh
+   use centreofmass,    only: get_centreofmass
+   use sortutils,       only:set_r2func_origin,indexxfunc,r2func_origin
+   use eos,             only:equationofstate
 
-   character(len=*),   intent(in) :: dumpfile
-   integer,            intent(in) :: numfile,npart,iunit
-   real,               intent(in) :: xyzh(:,:),vxyzu(:,:)
-   real,               intent(in) :: pmass,time
-   character(len=120)             :: output
-   real                           :: xpos(3),vpos(3) !COM position and velocity
-   !character(len=20)              :: filename
 
    integer :: i,ierr
-   !logical :: iexist
-   real(4) :: pressure(npart)
-   real(4) :: density(npart)
-   real(4) :: temperature(npart)
-   real(4) :: entropy(npart)
-   real(4) :: int_eng(npart)
-   real(4) :: ang_vel(npart)
-   real, dimension(npart) :: mass, velocity, outer_rad, tot_mass
-   real    :: gamma = 5./3.
-   real    :: Li(3) !defining angular momentum vector
-   real , dimension(npart):: Li_mag !|Li(3)|
+   integer :: no_in_bin !this stores the number of particles in bin after each loop.
+   integer :: ibin
+   integer :: number_particle, ieos
+   integer, parameter   :: ngrid = 512 !resolution in grid in kepler,
+   integer,  intent(in) :: numfile,npart,iunit
+   integer :: iorder(npart), j
+
+
+   real :: pressure(ngrid)
+   real :: rad_grid(ngrid) !radius
+   real :: mass(ngrid)
+   real :: rad_vel(ngrid) !radial velocity
+   real :: density(ngrid)
+   real :: temperature(ngrid)
+   real :: entropy(ngrid) !entropy
+   real :: int_eng(ngrid) !specific internal energy
+   real :: ang_vel(ngrid) !angular velocity
+   real :: density_sum, density_i
+   real :: u_sum, u_i !specific internal energy storage
+   real :: temperature_i, temperature_sum
+   real :: pressure_i, pressure_sum
+   real :: Li(3), pos(3), vel(3), rad, grid !defining angular momentum vector
+   real :: xpos(3), vpos(3) !COM position and velocity
+   real :: ponrhoi, spsoundi, vel_i, vel_sum
+   real :: ang_vel_sum ,ang_i
+   real,   intent(in) :: xyzh(:,:),vxyzu(:,:)
+   real,   intent(in) :: pmass,time
+
+
+   character(len=120)             :: output
+   character(len=*),   intent(in) :: dumpfile
 
    !If dumpfile is not a complete dump we don't read it.
    if (.not.opened_full_dump) then
@@ -55,109 +62,121 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
       return
    endif
 
-   !read the particle mass, velocity, radius and save it as an array
-   do i = 1, npart
-     !set the mass as the particle mass.
-     mass(i) = pmass !every particle has same mass.
+   !get CoM
+   call get_centreofmass(xpos,vpos,npart,xyzh,vxyzu,nptmass,xyzmh_ptmass,vxyz_ptmass)
+
+   !use sorting algorithm to sort the particles from the center of mass as a function of radius.
+   call set_r2func_origin(xpos(1),xpos(2),xpos(3))
+   call indexxfunc(npart,r2func_origin,xyzh,iorder)
+
+    ibin = 1
+    number_particle = (npart)/(ngrid) !number of particles per bin
+    print*, number_particle, 'particle no'
+    no_in_bin = 0 !this keeps tracks of the particles added to the bin in the loop implemented.
+    density_sum = 0.
+    u_sum = 0.
+    temperature_sum = 0.
+    pressure_sum =0.
+    vel_sum = 0.
+
+   !implementing loop for calculating the values we require.
+   do j = 1, npart
+
+     i  = iorder(j) !Access the rank of each particle in radius.
+
+     !add 1 to no_in_bin
+     no_in_bin = no_in_bin + 1
+
+     !the position of the particle is calculated by subtracting the centre of mass.
+     !xyzh is position wrt the black hole present at origin.
+     pos(:) = xyzh(1:3,i) - xpos(:)
 
      !calculate the position which is the location of the particle.
-     outer_rad(i) = sqrt(dot_product(xyzh(1:3,i),xyzh(1:3,i)))
+     rad = sqrt(dot_product(pos(:),pos(:)))
 
-     !calculate the velocity using the magnitude of the velocity vector i.e., velocity = sqrt(v.v) where v is a vector.
-     velocity(i) = sqrt(dot_product(vxyzu(1:3,i),vxyzu(1:3,i)))
+     !radial velocity
+     vel(:)  = vxyzu(1:3,i) - vpos(:) !relative velocity
+     vel_i   = dot_product(vel(:),pos(:))/rad !this calculates (v.r)/|r| which is the dot production of velocity with position.
+     vel_sum = vel_sum + vel_i
+
+     !angular velocity
+     call cross_product3D(xyzh(1:3,i),vxyzu(1:3,i),Li)
+     ang_i       = sqrt(dot_product(Li, Li))
+     ang_vel_sum = ang_vel_sum + ang_i
+
+     !density
+     density_i   = rhoh(xyzh(4,i),pmass)  !density of particle
+     density_sum = density_sum + density_i
+
+     !internal energy
+     u_i   = vxyzu(4,i)
+     u_sum = u_sum + u_i
+     !print*, i, j , rad, no_in_bin, ibin, density_sum, density_i
+
+     !using the adiabatic equation of state.
+     ieos = 2
+     !call eos routine
+     call equationofstate(ieos,ponrhoi,spsoundi,density_i,xyzh(1,i),xyzh(2,i),xyzh(3,i),eni=u_i, tempi=temperature_i)
+
+     !pressure and temperature calculation.
+     pressure_i      = ponrhoi*density_i
+     pressure_sum    = pressure_sum + pressure_i
+     temperature_sum = temperature_sum + temperature_i
+
+     if (no_in_bin >= number_particle) then
+
+       !make the bin properties.
+       rad_grid(ibin)    = rad !last particle
+       mass(ibin)        = j*pmass !mass of paricles < r
+       density(ibin)     = density_sum / no_in_bin
+       temperature(ibin) = temperature_sum / no_in_bin
+       pressure(ibin)    = pressure_sum / no_in_bin
+       entropy(ibin)     = 0.
+       int_eng(ibin)     = u_sum / no_in_bin
+       ang_vel(ibin)     = ang_vel_sum / no_in_bin
+       rad_vel(ibin)     = vel_sum / no_in_bin
+
+       !print*, 'Created bin', ibin, rad_grid(ibin) , mass(ibin), density(ibin)
+       no_in_bin       = 0
+       ibin = ibin     + 1
+       density_sum     = 0.
+       u_sum           = 0.
+       temperature_sum = 0.
+       pressure_sum    = 0.
+       vel_sum         = 0.
+
+     end if
+
    end do
-
-   !Calculating angular momentum and saving it as an array.
-   !Calculate the (r_x, r_y, r_z) X (v_x, v_y, v_z), ignoring mass as it gets cancelled when equating to I omega
-   do i = 1, npart
-      call cross_product3D(xyzh(1:3,i),vxyzu(1:3,i),Li)
-      Li_mag(i) = sqrt(dot_product(Li, Li))
-   end do
-
-   !from angular momentum, calculate angular velocity using omega = sqrt(Li, Li)/r^2
-   do i = 1, npart
-     ang_vel(i) = Li_mag(i)/outer_rad(i)**2
-   end do
-
-   !Read density of each particle caculated.
-   call read_array_from_file(123,dumpfile,'rhogas',density(1:npart),ierr)
-   if (ierr/=0) then
-      write(*,*)
-      write(*,'("WARNING: could not read density from file. It will be set to zero")')
-      write(*,*)
-      density = 0.
-   endif
-   print*, 'density found'
-
-   !Read pressure of each particle calculated.
-   call read_array_from_file(123,dumpfile,'pressure',pressure(1:npart),ierr)
-   if (ierr/=0) then
-      write(*,*)
-      write(*,'("WARNING: could not read pressure from file. It will be set to zero")')
-      write(*,*)
-      pressure = 0.
-   endif
-   print*, 'pressure found'
-
-   !Read temperature of each particle calculated.
-   call read_array_from_file(123,dumpfile,'temperature',temperature(1:npart),ierr)
-   if (ierr/=0) then
-      write(*,*)
-      write(*,'("WARNING: could not read temperature from file. It will be set to zero")')
-      write(*,*)
-      temperature = 0.
-   endif
-   print*, 'temperature found'
-
-   !Read temperature of each particle calculated.
-   call read_array_from_file(123,dumpfile,'entropy',entropy(1:npart),ierr)
-   if (ierr/=0) then
-      write(*,*)
-      write(*,'("WARNING: could not read entropy from file. It will be set to zero")')
-      write(*,*)
-      entropy = 0.
-   endif
-   print*, 'entropy found'
-
-   !Calculate internal energy
-   !one problem if density is 0. fine to leave it like this??
-
-   do i = 1, npart
-     int_eng(i) = pressure(i)/((gamma - 1)*density(i))
-   end do
-   print*, 'energy calculated'
 
    ! Print the analysis being done
     write(*,'("Performing analysis type ",A)') analysistype
     write(*,'("Input file name is ",A)') dumpfile
 
-    write(output,"(a4,i5.5)") 'test',numfile
+    write(output,"(a4,i5.5)") 'ptok',numfile
     write(*,'("Output file name is ",A)') output
-
-
-    ! Assuming G=1
-    write(*,*)
-    write(*,'("ASSUMING G == 1")')
 
     !open the output file and save the data in the format kepler likes.
     open(iunit,file=output)
-    write(iunit,'("# ",es20.12,"   # TIME")') time !Don't need time for the kepler file but just testing things.
-    write(iunit,"('#',9(1x,'[',i2.2,1x,a11,']',2x))") &
-          1,'mass[g]',               &  !mass of particle
-          2,'position[cm]',          &  !position
-          3,'vel[cm/s]',             &  !velocity
-          4,'den[g/cm^3]',           &  !density
-          5,'temp[K]',               &  !temperature
-          6,'press[g/(cm s^2)]',     &  !pressure
-          7,'entropy',               &  !entropy
-          8,'int.eng[erg/g]' ,       &  !specific internal energy
-          9,'ang vel'                   !angular velocity
+    write(iunit,'("# ",es20.12,"   # TIME")') time
+    write(iunit,"('#',10(1x,'[',i2.2,1x,a11,']',2x))") &
+          !1,'grid',                 &  !grid number/ bin number
+          2,'mass ',                &  !mass of particle
+          3,'radius ',              &  !position
+          4,'velocity',             &  !velocity
+          5,'density',              &  !density
+          6,'temp',                 &  !temperature
+          7,'pressure',             &  !pressure
+          8,'entropy',              &  !entropy
+          9,'int.engergy' ,         &  !specific internal energy
+          10,'ang vel'                 !angular velocity
 
-    do i = 1, npart
-       write(iunit,'(9(es18.10,1X))') &
+    do i = 1, ngrid
+       write(iunit,'(10(es18.10,1X))') &
+       !write(iunit,*) &
               mass(i)*umass,                   &
-              outer_rad(i)*udist,              &
-              velocity(i)*unit_velocity,       &
+              rad_grid(i)*udist,               &
+              rad_vel(i)*unit_velocity,        &
               density(i)*unit_density,         &
               temperature(i),                  &
               pressure(i)*unit_pressure,       &
@@ -165,11 +184,6 @@ subroutine do_analysis(dumpfile,numfile,xyzh,vxyzu,pmass,npart,time,iunit)
               int_eng(i)*unit_ergg,            &
               ang_vel(i)
     enddo
-
- print*, sum(mass*umass), 'tot mass?'
-
- !get CoM
- call get_centreofmass(xpos,vpos,npart,xyzh,vxyzu,nptmass,xyzmh_ptmass,vxyz_ptmass)
 
  print*, '(',xpos(1), xpos(2), xpos(3),')', '-> COM'
 
