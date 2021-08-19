3e1a23cee9c562ec949a038b6fedeae172f0deda
Conrad Chan <conradchan@hotmail.com.au>
2021-08-19 11:33:50 +1000

    update sort_part subroutine calls to fix build errors

--- src/main/sort_particles.F90
+++ src/main/sort_particles.F90
@@ -34,11 +34,12 @@ subroutine sort_part
  use io,       only:iprint,fatal
  use part,     only:reorder_particles,npart,ll,xyzh,vxyzu,isdead
  use linklist, only:set_linklist,ncells,ifirstincell
- integer :: i,icell,ipart,iprev,ifirst
- real    :: t0,t1,t2
+ integer         :: i,ipart,iprev,ifirst
+ integer(kind=8) :: icell
+ real            :: t0,t1,t2
 
  call cpu_time(t0)
- call set_linklist(npart,xyzh,vxyzu)  ! don't include MPI ghosts
+ call set_linklist(npart,npart,xyzh,vxyzu)  ! don't include MPI ghosts
  call cpu_time(t1)
  write(iprint,*) '> sorting particles...',t1-t0,'s for linklist'
 
