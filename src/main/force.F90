5484768a0ed6ba266a0da80c3c14d122a68a8f36
Fangyi (Fitz) Hu <fhuu0005@student.monash.edu>
2021-08-31 17:17:21 +1000

    (force) Always constrain timestep with u/(du/dt); fixes problems with u<0

--- src/main/force.F90
+++ src/main/force.F90
@@ -2840,9 +2840,8 @@ subroutine finish_cell_and_store_results(icall,cell,fxyzu,xyzh,vxyzu,poten,dt,dv
        endif
 
        ! cooling timestep dt < fac*u/(du/dt)
-       ! Note: Why is this not used for *all* energy changes?  Regrettably, Sedov will crash if this timestep is included since eni0 = 0
-       if (maxvxyzu >= 4 .and. cooling_explicit) then
-          if (abs(fxyzu(4,i)) > 0.) dtcool = C_cool*abs(eni/fxyzu(4,i))
+       if (maxvxyzu >= 4) then
+          if (abs(fxyzu(4,i)) > tiny(0.) .and. eni > tiny(0.)) dtcool = C_cool*abs(eni/fxyzu(4,i))
        endif
 
        ! timestep based on non-ideal MHD
