88bd11c85d6a7f92af2689d51a1e2bbc8ee4e3ee
Daniel Price <daniel.price@monash.edu>
2021-10-08 17:43:15 +1100

    (dtcool) bug fix with test suite hang

--- src/main/force.F90
+++ src/main/force.F90
@@ -2843,7 +2843,7 @@ subroutine finish_cell_and_store_results(icall,cell,fxyzu,xyzh,vxyzu,poten,dt,dv
 
        ! cooling timestep dt < fac*u/(du/dt)
        if (maxvxyzu >= 4) then
-          if (abs(fxyzu(4,i)) > tiny(0.) .and. eni > tiny(0.)) dtcool = C_cool*abs(eni/fxyzu(4,i))
+          if (abs(fxyzu(4,i)) > epsilon(0.) .and. eni > epsilon(0.)) dtcool = C_cool*abs(eni/fxyzu(4,i))
        endif
 
        ! timestep based on non-ideal MHD
