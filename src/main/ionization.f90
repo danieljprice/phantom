b024f3c4e5d0ba385d0deaae4f60af1a0e6b06dc
Mike Lau <mike.lau@monash.edu>
2021-12-09 10:40:34 +1100

    (eos_gasradrec) change to phantom formatting style

--- src/main/ionization.f90
+++ src/main/ionization.f90
@@ -18,26 +18,28 @@ module ionization_mod
 !
  implicit none
 
- real,allocatable,public,dimension(:):: eion
- real,allocatable,private,dimension(:):: logeion, &
-                                   arec, brec, crec, drec, arec1c, brec1c
- real,private:: frec, edge, tanh_c, dtanh_c
- real,parameter,private:: tanh_edge = 3.6467
+ real, allocatable, public, dimension(:)  :: eion
+ real, allocatable, private, dimension(:) :: logeion,arec,brec,crec,drec,arec1c,brec1c
+ real, private                            :: frec,edge,tanh_c,dtanh_c
+ real, parameter, private                 :: tanh_edge=3.6467
 
  interface rapid_tanh
-  module procedure rapid_tanhs,rapid_tanhv
+    module procedure rapid_tanhs,rapid_tanhv
  end interface rapid_tanh
  interface rapid_dtanh
-  module procedure rapid_dtanhs,rapid_dtanhv
+    module procedure rapid_dtanhs,rapid_dtanhv
  end interface rapid_dtanh
 
 contains
-! **************************************************************************
+
+!-----------------------------------------------------------------------
+!+
+!  Rapid hyperbolic tangent function for scalars
+!+
+!-----------------------------------------------------------------------
 function rapid_tanhs(x)
-! Rapid hyperbolic tangent function for scalars
- implicit none
- real,intent(in)::x
- real:: a,b,x2,rapid_tanhs
+ real, intent(in) :: x
+ real             :: a,b,x2,rapid_tanhs
 
  if (abs(x)>=tanh_edge) then
     rapid_tanhs = sign(1.,x)-tanh_c/x
@@ -49,16 +51,19 @@ function rapid_tanhs(x)
  endif
 end function rapid_tanhs
 
+!-----------------------------------------------------------------------
+!+
+!  Rapid hyperbolic tangent function for vectors
+!+
+!-----------------------------------------------------------------------
 function rapid_tanhv(x)
-! Rapid hyperbolic tangent function for vectors
- implicit none
- real,intent(in)::x(:)
- real,dimension(size(x)):: rapid_tanhv
- real:: a,b,x2
- integer:: i
+ real, intent(in)         :: x(:)
+ real, dimension(size(x)) :: rapid_tanhv
+ real                     :: a,b,x2
+ integer                  :: i
 
- do i = 1, size(x)
-    if (abs(x(i))>=tanh_edge) then
+ do i = 1,size(x)
+    if (abs(x(i)) >= tanh_edge) then
        rapid_tanhv(i) = sign(1.,x(i))-tanh_c/x(i)
     else
        x2 = x(i)*x(i)
@@ -68,14 +73,17 @@ function rapid_tanhv(x)
     endif
  enddo
 end function rapid_tanhv
-! **************************************************************************
-function rapid_dtanhs(x)
-! Rapid hyperbolic tangent derivative (1/cosh^2) for scalars
- implicit none
- real,intent(in)::x
- real:: a,b,x2,rapid_dtanhs
 
- if (abs(x)>=tanh_edge) then
+!-----------------------------------------------------------------------
+!+
+!  Rapid hyperbolic tangent derivative (1/cosh^2) for scalars
+!+
+!-----------------------------------------------------------------------
+function rapid_dtanhs(x)
+ real, intent(in) :: x
+ real             :: a,b,x2,rapid_dtanhs
+
+ if (abs(x) >= tanh_edge) then
     rapid_dtanhs = dtanh_c/x**2
  else
     x2=x**2
@@ -85,16 +93,20 @@ function rapid_dtanhs(x)
  endif
 end function rapid_dtanhs
 
+!-----------------------------------------------------------------------
+!+
+!  Rapid hyperbolic tangent derivative (1/cosh^2) for vectors
+!+
+!-----------------------------------------------------------------------
 function rapid_dtanhv(x)
-! Rapid hyperbolic tangent derivative (1/cosh^2) for vectors
  implicit none
- real,intent(in)::x(:)
- real,dimension(size(x)):: rapid_dtanhv
- real:: a,b,x2
- integer:: i
+ real, intent(in)         :: x(:)
+ real, dimension(size(x)) :: rapid_dtanhv
+ real                     :: a,b,x2
+ integer                  :: i
 
- do i = 1, size(x)
-    if (abs(x(i))>=tanh_edge) then
+ do i = 1,size(x)
+    if (abs(x(i)) >= tanh_edge) then
        rapid_dtanhv(i) = dtanh_c/(x(i)*x(i))
     else
        x2=x(i)**2
@@ -104,14 +116,18 @@ function rapid_dtanhv(x)
     endif
  enddo
 end function rapid_dtanhv
-! **************************************************************************
+
+!-----------------------------------------------------------------------
+!+
+!  Set up all fitting coefficients
+!+
+!-----------------------------------------------------------------------
 subroutine ionization_setup
-! PURPOSE: Set up all fitting coefficients
- use physcon,only:kb_on_mh
- implicit none
- real:: x
- allocate( eion(1:4), arec(2:4), brec(2:4), crec(1:4), drec(1:4),&
-            arec1c(1:2), brec1c(1:2), logeion(1:4) )
+ use physcon, only:kb_on_mh
+ real :: x
+
+ allocate(eion(1:4),arec(2:4),brec(2:4),crec(1:4),drec(1:4),&
+          arec1c(1:2),brec1c(1:2),logeion(1:4))
 
  eion(1) = 4.36e12   ! H2   [erg/mol]
  eion(2) = 1.312e13  ! HI   [erg/mol]
@@ -119,7 +135,7 @@ subroutine ionization_setup
  eion(4) = 5.2505e13 ! HeII [erg/mol]
  logeion(1:4) = log10(eion(1:4)/kb_on_mh)
 
-! These fitting parameters are tuned for eosDT in the range X=0.6-0.8
+ ! These fitting parameters are tuned for eosDT in the range X=0.6-0.8
  frec = 0.005
  arec(2:4) = (/ 0.821, 0.829, 0.846 /)
  brec(2:4) = (/ 0.055, 0.055, 0.055 /)
@@ -130,7 +146,7 @@ subroutine ionization_setup
  brec1c(1:2) = (/ 0., 0.055 /)
  edge = -9.4
 
-! Parameter for rapid tanh function
+ ! Parameter for rapid tanh function
  x=tanh_edge
  tanh_c = x*(1.-((x*x+105.)*x*x+945.)*x/(((x*x+28.)*x*x+63.)*15.))
  dtanh_c= x*x*((((x*x-21.)*x*x+420.)*x*x-6615.)*x*x+59535.)&
@@ -138,11 +154,14 @@ subroutine ionization_setup
 
  return
 end subroutine ionization_setup
-! ***************************************************************************
+
+!-----------------------------------------------------------------------
+!+
+!  Molecular hydrogen fit coefficients
+!+
+!-----------------------------------------------------------------------
 real function arec1(x)
-! molecular hydrogen fit coefficients
- implicit none
- real,intent(in):: x ! logd
+ real, intent(in):: x ! logd
 
  if (x<edge) then
     arec1 = arec1c(1)
@@ -150,11 +169,9 @@ real function arec1(x)
     arec1 = arec1c(2)
  endif
 end function arec1
-! ***************************************************************************
+
 real function brec1(x)
-! molecular hydrogen fit coefficients
- implicit none
- real,intent(in):: x
+ real, intent(in):: x
 
  if (x<edge) then
     brec1 = brec1c(1)
@@ -162,16 +179,18 @@ real function brec1(x)
     brec1 = brec1c(2)
  endif
 end function brec1
-! ***************************************************************************
 
+!-----------------------------------------------------------------------
+!+
+!  Get ionization fractions (and dxdT) given rho and T
+!+
+!-----------------------------------------------------------------------
 subroutine get_xion(logd,T,X,Y,xion,dxion)
-! PURPOSE: Get ionization fractions (and dxdT) given rho and T
- implicit none
- real,intent(in):: logd,T,X,Y
- real,intent(out):: xion(1:4)
- real,intent(out),optional:: dxion(1:4)
- real:: logQ, logT, Yfac
- real,dimension(1:4):: Ttra, width, arg
+ real, intent(in)            :: logd,T,X,Y
+ real, intent(out)           :: xion(1:4)
+ real, intent(out), optional :: dxion(1:4)
+ real                        :: logQ,logT,Yfac
+ real, dimension(1:4)        :: Ttra,width,arg
 
  logT = log10(T)
  logQ = max(-14.,logd)-2.*logT+12.
@@ -198,15 +217,16 @@ subroutine get_xion(logd,T,X,Y,xion,dxion)
 
 end subroutine get_xion
 
-! ***************************************************************************
-
+!-----------------------------------------------------------------------
+!+
+!  Get recombination energy and mean molecular weight given rho and T
+!+
+!-----------------------------------------------------------------------
 subroutine get_erec_imurec(logd,T,X,Y,erec,imurec,derecdT,dimurecdT)
-! PURPOSE: Get recombination energy and mean molecular weight given rho and T
- implicit none
- real,intent(in):: logd,T,X,Y
- real,intent(out):: erec, imurec
- real,intent(out),optional:: derecdT, dimurecdT
- real,dimension(1:4):: e, xi, zi
+ real, intent(in)            :: logd,T,X,Y
+ real, intent(out)           :: erec,imurec
+ real, intent(out), optional :: derecdT,dimurecdT
+ real, dimension(1:4)        :: e,xi,zi
 
 ! CAUTION: This is only a poor man's way of implementing recombination energy.
 !          It only should be used for -3.5<logQ<-6 where logQ=logrho-2logT+12.
@@ -235,15 +255,16 @@ subroutine get_erec_imurec(logd,T,X,Y,erec,imurec,derecdT,dimurecdT)
  return
 end subroutine get_erec_imurec
 
-! ***************************************************************************
-
+!-----------------------------------------------------------------------
+!+
+!  Get the mean molecular weight for partially ionised plasma
+!+
+!-----------------------------------------------------------------------
 subroutine get_imurec(logd,T,X,Y,imurec,dimurecdT)
-! PURPOSE: Get the mean molecular weight for partially ionised plasma
- implicit none
- real,intent(in):: logd,T,X,Y
- real,intent(out):: imurec
- real,intent(out),optional:: dimurecdT
- real,dimension(1:4):: xi, zi
+ real, intent(in)            :: logd,T,X,Y
+ real, intent(out)           :: imurec
+ real, intent(out), optional :: dimurecdT
+ real, dimension(1:4)        :: xi,zi
 
 ! CAUTION: This is only a poor man's way of implementing recombination energy.
 !          It only should be used for -3.5<logQ<-6 where logQ=logrho-2logT+12.
@@ -262,12 +283,14 @@ subroutine get_imurec(logd,T,X,Y,imurec,dimurecdT)
  return
 end subroutine get_imurec
 
-! ***************************************************************************
+!-----------------------------------------------------------------------
+!+
+!  Get recombination energy given rho and T
+!+
+!-----------------------------------------------------------------------
 real function get_erec(logd,T,X,Y)
-! PURPOSE: Get recombination energy given rho and T
- implicit none
- real,intent(in):: logd,T,X,Y
- real,dimension(1:4):: e, xi
+ real, intent(in)     :: logd,T,X,Y
+ real, dimension(1:4) :: e, xi
 
 ! CAUTION: This is only a poor man's way of implementing recombination energy.
 !          It only should be used for -3.5<logQ<-6 where logQ=logrho-2logT+12.
