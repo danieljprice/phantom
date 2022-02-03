bd0a0b8a3555cee9c6dc1c2c1b84c2810b09e2d3
Conrad Chan <conradchan@hotmail.com.au>
2022-02-03 16:57:48 +1100

    increase memory allocation for mpi tasks to 3/n

--- src/main/readwrite_dumps_fortran.F90
+++ src/main/readwrite_dumps_fortran.F90
@@ -777,7 +777,7 @@ subroutine read_dump_fortran(dumpfile,tfile,hfactfile,idisk1,iprint,id,nprocs,ie
 #ifdef INJECT_PARTICLES
        call allocate_memory(maxp_hard)
 #else
-       call allocate_memory(int( min(nprocs,2)*nparttot / nprocs))
+       call allocate_memory(int(min(nprocs,3)*nparttot/nprocs))
 #endif
     endif
 !
@@ -969,7 +969,7 @@ subroutine read_smalldump_fortran(dumpfile,tfile,hfactfile,idisk1,iprint,id,npro
 #ifdef INJECT_PARTICLES
  call allocate_memory(maxp_hard)
 #else
- call allocate_memory(int( min(nprocs,2)*nparttot / nprocs))
+ call allocate_memory(int(min(nprocs,3)*nparttot/nprocs))
 #endif
 
 !
