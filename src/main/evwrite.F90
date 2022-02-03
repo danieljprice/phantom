d48730a19ae4cafbc31eec502e041536b791aa24
Conrad Chan <conradchan@hotmail.com.au>
2022-02-03 12:36:41 +1100

    [header-bot] updated file headers

--- src/main/evwrite.F90
+++ src/main/evwrite.F90
@@ -38,8 +38,8 @@ module evwrite
 ! :Runtime parameters: None
 !
 ! :Dependencies: boundary, dim, energies, extern_binary, externalforces,
-!   fileutils, gravwaveutils, io, nicil, options, part, ptmass, timestep,
-!   units, viscosity
+!   fileutils, gravwaveutils, io, mpiutils, nicil, options, part, ptmass,
+!   timestep, units, viscosity
 !
  use io,             only:fatal,iverbose
  use options,        only:iexternalforce
