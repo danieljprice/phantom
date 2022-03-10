danieljprice/phantom

commit 585c24ff09014fb7934582229dca877ed007b87f

remove remote_export from cell
because maxprocs is needed to get the right size, which we don't want to set as a parameter

subroutines adjusted accordingly (replaced direction with remote_export)

src/main/mpi_derivs.F90
@@ -169,34 +169,23 @@ end subroutine init_cellforce_exchange
 !  Subroutine to broadcast particle buffer to a bunch of processors
 !+
 !-----------------------------------------------------------------------
-subroutine send_celldens(cell,direction,irequestsend,xsendbuf)
+subroutine send_celldens(cell,targets,irequestsend,xsendbuf)
  use mpidens,  only:celldens
 
  type(celldens),     intent(in)     :: cell
- integer,            intent(in)     :: direction
+ logical,            intent(in)     :: targets(nprocs)
  integer,            intent(inout)  :: irequestsend(nprocs)
  type(celldens),     intent(out)    :: xsendbuf
 
- logical                            :: targets(nprocs)
  integer                            :: newproc
 
 #ifdef MPI
  xsendbuf = cell
-
- ! export
- if (direction == 0) then
-    targets = cell%remote_export(1:nprocs)
-    ! return
- elseif (direction == 1) then
-    targets = .false.
-    targets(cell%owner+1) = .true.
- endif
-
  irequestsend = MPI_REQUEST_NULL
 
  do newproc=0,nprocs-1
     if ((newproc /= id) .and. (targets(newproc+1))) then ! do not send to self
-       call MPI_ISEND(xsendbuf,1,dtype_celldens,newproc,1+direction,comm_cellexchange,irequestsend(newproc+1),mpierr)
+       call MPI_ISEND(xsendbuf,1,dtype_celldens,newproc,0,comm_cellexchange,irequestsend(newproc+1),mpierr)
        nsent(newproc+1) = nsent(newproc+1) + 1
     endif
  enddo
@@ -204,34 +193,23 @@ subroutine send_celldens(cell,direction,irequestsend,xsendbuf)
 
 end subroutine send_celldens
 
-subroutine send_cellforce(cell,direction,irequestsend,xsendbuf)
+subroutine send_cellforce(cell,targets,irequestsend,xsendbuf)
  use mpiforce, only:cellforce
 
  type(cellforce),    intent(in)     :: cell
- integer,            intent(in)     :: direction
+ logical,            intent(in)     :: targets(nprocs)
  integer,            intent(inout)  :: irequestsend(nprocs)
  type(cellforce),    intent(out)    :: xsendbuf
 
- logical                            :: targets(nprocs)
  integer                            :: newproc
 
 #ifdef MPI
  xsendbuf = cell
-
- ! export
- if (direction == 0) then
-    targets = cell%remote_export(1:nprocs)
-    ! return
- elseif (direction == 1) then
-    targets = .false.
-    targets(cell%owner+1) = .true.
- endif
-
  irequestsend = MPI_REQUEST_NULL
 
  do newproc=0,nprocs-1
     if ((newproc /= id) .and. (targets(newproc+1))) then ! do not send to self
-       call MPI_ISEND(xsendbuf,1,dtype_cellforce,newproc,1+direction,comm_cellexchange,irequestsend(newproc+1),mpierr)
+       call MPI_ISEND(xsendbuf,1,dtype_cellforce,newproc,0,comm_cellexchange,irequestsend(newproc+1),mpierr)
        nsent(newproc+1) = nsent(newproc+1) + 1
     endif
  enddo
@@ -380,10 +358,6 @@ subroutine recv_celldens(target_stack,xbuf,irequestrecv)
              target_stack%cells(iwait)%rhosums(:,k) = target_stack%cells(iwait)%rhosums(:,k) + xbuf(iproc)%rhosums(:,k)
              target_stack%cells(iwait)%nneigh(k) = target_stack%cells(iwait)%nneigh(k) + xbuf(iproc)%nneigh(k)
           enddo
-          do k = 1,nprocs
-             target_stack%cells(iwait)%remote_export(k) = target_stack%cells(iwait)%remote_export(k) &
-                  .and. xbuf(iproc)%remote_export(k)
-          enddo
           target_stack%cells(iwait)%nneightry = target_stack%cells(iwait)%nneightry + xbuf(iproc)%nneightry
           nrecv(iproc) = nrecv(iproc) + 1
        elseif (status(MPI_TAG) == 1) then
@@ -430,10 +404,6 @@ subroutine recv_cellforce(target_stack,xbuf,irequestrecv)
              target_stack%cells(iwait)%fgrav(k) = target_stack%cells(iwait)%fgrav(k) + xbuf(iproc)%fgrav(k)
           enddo
 #endif
-          do k =1,nprocs
-             target_stack%cells(iwait)%remote_export(k) = target_stack%cells(iwait)%remote_export(k) &
-               .and. xbuf(iproc)%remote_export(k)
-          enddo
           target_stack%cells(iwait)%ndrag = target_stack%cells(iwait)%ndrag + xbuf(iproc)%ndrag
           target_stack%cells(iwait)%nstokes = target_stack%cells(iwait)%nstokes + xbuf(iproc)%nstokes
           target_stack%cells(iwait)%nsuper = target_stack%cells(iwait)%nsuper + xbuf(iproc)%nsuper

src/main/dens.F90
@@ -307,7 +307,6 @@ subroutine densityiterate(icall,npart,nactive,xyzh,vxyzu,divcurlv,divcurlB,Bevol
     cell%owner                   = id
     cell%nits                    = 0
     cell%nneigh                  = 0
-    cell%remote_export(1:nprocs) = remote_export
 
     call start_cell(cell,iphase,xyzh,vxyzu,fxyzu,fext,Bevol,rad)
     call get_cell_location(icell,cell%xpos,cell%xsizei,cell%rcuti)
@@ -319,7 +318,7 @@ subroutine densityiterate(icall,npart,nactive,xyzh,vxyzu,divcurlv,divcurlB,Bevol
        if (do_export) then
           if (stack_waiting%n > 0) call check_send_finished(stack_remote,irequestsend,irequestrecv,xrecvbuf)
           call reserve_stack(stack_waiting,cell%waiting_index)  ! make a reservation on the stack
-          call send_cell(cell,0,irequestsend,xsendbuf)  ! export the cell: direction 0 for exporting
+          call send_cell(cell,remote_export,irequestsend,xsendbuf)  ! send the cell to remote
        endif
        !$omp end critical (send_and_recv_remote)
     endif
@@ -341,14 +340,13 @@ subroutine densityiterate(icall,npart,nactive,xyzh,vxyzu,divcurlv,divcurlB,Bevol
                 call get_neighbour_list(-1,listneigh,nneigh,xyzh,ompcache(:,1:3,ithread),maxcellcache,getj=.false., &
                                       cell_xpos=cell%xpos,cell_xsizei=cell%xsizei,cell_rcuti=cell%rcuti, &
                                       remote_export=remote_export)
-                cell%remote_export(1:nprocs) = remote_export
 
                 if (any(remote_export)) then
                    do_export = .true.
                    !$omp critical (send_and_recv_remote)
                    if (stack_waiting%n > 0) call check_send_finished(stack_remote,irequestsend,irequestrecv,xrecvbuf)
                    call reserve_stack(stack_waiting,cell%waiting_index)
-                   call send_cell(cell,0,irequestsend,xsendbuf)  ! direction export (0)
+                   call send_cell(cell,remote_export,irequestsend,xsendbuf) ! send the cell to remote
                    !$omp end critical (send_and_recv_remote)
                 endif
                 nrelink = nrelink + 1
@@ -412,14 +410,14 @@ subroutine densityiterate(icall,npart,nactive,xyzh,vxyzu,divcurlv,divcurlB,Bevol
 
           call compute_cell(cell,listneigh,nneigh,getdv,getdB,Bevol,xyzh,vxyzu,fxyzu,fext,ompcache(:,1:3,ithread),rad)
 
-          cell%remote_export(id+1) = .false.
+          remote_export = .false.
+          remote_export(cell%owner+1) = .true. ! use remote_export array to send back to the owner
 
           ! communication happened while computing contributions to remote cells
           !$omp critical (send_and_recv_waiting)
           call recv_cells(stack_waiting,xrecvbuf,irequestrecv)
           call check_send_finished(stack_waiting,irequestsend,irequestrecv,xrecvbuf)
-          ! direction return (1)
-          call send_cell(cell,1,irequestsend,xsendbuf)
+          call send_cell(cell,remote_export,irequestsend,xsendbuf) ! send the cell back to owner
           !$omp end critical (send_and_recv_waiting)
        enddo over_remote
        !$omp enddo
@@ -446,10 +444,6 @@ subroutine densityiterate(icall,npart,nactive,xyzh,vxyzu,divcurlv,divcurlB,Bevol
        over_waiting: do i = 1, stack_waiting%n
           cell = stack_waiting%cells(i)
 
-          if (any(cell%remote_export(1:nprocs))) then
-             call fatal('dens', 'not all results returned from remote processor')
-          endif
-
           if (calculate_density) then
              call finish_cell(cell,converged)
              call compute_hmax(cell,redo_neighbours)
@@ -466,12 +460,11 @@ subroutine densityiterate(icall,npart,nactive,xyzh,vxyzu,divcurlv,divcurlB,Bevol
              call get_neighbour_list(-1,listneigh,nneigh,xyzh,ompcache(:,1:3,ithread),maxcellcache,getj=.false., &
                                     cell_xpos=cell%xpos,cell_xsizei=cell%xsizei,cell_rcuti=cell%rcuti, &
                                     remote_export=remote_export)
-             cell%remote_export(1:nprocs) = remote_export
+
              !$omp critical (send_and_recv_remote)
              call check_send_finished(stack_remote,irequestsend,irequestrecv,xrecvbuf)
              call reserve_stack(stack_redo,cell%waiting_index)
-             ! direction export (0)
-             call send_cell(cell,0,irequestsend,xsendbuf)
+             call send_cell(cell,remote_export,irequestsend,xsendbuf) ! send the cell to remote
              !$omp end critical (send_and_recv_remote)
              call compute_cell(cell,listneigh,nneigh,getdv,getdB,Bevol,xyzh,vxyzu,fxyzu,fext,ompcache(:,1:3,ithread),rad)
 

src/main/force.F90
@@ -41,9 +41,9 @@ module forces
 !
 ! :Dependencies: boundary, cooling, dim, dust, eos, eos_shen, fastmath,
 !   growth, io, io_summary, kdtree, kernel, linklist, metric_tools,
-!   mpiderivs, mpiforce, mpimemory, mpiutils, nicil, options, part, physcon,
-!   ptmass, ptmass_heating, radiation_utils, timestep, timestep_ind,
-!   timestep_sts, units, utils_gr, viscosity
+!   mpiderivs, mpiforce, mpimemory, mpiutils, nicil, omp_cache, options,
+!   part, physcon, ptmass, ptmass_heating, radiation_utils, timestep,
+!   timestep_ind, timestep_sts, units, utils_gr, viscosity
 !
  use dim, only:maxfsum,maxxpartveciforce,maxp,ndivcurlB,ndivcurlv,&
                maxdusttypes,maxdustsmall,do_radiation
@@ -461,7 +461,7 @@ subroutine force(icall,npart,xyzh,vxyzu,fxyzu,divcurlv,divcurlB,Bevol,dBevol,&
                            remote_export=remote_export)
 
     cell%owner                   = id
-    cell%remote_export(1:nprocs) = remote_export
+
     do_export = any(remote_export)
 
     if (mpi) then
@@ -470,7 +470,7 @@ subroutine force(icall,npart,xyzh,vxyzu,fxyzu,divcurlv,divcurlB,Bevol,dBevol,&
        if (do_export) then
           if (stack_waiting%n > 0) call check_send_finished(stack_remote,irequestsend,irequestrecv,xrecvbuf)
           call reserve_stack(stack_waiting,cell%waiting_index)
-          call send_cell(cell,0,irequestsend,xsendbuf)  ! export the cell: direction 0 for exporting
+          call send_cell(cell,remote_export,irequestsend,xsendbuf)  ! send to remote
        endif
        !$omp end critical (send_and_recv_remote)
     endif
@@ -529,12 +529,12 @@ subroutine force(icall,npart,xyzh,vxyzu,fxyzu,divcurlv,divcurlB,Bevol,dBevol,&
                          dustfrac,dustprop,gradh,ibinnow_m1,ibin_wake,stressmax,ompcache(:,:,ithread),&
                          rad,radprop,dens,metrics)
 
-       cell%remote_export(id+1) = .false.
-
+       remote_export = .false.
+       remote_export(cell%owner+1) = .true. ! use remote_export array to send back to the owner
        !$omp critical (send_and_recv_waiting)
        call recv_cells(stack_waiting,xrecvbuf,irequestrecv)
        call check_send_finished(stack_waiting,irequestsend,irequestrecv,xrecvbuf)
-       call send_cell(cell,1,irequestsend,xsendbuf)
+       call send_cell(cell,remote_export,irequestsend,xsendbuf) ! send the cell back to owner
        !$omp end critical (send_and_recv_waiting)
 
     enddo over_remote
@@ -559,10 +559,6 @@ subroutine force(icall,npart,xyzh,vxyzu,fxyzu,divcurlv,divcurlB,Bevol,dBevol,&
     !$omp do schedule(runtime)
     over_waiting: do i = 1, stack_waiting%n
        cell = stack_waiting%cells(i)
-
-       if (any(cell%remote_export(1:nprocs))) then
-          call fatal('force', 'not all results returned from remote processor')
-       endif
 
        call finish_cell_and_store_results(icall,cell,fxyzu,xyzh,vxyzu,poten,dt,dvdx, &
                                           divBsymm,divcurlv,dBevol,ddustevol,deltav,dustgasprop, &
@@ -3006,24 +3002,6 @@ subroutine finish_cell_and_store_results(icall,cell,fxyzu,xyzh,vxyzu,poten,dt,dv
 #endif
  enddo over_parts
 end subroutine finish_cell_and_store_results
-
-pure subroutine combine_cells(cella, cellb)
- type(cellforce),   intent(inout)        :: cella
- type(cellforce),   intent(in)           :: cellb
-
- integer                                 :: i
-
- do i = 1,cella%npcell
-    cella%fsums(:,i) = cella%fsums(:,i) + cellb%fsums(:,i)
- enddo
-
- cella%ndrag   = cella%ndrag     + cellb%ndrag
- cella%nstokes = cella%nstokes   + cellb%nstokes
- cella%nsuper  = cella%nsuper    + cellb%nsuper
-
- cella%remote_export = (cella%remote_export .and. cellb%remote_export)
-
-end subroutine combine_cells
 
 !-----------------------------------------------------------------------------
 !+

src/main/mpi_dens.F90
@@ -17,7 +17,7 @@ module mpidens
 ! :Dependencies: dim, io, mpi, mpiutils
 !
  use io,       only:nprocs,fatal,error
- use dim,      only:minpart,maxrhosum,maxprocs,maxxpartvecidens
+ use dim,      only:minpart,maxrhosum,maxxpartvecidens
 
  implicit none
  private
@@ -44,9 +44,8 @@ module mpidens
     integer          :: nneightry
     integer          :: nneigh(minpart)                        ! number of actual neighbours (diagnostic)
     integer          :: waiting_index
-    logical          :: remote_export(maxprocs)                ! remotes we are waiting for
     integer(kind=1)  :: iphase(minpart)
-    integer(kind=1)  :: pad(8 - mod(4 * (6 + 2 * minpart + maxprocs) + minpart, 8))
+    integer(kind=1)  :: pad(8 - mod(4 * (6 + 2 * minpart) + minpart, 8))
  end type celldens
 
  type stackdens
@@ -66,7 +65,7 @@ subroutine get_mpitype_of_celldens(dtype)
  use mpiutils, only:mpierr
  use io,       only:error
 
- integer, parameter              :: ndata = 20
+ integer, parameter              :: ndata = 18
 
  integer, intent(out)            :: dtype
  integer                         :: nblock, blens(ndata), mpitypes(ndata)
@@ -176,19 +175,13 @@ subroutine get_mpitype_of_celldens(dtype)
  disp(nblock) = addr - start
 
  nblock = nblock + 1
- blens(nblock) = size(cell%remote_export)
- mpitypes(nblock) = MPI_LOGICAL
- call MPI_GET_ADDRESS(cell%remote_export,addr,mpierr)
- disp(nblock) = addr - start
-
- nblock = nblock + 1
  blens(nblock) = size(cell%iphase)
  mpitypes(nblock) = MPI_INTEGER1
  call MPI_GET_ADDRESS(cell%iphase,addr,mpierr)
  disp(nblock) = addr - start
 
  nblock = nblock + 1
- blens(nblock) = 8 - mod(4 * (6 + 2 * minpart + maxprocs) + minpart, 8)
+ blens(nblock) = 8 - mod(4 * (6 + 2 * minpart) + minpart, 8)
  mpitypes(nblock) = MPI_INTEGER1
  call MPI_GET_ADDRESS(cell%pad,addr,mpierr)
  disp(nblock) = addr - start

src/main/mpi_force.F90
@@ -17,7 +17,7 @@ module mpiforce
 ! :Dependencies: dim, io, mpi, mpiutils
 !
  use io,       only:nprocs,fatal
- use dim,      only:minpart,maxfsum,maxprocs,maxxpartveciforce
+ use dim,      only:minpart,maxfsum,maxxpartveciforce
 
  implicit none
  private
@@ -44,10 +44,9 @@ module mpiforce
     integer          :: nsuper
     integer          :: owner                                  ! id of the process that owns this
     integer          :: waiting_index
-    logical          :: remote_export(maxprocs)                ! remotes we are waiting for
     integer(kind=1)  :: iphase(minpart)
     integer(kind=1)  :: ibinneigh(minpart)
-    integer(kind=1)  :: pad(8 - mod(4 * (7 + minpart + maxprocs) + 2*minpart, 8)) !padding to maintain alignment of elements
+    integer(kind=1)  :: pad(8 - mod(4 * (7 + minpart) + 2*minpart, 8)) !padding to maintain alignment of elements
  end type cellforce
 
  type stackforce
@@ -67,7 +66,7 @@ subroutine get_mpitype_of_cellforce(dtype)
  use mpiutils, only:mpierr
  use io,       only:error
 
- integer, parameter              :: ndata = 20
+ integer, parameter              :: ndata = 19
 
  integer, intent(out)            :: dtype
  integer                         :: nblock, blens(ndata), mpitypes(ndata)
@@ -177,12 +176,6 @@ subroutine get_mpitype_of_cellforce(dtype)
  disp(nblock) = addr - start
 
  nblock = nblock + 1
- blens(nblock) = size(cell%remote_export)
- mpitypes(nblock) = MPI_LOGICAL
- call MPI_GET_ADDRESS(cell%remote_export,addr,mpierr)
- disp(nblock) = addr - start
-
- nblock = nblock + 1
  blens(nblock) = size(cell%iphase)
  mpitypes(nblock) = MPI_INTEGER1
  call MPI_GET_ADDRESS(cell%iphase,addr,mpierr)
@@ -195,7 +188,7 @@ subroutine get_mpitype_of_cellforce(dtype)
  disp(nblock) = addr - start
 
  nblock = nblock + 1
- blens(nblock) = 8 - mod(4 * (7 + minpart + maxprocs) + 2*minpart, 8)
+ blens(nblock) = 8 - mod(4 * (7 + minpart) + 2*minpart, 8)
  mpitypes(nblock) = MPI_INTEGER1
  call MPI_GET_ADDRESS(cell%pad,addr,mpierr)
  disp(nblock) = addr - start

commit 0f80e28feadfbd50d8a7e3693aecf1d18e1ed728

bugfix: 585c24ff09014fb7934582229dca877ed007b87f broke the message tag

src/main/mpi_derivs.F90
@@ -15,8 +15,8 @@ module mpiderivs
 !
 ! :Runtime parameters: None
 !
-! :Dependencies: dim, dtypekdtree, io, mpi, mpidens, mpiforce, mpimemory,
-!   mpiutils
+! :Dependencies: allocutils, dtypekdtree, io, mpi, mpidens, mpiforce,
+!   mpimemory, mpiutils
 !
 #ifdef MPI
  use mpi
@@ -93,15 +93,15 @@ module mpiderivs
 contains
 
 subroutine allocate_comms_arrays
-   use allocutils, only:allocate_array
-   call allocate_array('nsent',       nsent,       nprocs)
-   call allocate_array('nexpect',     nexpect,     nprocs)
-   call allocate_array('nrecv',       nrecv,       nprocs)
-   call allocate_array('countrequest',countrequest,nprocs)
-   call allocate_array('comm_cofm',   comm_cofm,   nprocs)
-   call allocate_array('comm_owner',  comm_owner,  nprocs)
+ use allocutils, only:allocate_array
+ call allocate_array('nsent',       nsent,       nprocs)
+ call allocate_array('nexpect',     nexpect,     nprocs)
+ call allocate_array('nrecv',       nrecv,       nprocs)
+ call allocate_array('countrequest',countrequest,nprocs)
+ call allocate_array('comm_cofm',   comm_cofm,   nprocs)
+ call allocate_array('comm_owner',  comm_owner,  nprocs)
 
-   call init_tree_comms
+ call init_tree_comms
 end subroutine allocate_comms_arrays
 
 !----------------------------------------------------------------
@@ -190,14 +190,21 @@ subroutine send_celldens(cell,targets,irequestsend,xsendbuf)
  type(celldens),     intent(out)    :: xsendbuf
 
  integer                            :: newproc
+ integer                            :: tag
 
 #ifdef MPI
  xsendbuf = cell
  irequestsend = MPI_REQUEST_NULL
 
+ if (targets(cell%owner)) then
+    tag = 2
+ else
+    tag = 1
+ endif
+
  do newproc=0,nprocs-1
     if ((newproc /= id) .and. (targets(newproc+1))) then ! do not send to self
-       call MPI_ISEND(xsendbuf,1,dtype_celldens,newproc,0,comm_cellexchange,irequestsend(newproc+1),mpierr)
+       call MPI_ISEND(xsendbuf,1,dtype_celldens,newproc,tag,comm_cellexchange,irequestsend(newproc+1),mpierr)
        nsent(newproc+1) = nsent(newproc+1) + 1
     endif
  enddo
@@ -214,14 +221,21 @@ subroutine send_cellforce(cell,targets,irequestsend,xsendbuf)
  type(cellforce),    intent(out)    :: xsendbuf
 
  integer                            :: newproc
+ integer                            :: tag
 
 #ifdef MPI
  xsendbuf = cell
  irequestsend = MPI_REQUEST_NULL
 
+ if (targets(cell%owner)) then
+    tag = 2
+ else
+    tag = 1
+ endif
+
  do newproc=0,nprocs-1
     if ((newproc /= id) .and. (targets(newproc+1))) then ! do not send to self
-       call MPI_ISEND(xsendbuf,1,dtype_cellforce,newproc,0,comm_cellexchange,irequestsend(newproc+1),mpierr)
+       call MPI_ISEND(xsendbuf,1,dtype_cellforce,newproc,tag,comm_cellexchange,irequestsend(newproc+1),mpierr)
        nsent(newproc+1) = nsent(newproc+1) + 1
     endif
  enddo