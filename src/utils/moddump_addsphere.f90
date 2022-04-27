e7aec39ef182e8a511b20e5c899751d73fea6501
joshcalcino <josh.calcino@gmail.com>
2022-04-26 23:07:07 -0600

    bug fix

--- src/utils/moddump_addsphere.f90
+++ src/utils/moddump_addsphere.f90
@@ -22,7 +22,7 @@ contains
 
 subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  use partinject, only:add_or_update_particle
- use part,              only:igas
+ use part,              only:igas,xyzmh_ptmass
  use spherical,         only:set_sphere
  use kernel,       only:hfact_default
  use prompting,    only:prompt
@@ -34,15 +34,15 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  real,    intent(inout) :: massoftype(:)
  real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
  real, dimension(:,:), allocatable :: xyzh_add,vxyzu_add
- real                   :: totmass,psep,hfact,totmass_sphere,r_sphere,pmass
- real    :: xp(3)
+ real                   :: totmass,psep,hfact,totmass_sphere,r_sphere,pmass,vphi,star_m,x_sphere
+ real    :: xp(3), vp(3)
  integer                :: i,np_add,np,ipart,in_disk,r_disk
  integer(kind=8) :: nptot
  hfact = hfact_default
  totmass_sphere = 1.0
  pmass = massoftype(igas)
  in_disk = 0
- r_disk = 100.
+ x_sphere = 100.
 
  call prompt('Enter total mass in sphere ',totmass_sphere,0.)
  r_sphere = 4.
@@ -68,16 +68,17 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
 write(*,*), "The sphere has been succesfully initialised."
 
 if (in_disk==1) then
+   star_m = xyzmh_ptmass(4,1)
    vphi = sqrt((star_m)/x_sphere)
-   vxyzu_add(1:3,:) = (/0.,vphi,0./)
+   vp = (/0.,vphi,0./)
 else
-   vxyzu_add(1:3,:) = (/0.,0.,0./)
+   vp = (/0.,0.,0./)
 end if
 
  do i = 1,np_add
     ! Add the particle
     ipart = ipart + 1
-    call  add_or_update_particle(igas, xyzh_add(1:3,i), vxyzu_add(1:3,i), xyzh_add(4,i), &
+    call  add_or_update_particle(igas, xyzh_add(1:3,i), vp, xyzh_add(4,i), &
                          vxyzu_add(4,i), ipart, npart, npartoftype, xyzh, vxyzu)
  enddo
 
