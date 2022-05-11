22164d9fb61a6ddf3b6c89d6213e93236c63d277
Lionel Siess <Lionel.Siess@astro.ulb.ac.be>
2022-05-11 12:02:10 +0200

    (config) increase the number of particle to 5.2 millions

--- src/main/config.F90
+++ src/main/config.F90
@@ -35,7 +35,7 @@ module dim
 #ifdef MAXP
  integer, parameter :: maxp_hard = MAXP
 #else
- integer, parameter :: maxp_hard = 1200000
+ integer, parameter :: maxp_hard = 5200000
 #endif
 
  ! maximum number of point masses
