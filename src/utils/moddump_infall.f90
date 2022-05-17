4b6dd88f84a971a4b7bd6d9c66e129dc75641f5c
joshcalcino <josh.calcino@gmail.com>
2022-05-17 16:30:38 -0600

    update how ellipse is added

--- src/utils/moddump_infall.f90
+++ src/utils/moddump_infall.f90
@@ -42,15 +42,15 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  integer, intent(inout) :: npartoftype(:)
  real,    intent(inout) :: massoftype(:)
  real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
- real, dimension(:,:), allocatable :: xyzh_add,vxyzu_add(:,:)
+ real, dimension(:,:), allocatable :: xyzh_add,vxyzu_add,xyzh_add2
  integer :: in_shape,in_orbit,ipart,i,n_add,np,use_star
  integer(kind=8) :: nptot
  integer, parameter :: iunit = 23
- real    :: r_close,in_mass,hfact,pmass,delta,r_init,r_in,r_a,inc,big_omega
+ real    :: r_close,in_mass,hfact,pmass,delta,r_init,r_init_min,r_in,r_a,inc,big_omega
  real    :: v_inf,b,b_frac,theta_def,b_crit,a,ecc,accr_star
- real    :: vp(3), xp(3), rot_axis(3), rellipsoid(3)
+ real    :: vp(3), xp(3), rot_axis(3), rellipsoid(3), xp2(3)
  real    :: dma,n0,pf,m0,x0,y0,z0,r0,vx0,vy0,vz0,mtot,tiny_number,ang
- real    :: theta1,y1,x1,y0t,y0_corr
+ real    :: y1,x1,dx,x_prime,y_prime,theta
  real    :: unit_velocity, G
  logical :: lrhofunc,call_prompt
  procedure(rho_func), pointer :: prhofunc
@@ -106,8 +106,12 @@ if (call_prompt) then
    endif
    call prompt('Enter infall mass in Msun:', in_mass, 0.0)
    call prompt('Enter value of power-law density along radius:', r_slope, 0.0)
-   write(*,*), "Initial radial distance is either centre of star/sphere, or tip of ellipse."
-   call prompt('Enter initial radial distance in au:', r_init, 0.0)
+   write(*,*), "Initial radial distance is centre of star/sphere or ellipse."
+   r_init_min = 0.0
+   ! if (in_shape == 1) then
+   !   r_init_min = r_a/2
+   ! endif
+   call prompt('Enter initial radial distance in au:', r_init, r_init_min)
 
    if (r_slope > tiny_number) then
       prhofunc => rhofunc
@@ -182,7 +186,7 @@ endif
    ! Parabolic orbit, taken from set_flyby
    dma = r_close
    if (in_shape==1) then
-     n0  = (r_init+r_in)/r_close
+     n0  = (r_init+r_a)/r_close
    else
      n0  = r_init/r_close
    endif
@@ -238,17 +242,22 @@ endif
     call set_ellipse('random',id,master,rellipsoid,delta,hfact,xyzh_add,np,xyz_origin=xp,&
      np_requested=n_add, nptot=nptot)
      ! Need to correct the ellipse
-     y0t = maxval(xyzh_add(2, :))
-     print*, "n_add is ", n_add
-
      do i = 1,n_add
        x1 = xyzh_add(1, i)
        y1 = xyzh_add(2, i)
-       theta1 = asin((y0t + r_in)/sqrt(x0**2+(y0t**2+r_in**2)))
-       xyzh_add(1, i) = x1 + (y1 - y0t)*sin(theta1)
-       xyzh_add(2, i) = y0t + (y1 - y0t)*cos(theta1)
+       dma = r_close
+       n0  = (sqrt(x1**2 + y1**2))/r_close
+       pf = 2*dma
+       m0 = 2*sqrt(n0-1.0)
+       x0 = -m0*dma
+       y0 = dma*(1.0-(x0/pf)**2)
+       theta = atan(abs(y0)/(x0-abs(y0)/2))
+       dx = xyzh_add(1, i) - xp(1)
+       x_prime = dx * cos(theta)
+       y_prime = dx * sin(theta)
+       xyzh_add(1, i) = x0 + x_prime
+       xyzh_add(2, i) = y0 + y_prime
      enddo
-
    write(*,*), "The ellipse has been succesfully initialised."
  endif
 endif
