3fbd8e9606360f5378a4cfbd9bddd06ac1816dcc
Daniel Price <daniel.price@monash.edu>
2022-06-02 23:54:44 +1000

    (docs) bug fix in table [skip ci]

--- docs/scripts/print_setups.sh
+++ docs/scripts/print_setups.sh
@@ -9,43 +9,43 @@ echo "phantom compile-time configurations"
 echo "==================================="
 echo ""
 printf "+"
-printf -- '-%.0s' {1..105}
+printf -- '-%.0s' {1..18}
 printf "+"
 printf -- '-%.0s' {1..63}
 printf "+"
-printf -- '-%.0s' {1..63}
+printf -- '-%.0s' {1..52}
 printf "+"
-printf -- '-%.0s' {1..63}
+printf -- '-%.0s' {1..123}
 printf "+\n"
-printf "| %-103s | %-61s | %-61s | %-61s |  \n" "setup"  "description"  "compile-time options" "initial conditions file"
+printf "| %-16s | %-61s | %-50s | %-121s |  \n" "setup" "description" "compile-time options"  "initial conditions file"
 printf "+"
-printf -- '=%.0s' {1..105}
-printf "+"
-printf -- '=%.0s' {1..63}
+printf -- '=%.0s' {1..18}
 printf "+"
 printf -- '=%.0s' {1..63}
 printf "+"
-printf -- '=%.0s' {1..63}
+printf -- '=%.0s' {1..52}
+printf "+"
+printf -- '=%.0s' {1..123}
 printf "+\n"
 allsetups=`grep 'ifeq ($(SETUP)' $phantomdir/build/Makefile_setups | grep -v skip | cut -d, -f 2 | cut -d')' -f 1 | sort`
 for setup in $allsetups; do
     descript=`grep -A 1 "ifeq (\\$(SETUP), $setup)" $phantomdir/build/Makefile_setups | grep '#' | cut -d'#' -f 2 | tail -1 | xargs`
-    lineno=`grep -n "ifeq (\\$(SETUP), $setup)" $phantomdir/build/Makefile_setups | cut -d':' -f 1`
+    #lineno=`grep -n "ifeq (\\$(SETUP), $setup)" $phantomdir/build/Makefile_setups | cut -d':' -f 1`
     options=`cd $phantomdir; make SETUP=$setup get_setup_opts`
     setupfile=`cd $phantomdir; make SETUP=$setup get_setup_file`
     lastfile='';
     for x in $setupfile; do
         lastfile=$x;
     done
-    printf "| %-103s | %-61s | %-61s | %-61s |  \n" "\`$setup <$url/build/Makefile_setups#L$lineno>\`__" "$descript" "$options" "\`$lastfile <$url/src/setup/$lastfile>\`__"
+    printf "| %-16s | %-61s | %-50s | %-121s |  \n" "$setup" "$descript" "$options" "\`$lastfile <$url/src/setup/$lastfile>\`__"
     printf "+"
-    printf -- '-%.0s' {1..105}
-    printf "+"
-    printf -- '-%.0s' {1..63}
+    printf -- '-%.0s' {1..18}
     printf "+"
     printf -- '-%.0s' {1..63}
     printf "+"
-    printf -- '-%.0s' {1..63}
+    printf -- '-%.0s' {1..52}
+    printf "+"
+    printf -- '-%.0s' {1..123}
     printf "+\n"
 done
 echo
