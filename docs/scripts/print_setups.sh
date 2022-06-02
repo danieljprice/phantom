aa225a0cad858d158eb2f5861fec82c0f8218a2f
Daniel Price <daniel.price@monash.edu>
2022-06-03 09:57:41 +1000

    (docs) added list of pre-cooked setups to docs [skip ci]

--- docs/scripts/print_setups.sh
+++ docs/scripts/print_setups.sh
@@ -5,8 +5,6 @@
 #
 phantomdir='../../'
 url="https://github.com/danieljprice/phantom/blob/master/"
-echo "phantom compile-time configurations"
-echo "==================================="
 echo ""
 printf "+"
 printf -- '-%.0s' {1..18}
@@ -27,25 +25,34 @@ printf -- '=%.0s' {1..52}
 printf "+"
 printf -- '=%.0s' {1..123}
 printf "+\n"
-allsetups=`grep 'ifeq ($(SETUP)' $phantomdir/build/Makefile_setups | grep -v skip | cut -d, -f 2 | cut -d')' -f 1 | sort`
-for setup in $allsetups; do
-    descript=`grep -A 1 "ifeq (\\$(SETUP), $setup)" $phantomdir/build/Makefile_setups | grep '#' | cut -d'#' -f 2 | tail -1 | xargs`
-    #lineno=`grep -n "ifeq (\\$(SETUP), $setup)" $phantomdir/build/Makefile_setups | cut -d':' -f 1`
-    options=`cd $phantomdir; make SETUP=$setup get_setup_opts`
-    setupfile=`cd $phantomdir; make SETUP=$setup get_setup_file`
-    lastfile='';
-    for x in $setupfile; do
+print_setup()
+{
+  setup=$1;
+  descript=`grep -A 1 "ifeq (\\$(SETUP), $setup)" $phantomdir/build/Makefile_setups | grep '#' | cut -d'#' -f 2 | tail -1 | xargs`
+  #lineno=`grep -n "ifeq (\\$(SETUP), $setup)" $phantomdir/build/Makefile_setups | cut -d':' -f 1`
+  options=`cd $phantomdir; make SETUP=$setup get_setup_opts`
+  setupfile=`cd $phantomdir; make SETUP=$setup get_setup_file`
+  lastfile='';
+  for x in $setupfile; do
         lastfile=$x;
-    done
-    printf "| %-16s | %-61s | %-50s | %-121s |  \n" "$setup" "$descript" "$options" "\`$lastfile <$url/src/setup/$lastfile>\`__"
-    printf "+"
-    printf -- '-%.0s' {1..18}
-    printf "+"
-    printf -- '-%.0s' {1..63}
-    printf "+"
-    printf -- '-%.0s' {1..52}
-    printf "+"
-    printf -- '-%.0s' {1..123}
-    printf "+\n"
+  done
+  printf "| %-16s | %-61s | %-50s | %-121s |  \n" "$setup" "$descript" "$options" "\`$lastfile <$url/src/setup/$lastfile>\`__"
+  printf "+"
+  printf -- '-%.0s' {1..18}
+  printf "+"
+  printf -- '-%.0s' {1..63}
+  printf "+"
+  printf -- '-%.0s' {1..52}
+  printf "+"
+  printf -- '-%.0s' {1..123}
+  printf "+\n"
+}
+if [ "$1" == "best" ]; then
+   listofsetups='disc star cluster turb'
+else
+   listofsetups=`grep 'ifeq ($(SETUP)' $phantomdir/build/Makefile_setups | grep -v skip | cut -d, -f 2 | cut -d')' -f 1 | sort`
+fi
+for setup in $listofsetups; do
+    print_setup $setup;
 done
 echo
