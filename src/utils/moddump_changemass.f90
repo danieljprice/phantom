5b2238b3213ef2c28225abf58f3111ff10a83d23
joshcalcino <josh.calcino@gmail.com>
2022-06-09 11:37:23 -0600

    update changemass

--- src/utils/moddump_changemass.f90
+++ src/utils/moddump_changemass.f90
@@ -21,13 +21,24 @@ module moddump
 contains
 
 subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
- use part, only : igas
+ use part, only : igas,isdead_or_accreted,kill_particle,shuffle_part
  use units, only : umass
  integer, intent(inout) :: npart
  integer, intent(inout) :: npartoftype(:)
  real,    intent(inout) :: massoftype(:)
  real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
  real     :: disc_mass, current_disc_mass, mass_factor
+ integer  :: i
+
+  ! Remove particles that are dead or accreted
+  do i=1,npartoftype(igas)
+    if (isdead_or_accreted(xyzh(4,i))) then
+      call kill_particle(i)
+    endif
+  enddo
+
+  call shuffle_part(npart)
+  npartoftype(igas) = npart
 
  ! Specify the disc mass, or explicitly set the mass factor to scale gas mass
  disc_mass = 0.05
@@ -39,6 +50,8 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  print*,'Particle mass is now ', massoftype(igas)*umass, ' g'
  print*,'Total disc mass is now ', npartoftype(igas)*massoftype(igas)*umass, ' g'
  print*,'Total disc mass is now ', npartoftype(igas)*massoftype(igas), 'Msun'
+
+
 
  return
 end subroutine modify_dump
