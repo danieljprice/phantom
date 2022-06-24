f9c6c676fecda01d4661f2a6597f86d158ea59e6
Mike Lau <mike.lau@monash.edu>
2022-06-23 21:13:33 -0400

    (implicit rad) add error message

--- src/main/radiation_implicit.f90
+++ src/main/radiation_implicit.f90
@@ -1,22 +1,24 @@
 module radiation_implicit
-
  implicit none
+ integer, parameter :: ierr_failed_to_converge = 1
 
- contains
+contains
 
-
- subroutine do_radiation_implicit(dt,dtmax,npart,rad,xyzh,vxyzu,drad)
+ subroutine do_radiation_implicit(dt,dtmax,npart,rad,xyzh,vxyzu,drad,ierr)
   integer, intent(in) :: npart
   real, intent(in) :: dt,dtmax,rad(:,:),xyzh(:,:),vxyzu(:,:),drad(:,:)
+  integer, intent(out) :: ierr
   integer :: nsubsteps,i,nit
   logical :: failed
   real :: dtsub,errorE,errorU
   
+  ierr = 0
   nsubsteps = 1
 
   dtsub = dt
   do i = 1,nsubsteps
      call do_radiation_onestep(dtsub,dtmax,npart,rad,xyzh,vxyzu,drad,failed,nit,errorE,errorU)
+     if (failed) ierr = ierr_failed_to_converge
   enddo
 
 
@@ -40,6 +42,8 @@ module radiation_implicit
   ccode = get_c_code()
   acode = get_radconst_code()
   !dtimax = dt/imaxstep
+
+  print*,"dt=",dt
 
  end subroutine do_radiation_onestep
 
