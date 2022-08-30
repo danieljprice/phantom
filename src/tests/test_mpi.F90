d0c7373cf3aa95d34d121be5f450c3e075406ccc
David Liptai <dliptai@swin.edu.au>
2022-08-30 11:39:49 +1000

    add framework for mpi unit tests

---
+++ src/tests/test_mpi.F90
@@ -0,0 +1,51 @@
+!--------------------------------------------------------------------------!
+! The Phantom Smoothed Particle Hydrodynamics code, by Daniel Price et al. !
+! Copyright (c) 2007-2022 The Authors (see AUTHORS)                        !
+! See LICENCE file for usage and distribution conditions                   !
+! http://phantomsph.bitbucket.io/                                          !
+!--------------------------------------------------------------------------!
+module testmpi
+!
+! MPI unit tests
+!
+! :References: None
+!
+! :Owner: Not Committed Yet
+!
+! :Runtime parameters: None
+!
+! :Dependencies: io, physcon, testutils, units
+!
+ use testutils, only:checkval,checkvalbuf,checkvalbuf_end,update_test_scores
+ implicit none
+
+ public :: test_mpi
+
+ private
+
+contains
+
+subroutine test_mpi(ntests,npass)
+ use io,      only:id,master
+ use units,   only:set_units
+ use physcon, only:solarm
+ integer, intent(inout)   :: ntests,npass
+
+ call set_units(mass=1.d6*solarm,G=1.d0,c=1.d0)
+ if (id==master) write(*,"(/,a,/)") '--> TESTING MPI'
+ call test_increase_mpi_memory(ntests,npass)
+ if (id==master) write(*,"(/,a)") '<-- MPI TESTS COMPLETE'
+
+end subroutine test_mpi
+
+subroutine test_increase_mpi_memory(ntests,npass)
+ integer, intent(inout) :: ntests,npass
+ integer :: nerr(1)
+
+ nerr = 0
+
+ call update_test_scores(ntests,nerr,npass)
+
+end subroutine test_increase_mpi_memory
+
+end module testmpi
