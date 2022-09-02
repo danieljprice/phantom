13a1a70c0c5cb48ebc7cc695a0c367e3e6ccdf64
David Liptai <dliptai@swin.edu.au>
2022-09-02 16:35:51 +1000

    (testmpi) use smaller stack and write+check values in cells
    github runners are limited in memory and require quite a small stack for this test to work

--- src/tests/test_mpi.F90
+++ src/tests/test_mpi.F90
@@ -14,7 +14,7 @@ module testmpi
 !
 ! :Runtime parameters: None
 !
-! :Dependencies: io, mpimemory, physcon, testutils, units
+! :Dependencies: io, mpiforce, mpimemory, physcon, testutils, units
 !
  use testutils, only:checkval,checkvalbuf,checkvalbuf_end,update_test_scores
  implicit none
@@ -39,17 +39,19 @@ subroutine test_mpi(ntests,npass)
 end subroutine test_mpi
 
 subroutine test_increase_mpi_memory(ntests,npass)
- use io,        only:id
  use mpimemory, only:allocate_mpi_memory,increase_mpi_memory,&
-                     deallocate_mpi_memory,stacksize
+                     deallocate_mpi_memory,stacksize, force_stack_1,&
+                     push_onto_stack
+ use mpiforce,  only:cellforce
  integer, intent(inout) :: ntests,npass
- integer :: nerr(1)
-
- integer :: stacksize_orig
+ integer, parameter :: new_stacksize=100
+ type(cellforce) :: cell
+ integer         :: nerr(3), ncheck(3), i, stacksize_orig
+ real            :: maxerr(3)
 
  nerr = 0
-
- print*,id,"Original stacksize=",stacksize
+ ncheck = 0
+ maxerr = 0.
 
  ! Save original stacksize
  stacksize_orig = stacksize
@@ -57,22 +59,41 @@ subroutine test_increase_mpi_memory(ntests,npass)
  ! Deallocate existing stack
  call deallocate_mpi_memory
 
- ! Allocate the stacks again at half the size. Assuming that the original allocation
- ! was fine, increasing it by a factor of 1.5 should be no problem.
- call allocate_mpi_memory(stacksize_in=int(stacksize_orig*0.4))
+ ! Allocate the stacks again at a smaller size.
+ call allocate_mpi_memory(stacksize_in=new_stacksize)
 
- print*,id,"Allocated stacksize=",int(stacksize_orig*0.4)
+ ! Write some data to each cell
+ do i=1,new_stacksize
+    cell%xpos = [1.,2.,3.] * i
+    call push_onto_stack(force_stack_1, cell)
+ enddo
+
+ ! Ensure size of force_stack_1 is what we expect it to be
+ call checkval(force_stack_1%n,new_stacksize,0,nerr(1),'stacksize after pushing cells')
+ call update_test_scores(ntests,nerr,npass)
 
  ! Trigger a stacksize increase - if this doesn't segfault, that's a good sign
  call increase_mpi_memory
 
- print*,id,"Increased to stacksize=",stacksize
+ ! Ensure stack size hasn't changed
+ call checkval(force_stack_1%n,new_stacksize,0,nerr(1),'stacksize after mem increase')
+ call update_test_scores(ntests,nerr,npass)
 
- !  Reallocate previous stack
+ ! Check cell data is the same as what was written into cells above
+ do i=1,new_stacksize
+    call checkvalbuf(force_stack_1%cells(i)%xpos(1),1.*i,1.e-15,'error in xpos(1) after mem increase',nerr(1),ncheck(1),maxerr(1))
+    call checkvalbuf(force_stack_1%cells(i)%xpos(2),2.*i,1.e-15,'error in xpos(2) after mem increase',nerr(2),ncheck(2),maxerr(2))
+    call checkvalbuf(force_stack_1%cells(i)%xpos(3),3.*i,1.e-15,'error in xpos(3) after mem increase',nerr(3),ncheck(3),maxerr(3))
+ enddo
+
+ call checkvalbuf_end('error in xpos(1) after mem increase asfgd',ncheck(1),nerr(1),maxerr(1),1.e-15)
+ call checkvalbuf_end('error in xpos(2) after mem increase asfgd',ncheck(2),nerr(2),maxerr(2),1.e-15)
+ call checkvalbuf_end('error in xpos(3) after mem increase asfgd',ncheck(3),nerr(3),maxerr(3),1.e-15)
+ call update_test_scores(ntests,nerr,npass)
+
+ ! Reallocate previous stack
  call deallocate_mpi_memory
  call allocate_mpi_memory(stacksize_in=stacksize_orig)
-
- call update_test_scores(ntests,nerr,npass)
 
 end subroutine test_increase_mpi_memory
 
