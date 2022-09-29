8d565e63799b124a57a3bc566d7fdab26ff969d7
Mike Lau <mike.lau@monash.edu>
2022-09-29 12:55:10 +1000

    (moddump-binary) add error message if maxp too small to duplicate star 1

--- src/utils/moddump_binary.f90
+++ src/utils/moddump_binary.f90
@@ -38,7 +38,7 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  use infile_utils,      only:open_db_from_file,inopts,read_inopt,close_db
  use table_utils,       only:yinterp
  use rho_profile,       only:read_mesa
- use dim,               only:maxptmass
+ use dim,               only:maxptmass,maxp
  use io,                only:fatal,idisk1,iprint
  use timestep,          only:tmax,dtmax
  use readwrite_dumps,   only:read_dump
@@ -280,6 +280,11 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
           call prompt('Enter no. of particles in second dumpfile',nstar2)
 
           ! Move star 1 particles to avoid getting overwritten when reading second dump file.
+          if (2*nstar1 > maxp) then  ! Check if particle array is large enough to provide particle-copying buffer
+             print*, 'Error: Two times number of particles in star 1 exceeds MAXP'
+             print*, 'Need to compile with larger MAXP'
+             stop
+          endif
           if (nstar1 > nstar2) then ! Move ith particle of star 1 to nstar1+i
              do i=1,nstar1
                 call copy_particle(i,nstar1+i,.false.)
