7bf68a8b1f5a36965176bafa317e22552f863efe
Daniel Price <daniel.price@monash.edu>
2022-11-02 10:23:40 +1100

    (config) update tagline

--- src/main/config.F90
+++ src/main/config.F90
@@ -28,7 +28,7 @@ module dim
  public
 
  character(len=80), parameter :: &
-    tagline='Phantom v'//phantom_version_string//' (c) 2007-2020 The Authors'
+    tagline='Phantom v'//phantom_version_string//' (c) 2007-2023 The Authors'
 
  ! maximum number of particles
  integer :: maxp = 0 ! memory not allocated initially
