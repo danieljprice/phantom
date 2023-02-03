35451ff3d7e6cbd25947b4d5b15b06281e00dc01
Daniel Price <daniel.price@monash.edu>
2023-02-03 18:27:59 +1100

    (damping) implemented radial damping zones for disc boundaries

--- src/main/damping.f90
+++ src/main/damping.f90
@@ -9,7 +9,10 @@ module damping
 ! Various implementations for artificial damping of velocities
 ! either to relax particles into equilibrium or enforce boundary conditions
 !
-! :References: None
+! :References:
+!   Gingold & Monaghan (1977) MNRAS 181, 375
+!   Reichardt et al. (2019) MNRAS 484, 631 (used idamp=2)
+!   González-Bolívar et al. (2022) MNRAS 517, 3181 (making idamp=2 obsolete)
 !
 ! :Owner: Thomas Reichardt
 !
@@ -27,7 +30,8 @@ module damping
 
  private
 
- integer, public :: idamp    = 0
+ integer, public    :: idamp     = 0
+ integer, parameter :: idamp_max = 3  ! maximum allowed value of idamp
  real, public :: tdyn_s      = 0.
  real, public :: damp        = 0.
  real, public :: r1in = 0.3
@@ -43,14 +47,15 @@ contains
 !+
 !-----------------------------------------------------------------------
 subroutine calc_damp(time, damp_fac)
- use units, only:utime
+ use units,   only:utime
+ use physcon, only:pi
  real, intent(out)   :: damp_fac
  real, intent(in)    :: time
  real                :: tau1, tau2, tdyn_star
 
  select case(idamp)
- case(1,3)
-    damp_fac = damp
+ case(3)
+    damp_fac = 1./(damp*2.*pi*sqrt(r1in**3)) ! fraction of orbital time at r=r1in with G=M=1
  case(2)
     tdyn_star = tdyn_s / utime
     tau1 = tdyn_star * 0.1
@@ -62,21 +67,68 @@ subroutine calc_damp(time, damp_fac)
     else
        damp_fac = tau1**(-1)
     endif
+ case(1)
+    damp_fac = damp  ! fraction per timestep
  case default
     damp_fac = 0.
  end select
 
 end subroutine calc_damp
 
-subroutine apply_damp(fextx, fexty, fextz, vxyz, damp_fac)
+!-----------------------------------------------------------------------
+!+
+!  apply damping term to velocity evolution (accelerations)
+!+
+!-----------------------------------------------------------------------
+subroutine apply_damp(fextx, fexty, fextz, vxyz, xyz, damp_fac)
  real, intent(inout) :: fextx, fexty, fextz
- real, intent(in)    :: vxyz(3), damp_fac
+ real, intent(in)    :: vxyz(3), xyz(3), damp_fac
+ real :: v0(3),fac
 
- fextx = fextx - damp_fac*vxyz(1)
- fexty = fexty - damp_fac*vxyz(2)
- fextz = fextz - damp_fac*vxyz(3)
+ v0 = 0.
+ fac = 1.
+ !
+ ! for idamp=3 damping is only applied in the boundary zones,
+ ! hence damping factor depends on spatial location
+ ! also in this case we relax to a prescribed velocity, not zero
+ !
+ if (idamp==3) fac = get_damp_fac_disc(xyz,v0)
+
+ fextx = fextx - damp_fac*(vxyz(1)-v0(1))*fac
+ fexty = fexty - damp_fac*(vxyz(2)-v0(2))*fac
+ fextz = fextz - damp_fac*(vxyz(3)-v0(3))*fac
 
 end subroutine apply_damp
+
+!-----------------------------------------------------------------------
+!+
+!  radial damping zones for inner and outer boundary of a disc
+!+
+!-----------------------------------------------------------------------
+real function get_damp_fac_disc(xyz,v0) result(fac)
+ use physcon, only:pi
+ real, intent(in) :: xyz(3)
+ real, intent(out) :: v0(3)
+ real :: rcyl,omega,vphi
+
+ rcyl = sqrt(xyz(1)**2 + xyz(2)**2)
+
+ if (rcyl < r2in) then
+    fac = 1. - (sin(0.5*pi*(rcyl - r1in)/(r2in - r1in)))**2
+ elseif (rcyl > r1out) then
+    fac = (sin(0.5*pi*(rcyl - r1out)/(r2out - r1out)))**2*sqrt((r1in/r2out)**3)
+ else
+    fac = 0.
+ endif
+
+ omega = sqrt(1./rcyl**3)
+ vphi = rcyl*omega
+
+ v0(1) = -vphi*xyz(2)/rcyl   ! sin(phi) = y/R
+ v0(2) =  vphi*xyz(1)/rcyl   ! cos(phi) = x/R
+ v0(3) = 0.
+
+end function get_damp_fac_disc
 
 !-----------------------------------------------------------------------
 !+
@@ -88,18 +140,22 @@ subroutine write_options_damping(iunit)
  integer, intent(in) :: iunit
 
  write(iunit,"(/,a)") '# options controlling damping'
- call write_inopt(idamp,'idamp','artificial damping of velocities (0=off, 1=constant, 2=star)',iunit)
+ call write_inopt(idamp,'idamp','artificial damping of velocities (0=off, 1=constant, 2=star, 3=disc)',iunit)
 
  select case(idamp)
- case(1,3)
-    call write_inopt(damp,'damp','artificial damping of velocities (if on, v=0 initially)',iunit)
+ case(1)
+    call write_inopt(damp,'damp','fraction to damp velocity each timestep (if > 0, v=0 initially)',iunit)
  case(2)
     call write_inopt(tdyn_s,'tdyn_s','dynamical timescale of star in seconds - damping is dependent on it',iunit)
+ case(3)
+    call write_inopt(damp,'damp','damping timescale as fraction of orbital timescale',iunit)
+    call write_inopt(r1in,'r1in','inner boundary of inner disc damping zone',iunit)
+    call write_inopt(r2in,'r2in','outer boundary of inner disc damping zone',iunit)
+    call write_inopt(r1out,'r1out','inner boundary of outer disc damping zone',iunit)
+    call write_inopt(r2out,'r2out','outer boundary of outer disc damping zone',iunit)
  end select
 
 end subroutine write_options_damping
-
-
 
 !-----------------------------------------------------------------------
 !+
@@ -107,8 +163,7 @@ end subroutine write_options_damping
 !+
 !-----------------------------------------------------------------------
 subroutine read_options_damping(name,valstring,imatch,igotall,ierr)
- use io,            only:fatal
- use eos_helmholtz, only:eos_helmholtz_set_relaxflag
+ use io, only:fatal
  character(len=*), intent(in)    :: name,valstring
  logical,          intent(out)   :: imatch,igotall
  integer,          intent(out)   :: ierr
@@ -120,7 +175,7 @@ subroutine read_options_damping(name,valstring,imatch,igotall,ierr)
  case('idamp')
     read(valstring,*,iostat=ierr) idamp
     ngot = ngot + 1
-    if (idamp < 0) call fatal(label,'damping form choice out of range')
+    if (idamp < 0 .or. idamp > idamp_max) call fatal(label,'damping form choice out of range')
  case('damp')
     read(valstring,*,iostat=ierr) damp
     if (damp < 0.)  call fatal(label,'damp <= 0: damping must be positive')
@@ -129,12 +184,30 @@ subroutine read_options_damping(name,valstring,imatch,igotall,ierr)
     read(valstring,*,iostat=ierr) tdyn_s
     if (tdyn_s < 0.)  call fatal(label,'tdyn_s < 0: this makes no sense')
     ngot = ngot + 1
+ case('r1in')
+    read(valstring,*,iostat=ierr) r1in
+    if (r1in <= 0.)  call fatal(label,'r1in <= 0: this makes no sense')
+    ngot = ngot + 1
+ case('r2in')
+    read(valstring,*,iostat=ierr) r2in
+    if (r2out < r1in)  call fatal(label,'r2in < r1in: this makes no sense')
+    ngot = ngot + 1
+ case('r1out')
+    read(valstring,*,iostat=ierr) r1out
+    if (r1out < r2in)  call fatal(label,'r1out < r2in: this makes no sense')
+    ngot = ngot + 1
+ case('r2out')
+    read(valstring,*,iostat=ierr) r2out
+    if (r2out < r1out)  call fatal(label,'r2out < r1out: this makes no sense')
+    ngot = ngot + 1
  case default
     imatch = .false.
  end select
 
  !--make sure we have got all compulsory options (otherwise, rewrite input file)
- if (idamp > 0) then
+ if (idamp==3) then
+    igotall = (ngot >= 6)
+ elseif (idamp > 0) then
     igotall = (ngot >= 2)
  else
     igotall = (ngot >= 1)
