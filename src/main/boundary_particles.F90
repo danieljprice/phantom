9c70908e92112e398c01f4dd8ef61cdad21cb0aa
Mike Lau <mike.lau@monash.edu>
2023-03-25 17:58:29 +1100

    (boundary-part) only calculate boundary particle forces if there are boundary particles

--- src/main/boundary_particles.F90
+++ src/main/boundary_particles.F90
@@ -55,22 +55,24 @@ subroutine get_boundary_particle_forces(npart,iphase,fxyz,dBevol,drad,ddustprop,
     endif
  enddo
  !$omp end parallel do
- avg_accel = avg_accel / nboundary
 
- !$omp parallel do default(none) &
- !$omp shared(npart,iphase,fxyz,avg_accel,dBevol,drad,ddustevol,ddustprop) &
- !$omp private(i)
- do i=1,npart
-    if (iamboundary(iamtype(iphase(i)))) then
-       fxyz(1:3,i) = avg_accel
-       if (maxvxyzu==4) fxyz(4,i) = 0. 
-       if (mhd) dBevol(:,i) = 0.
-       if (do_radiation) drad(iradxi,i) = 0.
-       if (use_dust) ddustevol(:,i) = 0.
-       if (use_dustgrowth) ddustprop(1,i) = 0.
-    endif
- enddo
- !$omp end parallel do
+ if (nboundary > 0) then
+    avg_accel = avg_accel / nboundary
+    !$omp parallel do default(none) &
+    !$omp shared(npart,iphase,fxyz,avg_accel,dBevol,drad,ddustevol,ddustprop) &
+    !$omp private(i)
+    do i=1,npart
+       if (iamboundary(iamtype(iphase(i)))) then
+          fxyz(1:3,i) = avg_accel
+          if (maxvxyzu==4) fxyz(4,i) = 0.
+          if (mhd) dBevol(:,i) = 0.
+          if (do_radiation) drad(iradxi,i) = 0.
+          if (use_dust) ddustevol(:,i) = 0.
+          if (use_dustgrowth) ddustprop(1,i) = 0.
+       endif
+    enddo
+    !$omp end parallel do
+ endif
 
 end subroutine get_boundary_particle_forces
 
@@ -107,35 +109,37 @@ subroutine average_boundary_particle_rotation(npart,iphase,fxyz,xyz)
     endif
  enddo
 !$omp end parallel do
- xyz_core_CM = xyz_core_CM / nboundary
 
+ if (nboundary > 0) then
+    xyz_core_CM = xyz_core_CM / nboundary
 
- ! Calculate average torque w.r.t. centre of mass
- avg_torque = 0.
- !$omp parallel do default(none) &
- !$omp shared(npart,iphase,fxyz,xyz_core_CM,xyz) &
- !$omp private(i,torquei) &
- !$omp reduction(+:avg_torque)
- do i=1,npart
-    if (iamboundary(iamtype(iphase(i)))) then
-       call cross_product3D(xyz(1:3,i) - xyz_core_CM, fxyz(1:3,i), torquei)
-       avg_torque = avg_torque + torquei
-    endif
- enddo
- !$omp end parallel do
- avg_torque = avg_torque / nboundary
+    ! Calculate average torque w.r.t. centre of mass
+    avg_torque = 0.
+    !$omp parallel do default(none) &
+    !$omp shared(npart,iphase,fxyz,xyz_core_CM,xyz) &
+    !$omp private(i,torquei) &
+    !$omp reduction(+:avg_torque)
+    do i=1,npart
+       if (iamboundary(iamtype(iphase(i)))) then
+          call cross_product3D(xyz(1:3,i) - xyz_core_CM, fxyz(1:3,i), torquei)
+          avg_torque = avg_torque + torquei
+       endif
+    enddo
+    !$omp end parallel do
+    avg_torque = avg_torque / nboundary
 
- !$omp parallel do default(none) &
- !$omp shared(npart,iphase,xyz,fxyz,avg_torque,xyz_core_CM) &
- !$omp private(i,accel_i,sep2)
- do i=1,npart
-    if (iamboundary(iamtype(iphase(i)))) then
-       sep2 = dot_product(xyz(1:3,i)-xyz_core_CM, xyz(1:3,i)-xyz_core_CM)
-       call cross_product3D(avg_torque / sep2, xyz(1:3,i)-xyz_core_CM, accel_i)
-       fxyz(1:3,i) = fxyz(1:3,i) + accel_i
-    endif
- enddo
- !$omp end parallel do
+    !$omp parallel do default(none) &
+    !$omp shared(npart,iphase,xyz,fxyz,avg_torque,xyz_core_CM) &
+    !$omp private(i,accel_i,sep2)
+    do i=1,npart
+       if (iamboundary(iamtype(iphase(i)))) then
+          sep2 = dot_product(xyz(1:3,i)-xyz_core_CM, xyz(1:3,i)-xyz_core_CM)
+          call cross_product3D(avg_torque / sep2, xyz(1:3,i)-xyz_core_CM, accel_i)
+          fxyz(1:3,i) = fxyz(1:3,i) + accel_i
+       endif
+    enddo
+    !$omp end parallel do
+ endif
 
 end subroutine average_boundary_particle_rotation
 
