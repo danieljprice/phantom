b59f99c1fd3219910fd63f92a7ba9172c67e0066
Daniel Price <daniel.price@monash.edu>
2024-03-05 08:26:57 +1100

    (growth2multi) minor cleanups in moddump_growth2multi

--- src/utils/moddump_growthtomultigrain.f90
+++ src/utils/moddump_growthtomultigrain.f90
@@ -26,25 +26,24 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
  use prompting,      only:prompt
  use timestep,       only:nmax
  use growth,         only:bin_to_multi
+ use io,             only:fatal
  integer, intent(inout) :: npart
  integer, intent(inout) :: npartoftype(:)
  real,    intent(inout) :: massoftype(:)
  real,    intent(inout) :: xyzh(:,:),vxyzu(:,:)
- integer                :: bins_per_dex,iforce_smax
+ integer                :: bins_per_dex,iu
  real                   :: smax_user
  logical                :: force_smax,file_exists
  character(len=20)      :: infile  = "bin_param.txt"
 
  if ((.not. use_dust) .or. (.not. use_dustgrowth)) then
-    print*,' DOING NOTHING: COMPILE WITH DUST=yes AND DUSTGROWTH=yes'
-    stop
+    call fatal('moddump_growth2multi','Need to compile with DUST=yes and DUSTGROWTH=yes')
  endif
 
  !- initialise variables
  bins_per_dex = 5
- iforce_smax  = 2
  force_smax   = .false.
- smax_user    = 2
+ smax_user    = 2.
  nmax         = 0 !- deriv called once after moddump
 
  !- check if param file exists, created by python script growthtomcfost.py
@@ -52,19 +51,18 @@ subroutine modify_dump(npart,npartoftype,massoftype,xyzh,vxyzu)
 
  !- if not, switch to interactive method
  if (.not.file_exists) then
-    call prompt('Set smax manually? (1=yes, 2=no)',iforce_smax,1,2)
-    if (iforce_smax == 1) then
-       force_smax = .true.
-    else
-       force_smax = .false.
-    endif
+    call prompt('Set smax manually?',force_smax)
     if (force_smax) call prompt('Enter smax in cm',smax_user,0.05)
     call prompt('Enter number of bins per dex',bins_per_dex,1)
+    ! write the input file so don't have to ask questions again
+    open(newunit=iu,file=infile,status='new',action='write')
+    write(iu,*) force_smax,smax_user,bins_per_dex
+    close(iu)
  else
     !- file created by phantom/scripts/growthtomcfost.py module
-    open (unit=420, file=infile)
-    read(420,*) force_smax, smax_user, bins_per_dex
-    close(unit=420)
+    open(newunit=iu, file=infile,status='old',action='read')
+    read(iu,*) force_smax,smax_user,bins_per_dex
+    close(iu)
  endif
 
  !- delete dead or accreted particles before doing anything
