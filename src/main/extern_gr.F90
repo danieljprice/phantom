b5d18c7c0c25c986baa9278fdffdb12bba89fd5b
Daniel Price <daniel.price@monash.edu>
2024-04-09 09:52:55 +1000

    (gr) cleanup extern_gr

--- src/main/extern_gr.F90
+++ src/main/extern_gr.F90
@@ -6,9 +6,12 @@
 !--------------------------------------------------------------------------!
 module extern_gr
 !
-! None
+! Compute terms related to derivatives of the metric which appear
+! on the right hand side of the momentum equation
 !
-! :References: None
+! :References:
+!   Liptai & Price (2019), MNRAS 485, 819
+!   Magnall, Price, Lasky & Macpherson (2023), Phys. Rev D. 108, 103534
 !
 ! :Owner: Spencer Magnall
 !
@@ -49,6 +52,12 @@ subroutine get_grforce(xyzhi,metrici,metricderivsi,veli,densi,ui,pi,fexti,dtf)
 
 end subroutine get_grforce
 
+!---------------------------------------------------------------
+!+
+!  Wrapper of the above, computing accelerations due to metric
+!  gradients on all particles
+!+
+!---------------------------------------------------------------
 subroutine get_grforce_all(npart,xyzh,metrics,metricderivs,vxyzu,dens,fext,dtexternal)
  use timestep, only:C_force
  use eos,      only:ieos,get_pressure
@@ -77,8 +86,12 @@ subroutine get_grforce_all(npart,xyzh,metrics,metricderivs,vxyzu,dens,fext,dtext
 
 end subroutine get_grforce_all
 
-!--- Subroutine to calculate the timestep constraint from the 'external force'
-!    this is multiplied by the safety factor C_force elsewhere
+!---------------------------------------------------------------------------
+!+
+!  Subroutine to calculate the timestep constraint from the 'external force'
+!  this is multiplied by the safety factor C_force elsewhere
+!+
+!---------------------------------------------------------------------------
 subroutine dt_grforce(xyzh,fext,dtf)
  use physcon,      only:pi
  use metric_tools, only:imetric,imet_schwarzschild,imet_kerr
@@ -106,7 +119,6 @@ subroutine dt_grforce(xyzh,fext,dtf)
  dtf = min(dtf1,dtf2)
 
 end subroutine dt_grforce
-
 
 !----------------------------------------------------------------
 !+
@@ -139,19 +151,18 @@ pure subroutine forcegr(x,metrici,metricderivsi,v,dens,u,p,fterm,ierr)
 
  ! energy-momentum tensor times sqrtg on 2rho*
  do i=0,3
-    term(0:3,i) =  0.5*(enth*uzero*v4(0:3)*v4(i) + P*gcon(0:3,i)/(dens*uzero))
+    term(0:3,i) = 0.5*(enth*uzero*v4(0:3)*v4(i) + P*gcon(0:3,i)/(dens*uzero))
  enddo
 
  ! source term
  fterm = 0.
  do i=0,3
-    fterm(1) =  fterm(1) + dot_product(term(:,i),metricderivsi(:,i,1))
-    fterm(2) =  fterm(2) + dot_product(term(:,i),metricderivsi(:,i,2))
-    fterm(3) =  fterm(3) + dot_product(term(:,i),metricderivsi(:,i,3))
+    fterm(1) = fterm(1) + dot_product(term(:,i),metricderivsi(:,i,1))
+    fterm(2) = fterm(2) + dot_product(term(:,i),metricderivsi(:,i,2))
+    fterm(3) = fterm(3) + dot_product(term(:,i),metricderivsi(:,i,3))
  enddo
 
 end subroutine forcegr
-
 
 !-------- I don't think this is actually being used at the moment....
 subroutine update_grforce_leapfrog(vhalfx,vhalfy,vhalfz,fxi,fyi,fzi,fexti,dt,xi,yi,zi,densi,ui,pi)
@@ -220,82 +231,45 @@ subroutine update_grforce_leapfrog(vhalfx,vhalfy,vhalfz,fxi,fyi,fzi,fexti,dt,xi,
 
 end subroutine update_grforce_leapfrog
 
+!----------------------------------------------------------------
+!+
+!  compute stress energy tensor on all particles
+!+
+!----------------------------------------------------------------
 subroutine get_tmunu_all(npart,xyzh,metrics,vxyzu,metricderivs,dens,tmunus)
- use eos,         only:ieos,get_pressure
- use part,        only:isdead_or_accreted
+ use eos,  only:ieos,get_pressure
+ use part, only:isdead_or_accreted
  integer, intent(in) :: npart
  real, intent(in)    :: xyzh(:,:), metrics(:,:,:,:), metricderivs(:,:,:,:), dens(:)
  real, intent(inout) :: vxyzu(:,:),tmunus(:,:,:)
  real                :: pi
  integer             :: i
- logical             :: verbose
 
- verbose = .false.
- ! TODO write openmp parallel code
  !$omp parallel do default(none) &
  !$omp shared(npart,xyzh,metrics,vxyzu,dens,ieos,tmunus) &
- !$omp private(i,pi,verbose)
- do i=1, npart
-    !print*, "i: ", i
-    if (i==1) then
-       verbose = .true.
-    else
-       verbose = .false.
-    endif
+ !$omp private(i,pi)
+ do i=1,npart
     if (.not.isdead_or_accreted(xyzh(4,i))) then
        pi = get_pressure(ieos,xyzh(:,i),dens(i),vxyzu(:,i))
-       call get_tmunu(xyzh(:,i),metrics(:,:,:,i),&
-         vxyzu(1:3,i),dens(i),vxyzu(4,i),pi,tmunus(:,:,i),verbose)
+       call get_tmunu(xyzh(:,i),metrics(:,:,:,i),vxyzu(1:3,i),&
+                      dens(i),vxyzu(4,i),pi,tmunus(:,:,i))
     endif
  enddo
  !$omp end parallel do
- !print*, "tmunu calc val is: ", tmunus(0,0,5)
+
 end subroutine get_tmunu_all
 
-subroutine get_tmunu_all_exact(npart,xyzh,metrics,vxyzu,metricderivs,dens,tmunus)
- use eos,         only:ieos,get_pressure
- use part,        only:isdead_or_accreted
- integer, intent(in) :: npart
- real, intent(in)    :: xyzh(:,:), metrics(:,:,:,:), metricderivs(:,:,:,:), dens(:)
- real, intent(inout) :: vxyzu(:,:),tmunus(:,:,:)
- real                :: pi
- integer             :: i
- logical             :: firstpart
- real                :: tmunu(4,4)
- !print*, "entered get tmunu_all_exact"
- tmunu = 0.
- firstpart = .true.
- ! TODO write openmp parallel code
- do i=1, npart
-    if (.not.isdead_or_accreted(xyzh(4,i)) .and. firstpart) then
-       pi = get_pressure(ieos,xyzh(:,i),dens(i),vxyzu(:,i))
-       call get_tmunu_exact(xyzh(:,i),metrics(:,:,:,i), metricderivs(:,:,:,i), &
-         vxyzu(1:3,i),dens(i),vxyzu(4,i),pi,tmunus(:,:,i))
-       !print*, "finished get_tmunu call!"
-       firstpart = .false.
-       !print*, "tmunu: ", tmunu
-       !print*, "tmunus: ", tmunus(:,:,i)
-       tmunu(:,:) = tmunus(:,:,i)
-       !print*, "Got tmunu val: ", tmunu
-       !stop
-    else
-       !print*, "setting tmunu for part: ", i
-       tmunus(:,:,i) = tmunu(:,:)
-    endif
-
- enddo
- !print*, "tmunu calc val is: ", tmunus(0,0,5)
-end subroutine get_tmunu_all_exact
-
-
-! Subroutine to calculate the covariant form of the stress energy tensor
-! For a particle at position p
-subroutine get_tmunu(x,metrici,v,dens,u,p,tmunu,verbose)
- use metric_tools,     only:unpack_metric
+!-------------------------------------------------------------------------
+!+
+!  calculate the covariant form of the stress energy tensor
+!  for a particle at position x
+!+
+!-------------------------------------------------------------------------
+subroutine get_tmunu(x,metrici,v,dens,u,p,tmunu)
+ use metric_tools, only:unpack_metric
  use utils_gr,     only:get_u0
  real,    intent(in)  :: x(3),metrici(:,:,:),v(3),dens,u,p
  real,    intent(out) :: tmunu(0:3,0:3)
- logical, optional, intent(in) :: verbose
  real                 :: w,v4(0:3),uzero,u_upper(0:3),u_lower(0:3)
  real                 :: gcov(0:3,0:3), gcon(0:3,0:3)
  real                 :: gammaijdown(1:3,1:3),betadown(3),alpha
@@ -318,42 +292,7 @@ subroutine get_tmunu(x,metrici,v,dens,u,p,tmunu,verbose)
 
  ! Get cov and con versions of the metric + spatial metric and lapse and shift
  ! Not entirely convinced that the lapse and shift calculations are acccurate for the general case!!
- !print*, "Before unpack metric "
  call unpack_metric(metrici,gcov=gcov,gcon=gcon,gammaijdown=gammaijdown,alpha=alpha,betadown=betadown)
- !print*, "After unpack metric"
-
-!  if (present(verbose) .and. verbose) then
-!     ! Do we get sensible values
-!     print*, "Unpacked metric quantities..."
-!     print*, "gcov: ", gcov
-!     print*, "gcon: ", gcon
-!     print*, "gammaijdown: ", gammaijdown
-!     print* , "alpha: ", alpha
-!     print*, "betadown: ", betadown
-!     print*, "v4: ", v4
-!  endif
-
- ! ! Need to change Betadown to betaup
- ! ! Won't matter at this point as it is allways zero
- ! ! get big V
- ! bigV(:) = (v(:) + betadown)/alpha
-
- ! ! We need the covariant version of the 3 velocity
- ! ! gamma_ij v^j = v_i where gamma_ij is the spatial metric
- ! do i=1, 3
- !    vcov(i) = gammaijdown(i,1)*bigv(1) + gammaijdown(i,2)*bigv(2) + gammaijdown(i,3)*bigv(3)
- ! enddo
-
-
- ! ! Calculate the lorentz factor
- ! lorentz = (1. -  (vcov(1)*bigv(1) + vcov(2)*bigv(2) + vcov(3)*bigv(3)))**(-0.5)
-
- ! ! Calculate the 4-velocity
- ! velshiftterm = vcov(1)*betadown(1) + vcov(2)*betadown(2) + vcov(3)*betadown(3)
- ! v4(0) = lorentz*(-alpha + velshiftterm)
- ! ! This should be vcov not v
- ! v4(1:3) = lorentz*vcov(1:3)
-
 
  ! We are going to use the same Tmunu calc as force GR
  ! And then lower it using the metric
@@ -362,7 +301,6 @@ subroutine get_tmunu(x,metrici,v,dens,u,p,tmunu,verbose)
  ! lower-case 4-velocity (contravariant)
  v4(0) = 1.
  v4(1:3) = v(:)
-
 
  ! first component of the upper-case 4-velocity (contravariant)
  call get_u0(gcov,v,uzero,ierr)
@@ -380,34 +318,14 @@ subroutine get_tmunu(x,metrici,v,dens,u,p,tmunu,verbose)
     enddo
  enddo
 
-
-!  if (present(verbose) .and. verbose) then
-!     ! Do we get sensible values
-!     print*, "Unpacked metric quantities..."
-!     print*, "gcov: ", gcov
-!     print*, "gcon: ", gcon
-!     print*, "gammaijdown: ", gammaijdown
-!     print* , "alpha: ", alpha
-!     print*, "betadown: ", betadown
-!     print*, "v4: ", v4
-!  endif
-
-!  if (verbose) then
-!     print*, "tmunu part: ", tmunu
-!     print*, "dens: ", dens
-!     print*, "w: ", w
-!     print*, "p: ", p
-!     print*, "gcov: ", gcov
-!  endif
-
- ! print*, "tmunu part: ", tmunu
- ! print*, "dens: ", dens
- ! print*, "w: ", w
- ! print*, "p: ", p
- ! print*, "gcov: ", gcov
- ! stop
 end subroutine get_tmunu
 
+!-------------------------------------------------------------------------
+!+
+!  the following two routines are for testing purposes
+!  and could be deleted at some stage (as used in Magnall et al. 2023)
+!+
+!-------------------------------------------------------------------------
 subroutine get_tmunu_exact(x,metrici,metricderivsi,v,dens,u,p,tmunu)
  use metric_tools,     only:unpack_metric
  use utils_gr,         only:get_sqrtg
@@ -443,9 +361,9 @@ subroutine get_tmunu_exact(x,metrici,metricderivsi,v,dens,u,p,tmunu)
 
  rhostar = 13.294563008157013D0
  call get_sqrtg(gcov,negsqrtg)
+
  ! Set/Calculate primitive density using rhostar exactly
  rhoprim = rhostar/(negsqrtg/alpha)
-
 
  ! Stress energy tensor
  do j=0,3
@@ -454,8 +372,40 @@ subroutine get_tmunu_exact(x,metrici,metricderivsi,v,dens,u,p,tmunu)
     enddo
  enddo
 
-
-
 end subroutine get_tmunu_exact
+
+!-------------------------------------------------------------------------
+!+
+!  see above: for testing purposes and could be deleted at some stage
+!  (as used in Magnall et al. 2023)
+!+
+!-------------------------------------------------------------------------
+subroutine get_tmunu_all_exact(npart,xyzh,metrics,vxyzu,metricderivs,dens,tmunus)
+ use eos,         only:ieos,get_pressure
+ use part,        only:isdead_or_accreted
+ integer, intent(in) :: npart
+ real, intent(in)    :: xyzh(:,:), metrics(:,:,:,:), metricderivs(:,:,:,:), dens(:)
+ real, intent(inout) :: vxyzu(:,:),tmunus(:,:,:)
+ real                :: pi
+ integer             :: i
+ logical             :: firstpart
+ real                :: tmunu(4,4)
+
+ tmunu = 0.
+ firstpart = .true.
+ ! TODO write openmp parallel code
+ do i=1, npart
+    if (.not.isdead_or_accreted(xyzh(4,i)) .and. firstpart) then
+       pi = get_pressure(ieos,xyzh(:,i),dens(i),vxyzu(:,i))
+       call get_tmunu_exact(xyzh(:,i),metrics(:,:,:,i), metricderivs(:,:,:,i), &
+         vxyzu(1:3,i),dens(i),vxyzu(4,i),pi,tmunus(:,:,i))
+       firstpart = .false.
+       tmunu(:,:) = tmunus(:,:,i)
+    else
+       tmunus(:,:,i) = tmunu(:,:)
+    endif
+ enddo
+
+end subroutine get_tmunu_all_exact
 
 end module extern_gr
