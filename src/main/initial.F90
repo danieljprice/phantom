697dca8325ba183e545a9d7459d855b30f90edd6
Siméon <simeon.deschaux@gmail.com>
2025-02-15 18:30:28 +0100

    wind_1D.dat name fix

--- src/main/initial.F90
+++ src/main/initial.F90
@@ -587,21 +587,23 @@ subroutine startrun(infile,logfile,evfile,dumpfile,noread)
 #ifdef INJECT_PARTICLES
  call init_inject(ierr)
  if (ierr /= 0) call fatal('initial','error initialising particle injection')
- npart_old = npart
  do j = 1,nptmass
-    !rename wind profile filename
+    npart_old = npart
+    ! rename wind profile filename
     write(filename, '(A,I1,A)') 'wind', j, '_profile1D.dat'
     inquire(file=filename, exist=iexist)
     if (iexist) then
-        i = len(trim(dumpfile))
-        if (dumpfile(i-2:i) == 'tmp') then
-            file1D = dumpfile(1:i-9) // '1D' // char(48+j) // '.dat'
-        else
-            file1D = dumpfile(1:i-5) // '1D' // char(48+j) // '.dat'
-        endif
-        call rename(filename, trim(file1D))
+       i = len(trim(dumpfile))
+       if (dumpfile(i-2:i) == 'tmp') then
+          if (j==1) file1D = dumpfile(1:i-9) // '1D.dat'
+          if (j==2) file1D = dumpfile(1:i-9) // '1D' // char(48+j) // '.dat'
+       else
+          if (j==1) file1D = dumpfile(1:i-5) // '1D.dat'
+          if (j==2) file1D = dumpfile(1:i-5) // '1D' // char(48+j) // '.dat'
+       endif
+       call rename(filename, trim(file1D))
     endif
-    if (xyzmh_ptmass(imloss,j) > 0.) then
+    if (xyzmh_ptmass(imloss,j) > 0.) then 
        call inject_particles(time,0.,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,&
             npart,npart_old,npartoftype,dtinject,j)
        call update_injected_particles(npart_old,npart,istepfrac,nbinmax,time,dtmax,dt,dtinject)
