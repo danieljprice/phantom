0cfe6b27d387eea5b257aea0c0f03d72459a8ec6
Mike Lau <mike.lau@monash.edu>
2025-04-14 23:01:23 +0200

    (masstransfer) delete hold star functionality

--- src/main/inject_masstransfer.f90
+++ src/main/inject_masstransfer.f90
@@ -15,9 +15,7 @@ module inject
 !
 ! :Runtime parameters:
 !   - BHL_radius       : *radius of the wind cylinder (in code units)*
-!   - Rstar            : *radius of sphere where velocities are adjusted (code units)*
 !   - handled_layers   : *(integer) number of handled BHL wind layers*
-!   - hold_star        : *1: subtract CM velocity of star particles at each timestep*
 !   - lattice_type     : *0: cubic distribution, 1: closepacked distribution*
 !   - mach             : *mach number of injected particles*
 !   - rho_inf          : *ambient density (code units)*
@@ -43,15 +41,10 @@ module inject
 
  ! Particle-related parameters
  integer, public :: lattice_type = 0
- integer, public :: handled_layers = 1
+ integer, public :: handled_layers = 4
  real,    public :: wind_radius = 30.
  real,    public :: wind_injection_x = -10.
  real,    public :: wind_length = 100.
-
- ! option to fix the motion of a sphere of particles
- integer, public :: hold_star = 0
- real,    public :: Rstar = .1
- integer, public :: nstar  = 0
 
  !file with mesa values
  character(len=120), public :: filemesa='./test_data.txt'
@@ -99,7 +92,7 @@ subroutine init_inject(ierr)
       time_between_layers,nodd,neven,layer_even,layer_odd,distance_between_layers)
 
  call print_summary(v_inf,cs_inf,rho_inf,pres_inf,mach,pmass,distance_between_layers,&
-                    time_between_layers,imax_layers,nstar,imax_particles)
+                    time_between_layers,imax_layers,imax_particles)
 
 end subroutine init_inject
 
@@ -131,10 +124,10 @@ subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,&
  real :: pmass,cs_inf,pres_inf,kill_sep,distance_between_layers!,time_between_layers_last
  real, allocatable :: layer_even(:,:),layer_odd(:,:)
 
-   if (first_run) then
-      call init_inject(ierr)
-      first_run = .false.
-   endif
+ if (first_run) then
+    call init_inject(ierr)
+    first_run = .false.
+ endif
 
  !Interpolation of the mass transfer rate from the mesa file
  call find_nearest_index(time_mesa,time,time_index)
@@ -215,8 +208,6 @@ subroutine inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,&
 
  irrational_number_close_to_one = 3./pi
  dtinject = (irrational_number_close_to_one*time_between_layers)
-
- if (hold_star > 0) call subtract_star_vcom(nstar,xyzh,vxyzu)
 
 end subroutine inject_particles
 
@@ -394,8 +385,8 @@ subroutine calculate_lattice(ilattice,rho,pmass,radius,imax_layers,imax_particle
  time_between_layers = distance_between_layers/v_inf
 
 !these variables are not needed anymore
- imax_particles = int(imax_layers*(nodd+neven)/2) + nstar
- imax_layers = int(wind_length/distance_between_layers)
+!  imax_particles = int(imax_layers*(nodd+neven)/2) + nstar
+!  imax_layers = int(wind_length/distance_between_layers)
 
 end subroutine calculate_lattice
 
@@ -419,37 +410,6 @@ subroutine delete_particles_inside_sphere(center,radius,xyzi,hi)
 
 end subroutine delete_particles_inside_sphere
 
-!-----------------------------------------------------------------------
-!+
-!  Subtracts centre-of-mass motion of star particles
-!  Assumes star particles have particle IDs 1 to nsphere
-!+
-!-----------------------------------------------------------------------
-subroutine subtract_star_vcom(nsphere,xyzh,vxyzu)
- integer, intent(in) :: nsphere
- real, intent(in)    :: xyzh(:,:)
- real, intent(inout) :: vxyzu(:,:)
- real                :: vstar(3)
- integer             :: i,nbulk
-
-!  vstar = (/ sum(vxyzu(1,1:nsphere)), sum(vxyzu(2,1:nsphere)), sum(vxyzu(3,1:nsphere)) /) / real(nsphere)
- nbulk = 0
- vstar = 0.
- do i=1,nsphere
-    if (xyzh(1,i) < 2.*Rstar) then
-       vstar = vstar + vxyzu(1:3,i)
-       nbulk = nbulk + 1
-    endif
- enddo
- vstar = vstar/real(nbulk)
-
- do i=1,nsphere
-    if (xyzh(1,i) < 2.*Rstar) then
-       vxyzu(1:3,i) = vxyzu(1:3,i) - vstar
-    endif
- enddo
-
-end subroutine subtract_star_vcom
 
 !-----------------------------------------------------------------------
 !+
@@ -457,10 +417,10 @@ end subroutine subtract_star_vcom
 !+
 !-----------------------------------------------------------------------
 subroutine print_summary(v_inf,cs_inf,rho_inf,pres_inf,mach,pmass,distance_between_layers,&
-                         time_between_layers,imax_layers,nstar,imax_particles)
+                         time_between_layers,imax_layers,imax_particles)
  use units, only:unit_velocity,unit_pressure,unit_density
  real, intent(in)    :: v_inf,cs_inf,rho_inf,pres_inf,mach,pmass,distance_between_layers,time_between_layers
- integer, intent(in) :: imax_layers,nstar,imax_particles
+ integer, intent(in) :: imax_layers,imax_particles
 
  print*, 'wind speed: ',v_inf * unit_velocity / 1e5," km s^-1"
  print*, 'wind cs: ',cs_inf * unit_velocity / 1e5," km s^-1"
@@ -472,13 +432,7 @@ subroutine print_summary(v_inf,cs_inf,rho_inf,pres_inf,mach,pmass,distance_betwe
  print*, 'pmass: ',pmass
  print*, 'distance_between_layers: ',distance_between_layers
  print*, 'time_between_layers: ',time_between_layers
-
- if (hold_star > 0) then
-    print*, 'planet crossing time: ',2*Rstar/v_inf
-    print*, 'wind impact time: ',(abs(wind_injection_x) - Rstar)/v_inf
-    print*, 'nstar: ',nstar
- endif
- print*, 'nstar + max. wind particles: ', imax_particles
+ print*, 'max. wind particles: ', imax_particles
 
 end subroutine print_summary
 
@@ -498,11 +452,6 @@ subroutine write_options_inject(iunit)
  call write_inopt(rho_inf,'rho_inf','ambient density (code units)',iunit)
  call write_inopt(lattice_type,'lattice_type','0: cubic distribution, 1: closepacked distribution',iunit)
  call write_inopt(handled_layers,'handled_layers','(integer) number of handled BHL wind layers',iunit)
- call write_inopt(hold_star,'hold_star','1: subtract CM velocity of star particles at each timestep',iunit)
- if (hold_star > 0) then
-    call write_inopt(Rstar,'Rstar','radius of sphere where velocities are adjusted (code units)',iunit)
-    call write_inopt(nstar,'nstar','No. of particles that should have their velocity adjusted',iunit)  ! need to write actual no. of particles, not nstar_in
- endif
  call write_inopt(wind_radius,'BHL_radius','radius of the wind cylinder (in code units)',iunit)
  call write_inopt(wind_injection_x,'wind_injection_x','x position of the wind injection boundary (in code units)',iunit)
  call write_inopt(wind_length,'wind_length','crude wind length (in star radii)',iunit)
@@ -539,13 +488,6 @@ subroutine read_options_inject(name,valstring,imatch,igotall,ierr)
     read(valstring,*,iostat=ierr) rho_inf
     ngot = ngot + 1
     if (rho_inf <= 0.) call fatal(label,'rho_inf must be positive')
- case('nstar')
-    read(valstring,*,iostat=ierr) nstar
-    ngot = ngot + 1
- case('Rstar')
-    read(valstring,*,iostat=ierr) Rstar
-    ngot = ngot + 1
-    if (Rstar <= 0.)    call fatal(label,'invalid setting for Rstar (<=0)')
  case('lattice_type')
     read(valstring,*,iostat=ierr) lattice_type
     ngot = ngot + 1
@@ -557,15 +499,12 @@ subroutine read_options_inject(name,valstring,imatch,igotall,ierr)
  case('wind_injection_x')
     read(valstring,*,iostat=ierr) wind_injection_x
     ngot = ngot + 1
- case('hold_star')
-    read(valstring,*,iostat=ierr) hold_star
-    ngot = ngot + 1
 
 !LS the parameters below can be removed
  case('handled_layers')
     read(valstring,*,iostat=ierr) handled_layers
     ngot = ngot + 1
-    if (handled_layers < 0) call fatal(label,'handled_layers must be positive or zero')
+    if ((mod(handled_layers,2)> 0) .or. (handled_layers==0)) call fatal(label,'handled_layers must be non-zero even number')
  case('wind_length')
     read(valstring,*,iostat=ierr) wind_length
     ngot = ngot + 1
