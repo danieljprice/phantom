e0a87eef640fd9471003bef012d814197a43571e
Siméon <simeon.deschaux@gmail.com>
2025-05-03 23:28:45 +0200

    (cooling_functions.f90) HI cooling fix

--- src/main/cooling_functions.f90
+++ src/main/cooling_functions.f90
@@ -154,7 +154,7 @@ subroutine cooling_neutral_hydrogen(T, rho_cgs, Q_cgs, dlnQ_dlnT)
  real, parameter   :: f = 1.0d0
  real              :: ne,nH
 
- if (T > 3000.) then
+ if (T > 3000. .and. T < 1.2e+04) then
     nH = rho_cgs/(1.4*mass_proton_cgs)
     ne = min(calc_eps_e(T),1.)*nH
     !the term 1/(1+sqrt(T)) comes from Cen (1992, ApjS, 78, 341)
