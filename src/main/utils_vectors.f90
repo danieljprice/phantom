7a74bf98762b67eebfb42744b5a8dff0efbfdb46
joshcalcino <josh.calcino@gmail.com>
2025-06-17 18:56:19 +0400

    fix perp frame

--- src/main/utils_vectors.f90
+++ src/main/utils_vectors.f90
@@ -193,14 +193,14 @@ pure subroutine make_perp_frame(a, b, c)
    real, intent(in)  :: a(3)     ! arbitrary non-zero vector
    real, intent(out) :: b(3), c(3)
 
-   real :: inv_norm
+   real :: aa(3), inv_norm
 
    ! normalise a 
    inv_norm = 1.0 / sqrt(sum(a*a))
-   c        = a * inv_norm          ! temporarily store â in c (a is intent in so we can't modify it)
+   aa        = a * inv_norm          ! temporarily store â in c (a is intent in so we can't modify it)
 
    ! pick the largest component in magnitude
-   select case (maxloc(abs(c), dim=1))
+   select case (maxloc(abs(aa), dim=1))
    case (1)                          ! |a_x| is largest -> use y-axis
       b = (/ 0.0, 1.0, 0.0 /)
    case (2)                          ! |a_y| is largest -> use z-axis
@@ -210,12 +210,12 @@ pure subroutine make_perp_frame(a, b, c)
    end select
 
    ! make b perpendicular to a via Gram–Schmidt process (https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process)
-   b = b - dot_product(b, c) * c
+   b = b - dot_product(b, aa) * aa
    inv_norm = 1.0 / sqrt(sum(b*b))
    b = b * inv_norm
 
    ! c = a x b  
-   c = cross_product3D(c, b)
+   call cross_product3D(aa, b, c)
 end subroutine make_perp_frame
 
 end module vectorutils
