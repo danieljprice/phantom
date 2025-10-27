58afc27d928b85c8649638c65d4450724262f065
Daniel Price <daniel.price@monash.edu>
2025-10-27 11:42:46 +0100

    #754 bug fix with ntot argument also

--- src/main/memory.f90
+++ src/main/memory.f90
@@ -56,7 +56,7 @@ subroutine allocate_memory(ntot, part_only,reallocation)
 
  n = int(min(nprocs,4) * ntot / nprocs)
 
- call update_max_sizes(max(n,maxp),ntot)
+ call update_max_sizes(max(n,maxp),max(ntot,int(maxp,kind=8)))
  if (nbytes_allocated > 0.0 .and. (n <= maxp .and. .not.realloc_)) then
     !
     ! just silently skip if arrays are already large enough
