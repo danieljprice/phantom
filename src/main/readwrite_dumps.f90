0fae9386a94e3589292db4707ccbc3a196f2ca0e
Mike Lau <mike.lau@monash.edu>
2026-05-20 10:09:20 +0200

    (readwrite_dumps) remove ieos/=22 condition for writing gamma and mu

--- src/main/readwrite_dumps.f90
+++ src/main/readwrite_dumps.f90
@@ -239,10 +239,10 @@ subroutine write_fulldump(t,dumpfile,ntotal,iorder,sphNG)
           call write_array(1,eos_vars,eos_vars_label,1,npart,k,ipass,idump,nums,nerr,index=iX)
           call write_array(1,eos_vars,eos_vars_label,1,npart,k,ipass,idump,nums,nerr,index=iZ)
        endif
-       if (use_var_comp .or. eos_outputs_mu(ieos) .or. update_muGamma .or. (use_krome .and. (ieos/=22))) then
+       if (use_var_comp .or. eos_outputs_mu(ieos) .or. update_muGamma .or. use_krome) then
           call write_array(1,eos_vars,eos_vars_label,1,npart,k,ipass,idump,nums,nerr,index=imu)
        endif
-       if (eos_outputs_gamma(ieos) .or. update_muGamma .or. (use_krome .and. (ieos/=22))) then
+       if (eos_outputs_gamma(ieos) .or. update_muGamma .or. use_krome) then
           call write_array(1,eos_vars,eos_vars_label,1,npart,k,ipass,idump,nums,nerr,index=igamma)
        endif
        ! write stamatellos cooling values
