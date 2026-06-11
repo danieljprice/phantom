a1b21c3c02f70078190add36aada8093e4a30aa7
camillelandri <mangakam03@gmail.com>
2026-06-11 07:41:50 +0200

    fix of extra space that prevented the preprocessor to work properly

--- src/main/krome.F90
+++ src/main/krome.F90
@@ -40,16 +40,16 @@ subroutine initialise_krome()
  use part,       only:abundance,abundance_label,eos_vars,igamma,imu,T_gas_cool
  use dim,        only:maxvxyzu
  use timestep,   only:dtextforce,dtmax
- #ifdef KROME
+#ifdef KROME
  use krome_main, only:krome_init
  use krome_user, only:krome_idx_He,krome_idx_C,krome_idx_N,krome_idx_O,krome_idx_H,&
        krome_get_names,krome_get_mu_x,krome_get_gamma_x,&
        krome_idx_S,krome_idx_Fe,krome_idx_Si,krome_idx_Mg,krome_idx_Na,&
        krome_idx_P,krome_idx_F
- #endif
+#endif
  real :: wind_temperature
 
- #ifdef KROME
+#ifdef KROME
  print *, ""
  print *, "==================================================="
  print *, "=                                                 ="
@@ -107,25 +107,23 @@ subroutine initialise_krome()
     print *, "CHEMISTRY PROBLEM: ISOTHERMAL SETUP USED, INTERNAL ENERGY NOT STORED"
  endif
  dtextforce = min(dtextforce,dtmax/2.0**10)  ! Required since a cooling timestep is not initialised for implicit cooling
- #else
-  print *, "KROME NOT INITIALISED: CHEMISTRY NOT INCLUDED IN THIS SIMULATION"
-  #endif
+#endif
 
 end subroutine initialise_krome
 
 subroutine update_krome(dt,xyzh,u,rho,xchem,gamma_in,mu_in,T_gas_cool)
  use units,         only:unit_density,utime
  use eos,           only:ieos,get_temperature,get_local_u_internal!,temperature_coef
- #ifdef KROME
+#ifdef KROME
  use krome_main,    only:krome
  use krome_user,    only:krome_consistent_x,krome_get_mu_x,krome_get_gamma_x
- #endif
+#endif
  real, intent(in)    :: dt,xyzh(4),rho
  real, intent(inout) :: u,gamma_in,mu_in,xchem(:)
  real, intent(out)   :: T_gas_cool
  real :: T_local, dt_cgs, rho_cgs
 
- #ifdef KROME
+#ifdef KROME
  dt_cgs  = dt*utime
  rho_cgs = rho*unit_density
  T_local = get_temperature(ieos,xyzh(1:3),rho,(/0.,0.,0.,u/),gammai=gamma_in,mui=mu_in)
@@ -141,7 +139,7 @@ subroutine update_krome(dt,xyzh,u,rho,xchem,gamma_in,mu_in,T_gas_cool)
  ! get the new internal energy
  u = get_local_u_internal(gamma_in,mu_in,T_local)
  ! u = T_local/(mu_in*temperature_coef)/(gamma_in-1.)
- #endif
+#endif
 end subroutine update_krome
 
 !----------------------------------------------------------------
