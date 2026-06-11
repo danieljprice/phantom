7357427bd8520089b2b87f5644d7359e461fe5d0
Ali Pourmand <alpourmand@gmail.com>
2026-06-11 14:23:11 +1000

    adding some skeletons of some functions needed, just saving now

--- src/main/eos_mesa.f90
+++ src/main/eos_mesa.f90
@@ -61,6 +61,8 @@ subroutine init_eos_mesa(x,z,ierr)
  call read_eos_mesa(x,z,ierr)
  call get_opacity_constants_mesa
  call read_opacity_mesa(x,z)
+ mesa_eos_prefix="output_rhos_"
+ call read_eos_gr_mesa(x,z,ierr) ! read GR tables
 
 end subroutine init_eos_mesa
 
