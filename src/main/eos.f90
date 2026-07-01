902f7c4b848948b1bc5c807b803305e9ad42ec6b
Ali Pourmand <alpourmand@gmail.com>
2026-07-01 20:25:34 +1000

    made an important change . note that in eos mesa some tables are not in log, so you need to specify which are and areny

--- src/main/eos.f90
+++ src/main/eos.f90
@@ -1185,8 +1185,10 @@ subroutine get_p_from_rho_s(ieos,S,rho,mu,P,temp,niter_out)
  real,    intent(inout) :: temp
  real,    intent(out)   :: P
  integer, intent(in)    :: ieos
- integer, intent(out), optional :: niter_out
- real :: cgsrho,cgspres,cgss
+ real                :: corr,df,f,temp_new,cgsrho,cgsp,cgss
+ real,    parameter  :: eoserr=1e-12
+ integer             :: niter
+ integer, parameter  :: nitermax = 1000
 
  ! change to cgs unit
  cgsrho = rho*unit_density
@@ -1198,8 +1200,8 @@ subroutine get_p_from_rho_s(ieos,S,rho,mu,P,temp,niter_out)
     cgspres = cgsrho*Rg*temp / mu
  case(10)
     !!! For GR, not tested yet!
+    mesas = cgss/(kboltz*avogadro) ! the MESA tables are specific entropy divided by (avo*kerg).
     call get_eos_ptemp_from_rhos_mesa_gr(cgsrho,cgss,cgsP,temp)
-
  case (12)
     call get_idealplusrad_tempfromrhoS(cgsrho,cgss,mu,temp,cgspres,niter_out)
  case default
@@ -1238,7 +1240,7 @@ subroutine get_u_from_rho_s(ieos,S,rho,u)
  select case (ieos)
  case(10)
     !!! For GR, not tested yet!
-       call get_eos_u_from_rhos_mesa_gr(cgsrho,cgss,cgsu)
+    call get_eos_u_from_rhos_mesa_gr(cgsrho,cgss,cgsu)
 
  case default
     cgsu = 0.
