5ea5fe6b6689b54bf75ef05c71a92c08990a3b83
camillelandri <camille.landri@gmail.com>
2026-07-02 11:46:03 +0200

    -  write hdf5 output (just one group with abundances for now) - adapt number of threads to 1 if Krome is compiled with gfortran

--- src/utils/analysis_krome.f90
+++ src/utils/analysis_krome.f90
@@ -20,12 +20,15 @@ module analysis
  use krome_user, only:krome_nmols
  use part,       only: maxp
  use raytracer,  only: get_all_tau
+ use hdf5
+ use omp_lib
+
  implicit none
  character(len=20), parameter, public :: analysistype = 'krome'
  public :: do_analysis
  logical, allocatable :: mask(:)
 
- real, allocatable    :: abundance(:,:), abundance_prev(:,:), one(:)
+ real(8), allocatable    :: abundance(:,:), abundance_prev(:,:), one(:)
  character(len=16)    :: abundance_label(krome_nmols)
  integer(8), allocatable :: iorig_old(:)
  integer, allocatable :: iprev(:)
@@ -35,11 +38,12 @@ module analysis
 contains
 
 subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
- use part,        only: isdead_or_accreted, iorig, rhoh, nptmass, xyzmh_ptmass, iReff
- use neighkdtree, only:build_tree
- use units,       only: utime,unit_density
+ use part,        only: isdead_or_accreted, iorig, rhoh, nptmass, xyzmh_ptmass, iReff, iboundary, igas, iphase, iamtype
+ use neighkdtree, only: build_tree
+ use units,       only: utime,unit_density,udist
+ use physcon,     only: atomic_mass_unit
  use eos,         only: get_temperature, ieos, gamma,gmw, init_eos
- use io,          only: fatal
+ use io,          only: fatal, iverbose
  use krome_main,  only: krome_init, krome
  use krome_user,  only: krome_get_names,krome_set_user_Auv,krome_set_user_xi,&
                         krome_set_user_alb,krome_set_user_AuvAv
@@ -49,17 +53,25 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
  real,             intent(in) :: particlemass,time
  real, save    :: tprev = 0.
  integer, save :: nprev = 0
- real          :: dt_cgs, rho_cgs, numberdensity, T_gas, gammai, mui, AUV, xi
+ real          :: dt_cgs, rho_cgs, rholist(maxp), Tlist(maxp), mulist(maxp), Auvlist(maxp), xilist(maxp)
+ real          :: numberdensity, T_gas, gammai, mui, AUV, xi
  real          :: abundance_part(krome_nmols), Y(krome_nmols), column_density(npart), xyzh_copy(4,npart)
- integer       :: i, j, ierr, completed_iterations, npart_copy = 0
+ real          :: max_radius, radius
+ integer       :: i, j, ierr, completed_iterations, npart_copy = 0, hdferr, i_radius = 1
+ 
+#ifdef __GFORTRAN__ 
+   print*, "Setting number of threads to 1 (KROME is not thread-safe when compiled with gfortran)"
+   call omp_set_num_threads(1)
+#endif
 
  if (.not.done_init) then
     done_init = .true.
+
     print*, "initialising KROME"
     call krome_init()
     print*, "Initialised KROME"
+
     abundance_label(:) = krome_get_names()
-    maxp = maxp /2
     allocate(abundance(krome_nmols,maxp))
     abundance = 0.
     allocate(abundance_prev(krome_nmols,maxp))
@@ -70,6 +82,7 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
     iorig_old = 0
     allocate(iprev(maxp))
     iprev = 0
+
     print*, "setting abundances"
     !$omp parallel do default(none) &
     !$omp shared(npart,xyzh,vxyzu,dt_cgs,nprev,iorig,iorig_old,iprev) &
@@ -84,58 +97,100 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
     enddo
     call init_eos(ieos, ierr)
     if (ierr /= 0) call fatal(analysistype, "Failed to initialise EOS")
- else
+
+   ! initialize HDF5
+   call h5open_f(hdferr)
+   if (hdferr /= 0) then
+      print*,'ERROR: Failed to initialize HDF5'
+      return
+   endif
+
+   else
     dt_cgs = (time - tprev)*utime
     completed_iterations = 0
-    print*, "not first step data, timestep = ",dt_cgs, "npart = ",npart, "nprev = ",nprev
+    print*, dumpfile, ": not first step data, timestep = ",dt_cgs, "npart = ",npart, "nprev = ",nprev
+
     xyzmh_ptmass(iReff,1) = 2.
     npart_copy = npart
     xyzh_copy = xyzh(:,:npart)
     call build_tree(npart_copy,npart_copy,xyzh_copy,vxyzu)
     call get_all_tau(npart, nptmass, xyzmh_ptmass, xyzh, one, 5, .false., column_density)
+
+    max_radius = 0.0
+    do i = 1, npart
+       if (.not.isdead_or_accreted(xyzh(4, i))) then
+          radius = sqrt(xyzh(1, i)**2 + xyzh(2, i)**2 + xyzh(3, i)**2)
+          if (radius > max_radius) then
+             max_radius = radius
+             i_radius = i
+          endif
+       endif
+    enddo
+    column_density = column_density + rhoh(xyzh(4,i_radius),particlemass)*unit_density * max_radius * udist
+
+    rholist = 0.
+      Tlist = 0.
+     mulist = 0.
+    Auvlist = 0.
+     xilist = 0.
+
     !$omp parallel do default(none) &
-    !$omp shared(npart,xyzh,vxyzu,dt_cgs,nprev,iorig,iorig_old,iprev) &
-    !$omp shared(abundance,abundance_prev,particlemass,unit_density) &
+    !$omp shared(npart,xyzh,vxyzu,dt_cgs,nprev,iorig,iorig_old,iprev,iverbose) &
+    !$omp shared(abundance,abundance_prev,particlemass,unit_density,udist) &
     !$omp shared(ieos,gamma,gmw,time,completed_iterations,column_density,AuvAv,albedo) &
+    !$omp shared(rholist,Tlist,mulist,Auvlist,xilist,iphase) &
     !$omp private(i,j,abundance_part,Y,rho_cgs,numberdensity,T_gas,gammai,mui,AUV,xi)
+    
     outer: do i=1,npart
        if (.not.isdead_or_accreted(xyzh(4,i))) then
+
           inner: do j=1,nprev
              if (iorig(i) == iorig_old(j)) then
                 iprev(i) = j
                 exit inner
              endif
           enddo inner
+
           if (j == iprev(i)) then
              abundance_part(:) = abundance_prev(:,iprev(i))
           else
              call chem_init(abundance_part)
           endif
-          !Thermodynamic quantities
-          rho_cgs = rhoh(xyzh(4,i),particlemass)*unit_density
-          gammai = gamma
-          mui    = gmw
-          numberdensity = rho_cgs / (mui * 1.6605E-24)
-          T_gas = get_temperature(ieos,xyzh(1:3, i),rhoh(xyzh(4,i),particlemass),vxyzu(:,i),gammai,mui)
-          T_gas = max(T_gas,20.0d0)
+          if (iamtype(iphase(i)) /= iboundary .and. i > 2460) then ! 2460 is the amount of boundary particles
+            !Thermodynamic quantities
+             rho_cgs = rhoh(xyzh(4,i),particlemass)*unit_density
+             gammai = gamma
+             mui    = gmw
+             numberdensity = rho_cgs / (mui * atomic_mass_unit)
+             T_gas = get_temperature(ieos,xyzh(1:3, i),rhoh(xyzh(4,i),particlemass),vxyzu(:,i),gammai,mui)
+             T_gas = max(T_gas,20.0d0)
+             
+             !Radiation quantities
+             AUV = AuvAv * column_density(i) / (mui * atomic_mass_unit) / 1.87e21
+             xi = get_xi(AUV)
+             
+             call krome_set_user_Auv(AUV)
+             call krome_set_user_xi(xi)
+             call krome_set_user_alb(ALBEDO)
+             call krome_set_user_AuvAv(AuvAv)
 
-          !Radiation quantities
-          AUV = 4.65 * column_density(i) / 1.87e21
-          xi = get_xi(AUV)
+             Y = abundance_part*numberdensity
+             call krome(Y,T_gas,dt_cgs)
+             abundance_part = Y/numberdensity
+             abundance(:,i) = abundance_part
 
-          call krome_set_user_Auv(AUV)
-          call krome_set_user_xi(xi)
-          call krome_set_user_alb(ALBEDO)
-          call krome_set_user_AuvAv(AuvAv)
-
-          Y = abundance_part*numberdensity
-          call krome(Y,T_gas,dt_cgs)
-          abundance_part = Y/numberdensity
-          abundance(:,i) = abundance_part
+             rholist(i) = rho_cgs
+               Tlist(i) = T_gas
+              mulist(i) = mui
+             Auvlist(i) = AUV
+              xilist(i) = xi
+          endif
        endif
-       !$omp atomic
-       completed_iterations = completed_iterations + 1
-       print*, 'Completed ', completed_iterations, ' of ', npart
+       if (iverbose > 1) then
+          !$omp atomic
+          completed_iterations = completed_iterations + 1
+          print*, 'Completed ', completed_iterations, ' of ', npart
+       endif
     enddo outer
  endif
 
@@ -178,25 +233,51 @@ real function get_xi(AUV)
 end function get_xi
 
 subroutine write_chem(npart, dumpfile)
- use krome_user, only:krome_idx_He,krome_idx_C,krome_idx_N,krome_idx_O,&
-       krome_idx_H,krome_idx_S,krome_idx_Fe,krome_idx_Si,krome_idx_Mg,&
-       krome_idx_Na,krome_idx_P,krome_idx_F,krome_idx_CO,krome_idx_C2H2,&
-       krome_idx_C2H,krome_idx_H2,krome_idx_SiNC,krome_idx_e
+ use krome_user, only:krome_idx_He
  integer,          intent(in) :: npart
  character(len=*), intent(in) :: dumpfile
- integer :: i, iu
+ integer                      :: hdferr
+ integer :: i
+ integer(hid_t) :: file_id, dspace_id, dset_id, group_id
+ integer(hsize_t) :: dims(1)
 
- open(newunit=iu,file=dumpfile//'.comp',status='replace',action='write')
- write(iu, *) '# H, He, C, N, O, S, Fe, Si, Mg, Na, P, F, CO, C2H2, C2H, H2, SiNC, e-'
- do i=1, npart
-    write(iu, *) abundance(krome_idx_H, i),  abundance(krome_idx_He, i),   abundance(krome_idx_C, i),   &
-                 abundance(krome_idx_N, i),  abundance(krome_idx_O, i),    abundance(krome_idx_S, i),   &
-                 abundance(krome_idx_Fe, i), abundance(krome_idx_Si, i),   abundance(krome_idx_Mg, i),  &
-                 abundance(krome_idx_Na, i), abundance(krome_idx_P, i),    abundance(krome_idx_F, i),   &
-                 abundance(krome_idx_CO, i), abundance(krome_idx_C2H2, i), abundance(krome_idx_C2H, i), &
-                 abundance(krome_idx_H2, i), abundance(krome_idx_SiNC, i), abundance(krome_idx_e, i)
+ ! create HDF5 file
+ print "(1x,a)",'Writing to '//trim(dumpfile)//'.h5'
+ call h5fcreate_f(trim(dumpfile)//'.h5', H5F_ACC_TRUNC_F, file_id, hdferr)
+ if (hdferr /= 0) then
+    print*,'ERROR: Failed to create HDF5 file'
+    return
+ endif
+
+  ! create group for particle data
+ call h5gcreate_f(file_id, 'chemistry', group_id, hdferr)
+ if (hdferr /= 0) then
+    print*,'ERROR: Failed to create HDF5 group'
+    return
+ endif
+
+ ! create dataspace for particle datasets
+ dims(1) = npart
+ call h5screate_simple_f(1, dims, dspace_id, hdferr)
+  if (hdferr /= 0) then
+    print*,'ERROR: Failed to create HDF5 dataspace'
+    return
+ endif
+ do i=1,krome_nmols
+   ! create dataset for each abundance
+    call h5dcreate_f(group_id, trim(adjustl(abundance_label(i))), H5T_NATIVE_DOUBLE, dspace_id, dset_id, hdferr)
+    ! write abundance data to dataset
+    call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, abundance(i,1:npart), dims, hdferr)
+    if (hdferr /= 0) then
+       print*,'ERROR: Failed to create/write dataset for '//trim(adjustl(abundance_label(i)))
+       return
+    endif
+    call h5dclose_f(dset_id, hdferr)
  enddo
- close(iu)
+ call h5sclose_f(dspace_id, hdferr)
+ call h5gclose_f(group_id, hdferr)
+ call h5fclose_f(file_id, hdferr)
+ 
 
 end subroutine write_chem
 
