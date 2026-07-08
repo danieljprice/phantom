3c2e60dc5a029ffdd811321e9a7e2aa474179c21
Ali Pourmand <alpourmand@gmail.com>
2026-07-08 13:06:02 +1000

    added a hidden variable called mesa_use_legacy_tables which is set to false by default to use thomas reichardt's old tables

--- src/main/datafiles.f90
+++ src/main/datafiles.f90
@@ -56,13 +56,18 @@ end function find_phantom_datafile
 !+
 !----------------------------------------------------------------
 function map_dir_to_web(search_dir) result(url)
+ use options, only: mesa_use_legacy_tables
  character(len=*), intent(in) :: search_dir
  character(len=120) :: url
 
  !print*,' search_dir=',trim(search_dir)
  select case(search_dir)
- case('data/eos/mesa')
-    url = 'https://zenodo.org/records/13148447/files/'
+ case('data/eos/mesa') !!! here this is if someone wants to cehck the older version of the mesa tables (Reichardt et al 2020), which are no longer used by default
+   if (mesa_use_legacy_tables) then
+      url = 'https://zenodo.org/records/13148447/files/'
+   else
+      url = 'https://zenodo.org/records/newlink' !! not uploaded yet
+   endif
  case('data/eos/shen')
     url = 'https://zenodo.org/records/13163155/files/'
  case('data/eos/helmholtz')
